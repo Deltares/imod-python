a0456ca63b55e3809e114f525c157f7d182c8ec0
Martijn Visser <mgvisser@gmail.com>
2017-12-20 12:04:30 +0100

    add write_rasterio to write DataArray to GDAL

--- imod/io.py
+++ imod/io.py
@@ -352,3 +352,39 @@ def loadipf(globpath):
 
     bigdf = pd.concat(dfs, ignore_index=True)
     return bigdf
+
+# TODO perhaps unify write_rasterio and writeidf into a single function?
+
+def write_rasterio(path, da, driver=None, nodata=-9999):
+    """Write DataArray to GDAL supported geospatial rasters using rasterio"""
+    # Not directly related to iMOD, but provides a missing link, together
+    # with xarray.open_rasterio.
+    # Note that this function can quickly become outdated as
+    # the xarray rasterio connection matures, see for instance:
+    # https://github.com/pydata/xarray/issues/1736
+    # https://github.com/pydata/xarray/pull/1712
+    profile = da.attrs.copy()
+    if driver is None:
+        ext = os.path.splitext(path)[1].lower()
+        if ext in ('.tif', '.tiff'):
+            driver = 'GTiff'
+        elif ext == '.asc':
+            driver = 'AAIGrid'
+        else:
+            raise ValueError('Unknown extension {}, specifiy driver'.format(ext))
+    # prevent rasterio warnings
+    if driver == 'AAIGrid':
+        profile.pop('res')
+        profile.pop('is_tiled')
+    # transform will be affine object in next xarray
+    profile['transform'] = profile['transform'][:6]
+    profile['driver'] = driver
+    profile['height'] = da.y.size
+    profile['width'] = da.x.size
+    profile['count'] = 1
+    profile['dtype'] = da.dtype
+    profile['nodata'] = nodata
+    dafilled = da.fillna(nodata)
+    with rasterio.Env():
+        with rasterio.open(path, 'w', **profile) as ds:
+            ds.write(dafilled.values, 1)
