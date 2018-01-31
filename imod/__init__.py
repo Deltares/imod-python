c34d18af0fa75f7a631f0081c402daedaf32c4b4
Martijn Visser <mgvisser@gmail.com>
2018-01-31 18:45:31 +0100

    add all modules to __init__

--- imod/__init__.py
+++ imod/__init__.py
@@ -1,2 +1,8 @@
+# without these this usage wouldn't work:
+# import imod
+# imod.idf.load
 from imod import idf
+from imod import ipf
 from imod import tec
+from imod import rasterio
+from imod import util
