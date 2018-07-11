73925b75f9fedf88a2827d50a483ba33fa74b7b6
Martijn Visser <mgvisser@gmail.com>
2018-07-11 19:01:02 +0200

    finish making rasterio optional

--- imod/__init__.py
+++ imod/__init__.py
@@ -1,8 +1,14 @@
 # without these this usage wouldn't work:
 # import imod
-# imod.idf.load
+# imod.idf.*
 from imod import idf
 from imod import ipf
 from imod import tec
-from imod import rasterio
 from imod import util
+
+# since this is a big dependency that is sometimes hard to install
+# and not always required, we made this an optional dependency
+try:
+    from imod import rasterio
+except ImportError:
+    pass
