1cdb85ffb7fee39461ef732780d74b5f94a78e98
Huite Bootsma <huitebootsma@gmail.com>
2018-07-18 18:19:44 +0200

    Implemented roughly all IPF functionality in ipf.py, and included tests (which may still need some cleaning up). Set pytest as the testing suite.

--- imod/ipf.py
+++ imod/ipf.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import re
-from imod import util
-from glob import glob
-from pathlib import Path
-from collections import OrderedDict
 import numpy as np
+from collections import OrderedDict
+from glob import glob
 from io import StringIO
+from pathlib import Path
+from imod import util
 
 # Maybe look at dask dataframes, if we run into very large tabular datasets:
 # http://dask.pydata.org/en/latest/examples/dataframe-csv.html
 # the simple CSV format IPF cannot be read like this, it is best to use pandas.read_csv
-def read(path):
+def read(path, kwargs={}, assoc_kwargs={}):
     """Read an IPF file to a pandas.DataFrame"""
 
     if isinstance(path, str):
@@ -20,16 +20,22 @@ def read(path):
         nrow = int(f.readline().strip())
         ncol = int(f.readline().strip())
         colnames = [f.readline().strip().strip("'").strip('"') for _ in range(ncol)]
-        indexcol, ext = map(str.strip, f.readline().split(","))
+        line = f.readline()
+        # TODO: are space and comma the only allowed separators?
+        try:
+            indexcol, ext = map(str.strip, line.split(","))
+        except ValueError:
+            indexcol, ext = map(str.strip, line.split())
+
         indexcol = int(indexcol)
         if indexcol > 1:
-            df = pd.read_csv(f, header=None, names=colnames, nrows=nrow)
+            df = pd.read_csv(f, header=None, names=colnames, nrows=nrow, **kwargs)
             dfs = []
             for row in df.itertuples():
                 filename = row[indexcol]
                 # associate paths are relative to "mother" ipf
-                path_assoc = path.join_path(filename + "." + ext)
-                df_assoc = read_associated(path_assoc)
+                path_assoc = path.parent.joinpath(filename + "." + ext)
+                df_assoc = read_associated(path_assoc, assoc_kwargs)
 
                 for name, value in zip(colnames, row[1:]):  # ignores df.index in row
                     df_assoc[name] = value
@@ -37,15 +43,19 @@ def read(path):
                 dfs.append(df_assoc)
             df = pd.concat(dfs, ignore_index=True, sort=False)
         else:
-            df = pd.read_csv(f, header=None, names=colnames, nrows=nrow)
+            df = pd.read_csv(f, header=None, names=colnames, nrows=nrow, **kwargs)
     return df
 
 
 # TODO check if this also supports space separators like the iMOD manual 9.7.1
-# TODO allow passing kwargs to pandas.read_csv, like delim_whitespace=True
 # TODO check if it can infer the datetime formats
-def read_associated(path):
+def read_associated(path, kwargs={}):
     """Read a file that is associated from an IPF file"""
+    # TODO: passing on pandas.read_csv kwargs maybe not be enough?
+    # since txt's don't have to be consistent within a single ipf ...
+
+    # avoid kwargs mutation
+    assoc_kwargs = kwargs.copy()
 
     # deal with e.g. incorrect capitalization
     if isinstance(path, str):
@@ -54,7 +64,11 @@ def read_associated(path):
 
     with open(path) as f:
         nrow = int(f.readline().strip())
-        ncol, itype = map(int, map(str.strip, f.readline().split(",")))
+        line = f.readline()
+        try:
+            ncol, itype = map(int, map(str.strip, line.split(",")))
+        except ValueError:
+            ncol, itype = map(int, map(str.strip, line.split()))
         na_values = OrderedDict()
 
         # use pandas for csv parsing: stuff like commas within quotes
@@ -66,7 +80,8 @@ def read_associated(path):
         lines = "".join(lines)
         # Normally, this ought to work:
         # metadata = pd.read_csv(f, header=None, nrows=ncol).values
-        metadata = pd.read_csv(StringIO(lines), header=None, nrows=ncol)
+        # TODO: replace when bugfix is released
+        metadata = pd.read_csv(StringIO(lines), header=None, nrows=ncol, **assoc_kwargs)
         # header description possibly includes nodata
         usecols = np.arange(ncol)[pd.notnull(metadata[0])]
         metadata = metadata.iloc[usecols, :]
@@ -78,7 +93,7 @@ def read_associated(path):
         itype_kwargs = {}
         if itype == 1:  # Timevariant information: timeseries
             # check if first column is time in in [yyyymmdd] or [yyyymmddhhmmss]
-            itype_kwargs["parse_dates"] = True  # this parses the index
+            itype_kwargs["parse_dates"] = [0]  # this parses the first column
             itype_kwargs["infer_datetime_format"] = True
         elif itype in (2, 3):  # 1D borehole or Cone Penetration
             # enforce first column is a float
@@ -91,6 +106,8 @@ def read_associated(path):
                 colnames[2]: np.float64,
             }
 
+        assoc_kwargs.update(itype_kwargs)
+
         df = pd.read_csv(
             f,
             header=None,
@@ -98,16 +115,24 @@ def read_associated(path):
             usecols=usecols,
             nrows=nrow,
             na_values=na_values,
-            **itype_kwargs
+            **assoc_kwargs
         )
     return df
 
 
-def load(path):
+def load(path, kwargs={}, assoc_kwargs={}):
     """Load one or more IPF files to a single pandas.DataFrame
     
     The different IPF files can be from different model layers,
     but otherwise have to have identical columns"""
+    # TODO: there's no real necessity to force identical columns?
+    # pd.concatenate will just stuff everything together, with a lot of columns
+    # the user can then just correct.
+    # Because the IPF format mostly relies on column order, there is an
+    # extreme lack of consistency for ipf column names. Not supporting
+    # heterogeneous column names limits functionality regarding existing
+    # IPFs very strongly.
+
     # convert since for Path.glob non-relative patterns are unsupported
     if isinstance(path, Path):
         path = str(path)
@@ -120,7 +145,7 @@ def load(path):
     dfs = []
     for p in paths:
         layer = util.decompose(p).get("layer")
-        df = read(p)
+        df = read(p, kwargs, assoc_kwargs)
         if layer is not None:
             df["layer"] = layer
         dfs.append(df)
@@ -128,9 +153,51 @@ def load(path):
     bigdf = pd.concat(dfs, ignore_index=True, sort=False)
     # concat sorts the columns, restore original order, see pandas issue 4588
     # TODO: Decide why reindex? This throws out columns of other dataframes that may
-    # not be present in the first dataframe.
+    # not be present in the first dataframe. See todo comment above.
     # bigdf = bigdf.reindex(dfs[0].columns, axis=1)
     return bigdf
+
+
+def _coerce_itype(itype):
+    if itype in [None, 1, 2, 3, 4]:
+        pass
+    elif itype.lower() == "timeseries":
+        itype = 1
+    elif itype.lower() == "borehole1d":
+        itype = 2
+    elif itype.lower() == "cpt":
+        itype = 3
+    elif itype.lower() == "borehole3d":
+        itype = 4
+    else:
+        raise ValueError("Invalid IPF itype")
+    return itype
+
+
+def write_assoc(path, df, itype=1, nodata=np.nan):
+    # TODO: default nodata value?
+    itype = _coerce_itype(itype)
+    required_columns = {1: ["time"], 2: ["top"], 3: ["top"], 4: ["x", "y", "z"]}
+
+    # Ensure columns are in the right order for the itype
+    colnames = [s.lower() for s in list(df)]
+    columnorder = []
+    for colname in required_columns[itype]:
+        assert colname in df.columns, "given itype requires column {}".format(colname)
+        colnames.remove(colname)
+        columnorder.append(colname)
+    columnorder += colnames
+
+    nrecords, nfields = df.shape
+    with open(path, "w") as f:
+        f.write("{}\n{},{}\n".format(nrecords, nfields, itype))
+        [f.write("{},{}\n".format(colname, nodata)) for colname in columnorder]
+    # workaround pandas issue by closing the file first, see
+    # https://github.com/pandas-dev/pandas/issues/19827#issuecomment-398649163
+
+    df = df.fillna(nodata)
+    df = df[columnorder]
+    df.to_csv(path, index=False, header=False, mode="a", date_format="%Y%m%d%H%M%S")
 
 
 def write(path, df, indexcolumn=0):
@@ -145,7 +212,51 @@ def write(path, df, indexcolumn=0):
     df.to_csv(path, index=False, header=False, mode="a")
 
 
-def save(path, df, itype=None):
+def _is_single_value(group):
+    return len(pd.unique(group)) == 1
+
+
+def _compose_ipf(path, df, itype, assoc_ext, nodata):
+    if itype is None:
+        write(path, df)
+    else:
+        itype = _coerce_itype(itype)
+        colnames = [s.lower() for s in list(df)]
+        for refname in ["x", "y", "id"]:
+            assert refname in colnames, "given itype requires column {}".format(refname)
+            colnames.remove(refname)
+
+        grouped = df.groupby("id")
+        assert (
+            grouped["x"].apply(_is_single_value).all()
+        ), "column x contains more than one value per id"
+        assert (
+            grouped["y"].apply(_is_single_value).all()
+        ), "column y contains more than one value per id"
+        if "layer" in colnames:
+            assert (
+                grouped["layer"].apply(_is_single_value).all()
+            ), "column layer contains more than one value per id"
+        # get columns that have only one value within a group, to save them in ipf also
+
+        ipf_columns = [
+            (colname, "first")
+            for colname in colnames
+            if grouped[colname].apply(_is_single_value).all()
+        ]
+
+        for idcode, group in grouped:
+            assoc_path = path.parent.joinpath(idcode + "." + assoc_ext)
+            write_assoc(assoc_path, group, itype, nodata)
+
+        # ensures right order for x, y, id; so that also indexcolumn == 3
+        agg_kwargs = OrderedDict([("x", "first"), ("y", "first"), ("id", "first")])
+        agg_kwargs.update(ipf_columns)
+        agg_df = grouped.agg(agg_kwargs)
+        write(path, agg_df, 3)
+
+
+def save(path, df, itype=None, assoc_ext="txt", nodata=np.nan):
     """Save a pandas.DataFrame to one or more IPF files, split per layer"""
     d = util.decompose(path)
     d["extension"] = ".ipf"
@@ -155,86 +266,7 @@ def save(path, df, itype=None):
         for layer, group in df.groupby("layer"):
             d["layer"] = layer
             fn = util.compose(d)
-            write(fn, group)
+            _compose_ipf(fn, group, itype, assoc_ext, nodata)
     else:
         fn = util.compose(d)
-        write(fn, df)
-
-   # if itype in [None, 1, 2, 3, 4]:
-   #     pass
-   # elif itype.lower() == "timeseries":
-   #     itype = 1
-   # elif itype.lower() == "borehole1d":
-   #     itype = 2
-   # elif itype.lower() == "cpt":
-   #     itype = 3
-   # elif itype.lower() == "borehole3d":
-   #     itype = 4
-   # else
-   #     raise ValueError("Invalid IPF itype")
-
-    if itype == 1:
-        assert "time" in df.columns
-        try:
-            df["time"] = df["time"].to_datetime()
-    if itype == 2 or itype == 3:
-        # lame name? but top is less confusing than e.g. z
-        assert "top" in df.columns
-    if itype == 4:
-        assert "x" in df.columns
-        assert "y" in df.columns
-        assert "z" in df.columns
-
-    if "layer" in df.columns:
-        for layer, group in df.groupby("layer"):
-            d["layer"] = layer
-
-            for idcode, regroup in group.groupby("id"):
-                write_associated_itype1(path, regroup)
-
-    else:
-        fn = util.compose(d)
-        write(fn, df)
-    
-
-
-def write_associated_itype1(path, df):
-    nrecords, nfields = df.shape
-    with open(path, "w") as f:
-        f.write("{}\n{}\n".format(nrecords, nfields))
-        [f.write("{},{}\n".format(colname, na_value)) for colname in df.columns]
-    # workaround pandas issue by closing the file first, see
-    # https://github.com/pandas-dev/pandas/issues/19827#issuecomment-398649163
-    df.to_csv(path, index=False, header=False, mode="a")
-
-
-def write_associated_itype2(path, df):
-    nrecords, nfields = df.shape
-    with open(path, "w") as f:
-        f.write("{}\n{},2\n".format(nrecords, nfields))
-        [f.write("{}\n".format(colname)) for colname in df.columns]
-        f.write("1,TXT\n")
-    # workaround pandas issue by closing the file first, see
-    # https://github.com/pandas-dev/pandas/issues/19827#issuecomment-398649163
-    df.to_csv(path, index=False, header=False, mode="a")
-
-def write_associated_itype2(path, df):
-    nrecords, nfields = df.shape
-    with open(path, "w") as f:
-        f.write("{}\n{}\n".format(nrecords, nfields))
-        [f.write("{}\n".format(colname)) for colname in df.columns]
-        f.write("1,TXT\n")
-    # workaround pandas issue by closing the file first, see
-    # https://github.com/pandas-dev/pandas/issues/19827#issuecomment-398649163
-    df.to_csv(path, index=False, header=False, mode="a")
-
-
-def write_associated_itype4(path, df):
-    nrecords, nfields = df.shape
-    with open(path, "w") as f:
-        f.write("{}\n{}\n".format(nrecords, nfields))
-        [f.write("{}\n".format(colname)) for colname in df.columns]
-        f.write("1,TXT\n")
-    # workaround pandas issue by closing the file first, see
-    # https://github.com/pandas-dev/pandas/issues/19827#issuecomment-398649163
-    df.to_csv(path, index=False, header=False, mode="a")
+        _compose_ipf(fn, df, itype, assoc_ext, nodata)
--- tests/test_ipf.py
+++ tests/test_ipf.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 import pytest
 import filecmp
@@ -7,6 +8,7 @@ import numpy as np
 from collections import OrderedDict
 import xarray as xr
 import pandas as pd
+from pathlib import Path
 
 
 def remove(path):
@@ -16,44 +18,329 @@ def remove(path):
         pass
 
 
-class TestIPF(unittest.TestCase):
-    def setUp(self):
-        self.path = "test.ipf"
-        self.path_out = "test-out.ipf"
-        # example from iMOD manual
-        self.ipfstring = (
+@pytest.fixture(scope="module")
+def write_basic_ipf(request):
+
+    # factory function seems easiest way to parameterize tests
+    def _write_basic_ipf(path, delim):
+        if isinstance(path, str):
+            path = Path(path)
+        path.parent.mkdir(exist_ok=True, parents=True)
+        ipfstring = (
             "2\n"
             "4\n"
             "X\n"
             "Y\n"
             "Z\n"
             '"City of Holland"\n'
-            "0,TXT\n"
-            "100.0,435.0,-32.3,Amsterdam\n"
-            '553.0,143.0,-7.3,"Den Bosch"\n'
+            "0{delim}TXT\n"
+            "100.0{delim}435.0{delim}-32.3{delim}Amsterdam\n"
+            '553.0{delim}143.0{delim}-7.3{delim}"Den Bosch"\n'
         )
-        with open(self.path, "w") as f:
-            f.write(self.ipfstring)
+        ipfstring = ipfstring.format(delim=delim)
+        with open(path, "w") as f:
+            f.write(ipfstring)
 
-    def tearDown(self):
-        remove(self.path)
-        remove(self.path_out)
+    # Use global list to add to a list of paths
+    # that were generated during testing?
+    def teardown():
+        try:
+            shutil.rmtree("test_ipf_dir")
+        except:
+            pass
 
-    def test_load(self):
-        df = ipf.load(self.path)
-        self.assertIsInstance(df, pd.DataFrame)
-        self.assertTrue(list(df) == ["X", "Y", "Z", "City of Holland"])
-        self.assertTrue(len(df) == 2)
-        self.assertTrue(df.iloc[0, 2] == -32.3)
-        self.assertTrue(df.iloc[1, 3] == "Den Bosch")
-        ipf.save("test-out", df)
-        # cannot do exact comparison since quoting is slightly different
-        # manual says: The different data for each field should be delimited by
-        # a single (or more) space(s), or a comma.
-        # we assume the different delimiters are not mixed inside an IPF
-        df2 = ipf.load(self.path_out)
-        self.assertTrue(df.equals(df2))
+    request.addfinalizer(teardown)
+    return _write_basic_ipf
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture(scope="module")
+def write_assoc_ipf(request):
+    def _write_assoc_ipf(path, delim, assoc_delim):
+        if isinstance(path, str):
+            path = Path(path)
+        path.parent.mkdir(exist_ok=True, parents=True)
+        ipfstring = (
+            "2\n"
+            "3\n"
+            "X\n"
+            "Y\n"
+            "ID\n"
+            "3{delim}TXT\n"
+            "100.0{delim}435.0{delim}A1000\n"
+            "553.0{delim}143.0{delim}B2000\n"
+        )
+        assoc_string = (
+            "2\n"
+            "2{delim}1\n"
+            "time{delim}-999.0\n"
+            "level{delim}-999.0\n"
+            "20180101000000{delim}1.0\n"
+            "20180102000000{delim}-999.0\n"
+        )
+        ipfstring = ipfstring.format(delim=delim)
+        assoc_string = assoc_string.format(delim=assoc_delim)
+        with open(path, "w") as f:
+            f.write(ipfstring)
+        with open(path.parent.joinpath("A1000.txt"), "w") as f:
+            f.write(assoc_string)
+        with open(path.parent.joinpath("B2000.txt"), "w") as f:
+            f.write(assoc_string)
+
+    def teardown():
+        try:
+            shutil.rmtree("test_ipf_dir")
+        except:
+            pass
+
+    request.addfinalizer(teardown)
+    return _write_assoc_ipf
+
+
+def test_load__comma(write_basic_ipf):
+    path = "test_ipf_dir/basic_comma.ipf"
+    write_basic_ipf(path, ",")
+    df = ipf.load(path)
+    assert isinstance(df, pd.DataFrame)
+    assert list(df) == ["X", "Y", "Z", "City of Holland"]
+    assert len(df) == 2
+    assert df.iloc[0, 2] == -32.3
+    assert df.iloc[1, 3] == "Den Bosch"
+
+
+def test_load__space(write_basic_ipf):
+    path = "test_ipf_dir/basic_space.ipf"
+    write_basic_ipf(path, " ")
+    df = ipf.load(path, {"delim_whitespace": True})
+    assert isinstance(df, pd.DataFrame)
+    assert list(df) == ["X", "Y", "Z", "City of Holland"]
+    assert len(df) == 2
+    assert df.iloc[0, 2] == -32.3
+    assert df.iloc[1, 3] == "Den Bosch"
+
+
+def test_load_associated__comma_comma(write_assoc_ipf):
+    path = "test_ipf_dir/assoc.ipf"
+    write_assoc_ipf(path, ",", ",")
+    df = ipf.load(path)
+
+    nrecords, nfields = df.shape
+    assert isinstance(df, pd.DataFrame)
+    assert nrecords == 4
+    assert nfields == 5
+    assert df["time"].iloc[0] == pd.to_datetime("2018-01-01")
+    assert df["time"].iloc[1] == pd.to_datetime("2018-01-02")
+    assert df["level"].iloc[0] == df["level"].iloc[2] == 1.0
+    assert pd.isnull(df["level"].iloc[1])
+    assert pd.isnull(df["level"].iloc[3])
+
+
+def test_load_associated__comma_space(write_assoc_ipf):
+    path = "test_ipf_dir/assoc.ipf"
+    write_assoc_ipf(path, ",", " ")
+    df = ipf.load(path, assoc_kwargs={"delim_whitespace": True})
+
+    nrecords, nfields = df.shape
+    assert isinstance(df, pd.DataFrame)
+    assert nrecords == 4
+    assert nfields == 5
+    assert df["time"].iloc[0] == pd.to_datetime("2018-01-01")
+    assert df["time"].iloc[1] == pd.to_datetime("2018-01-02")
+    assert df["level"].iloc[0] == df["level"].iloc[2] == 1.0
+    assert pd.isnull(df["level"].iloc[1])
+    assert pd.isnull(df["level"].iloc[3])
+
+
+def test_load_associated__space_space(write_assoc_ipf):
+    path = "test_ipf_dir/assoc.ipf"
+    write_assoc_ipf(path, " ", " ")
+    df = ipf.load(
+        path, kwargs={"delim_whitespace": True}, assoc_kwargs={"delim_whitespace": True}
+    )
+
+    nrecords, nfields = df.shape
+    assert isinstance(df, pd.DataFrame)
+    assert nrecords == 4
+    assert nfields == 5
+    assert df["time"].iloc[0] == pd.to_datetime("2018-01-01")
+    assert df["time"].iloc[1] == pd.to_datetime("2018-01-02")
+    assert df["level"].iloc[0] == df["level"].iloc[2] == 1.0
+    assert pd.isnull(df["level"].iloc[1])
+    assert pd.isnull(df["level"].iloc[3])
+
+
+def test_load_associated__space_comma(write_assoc_ipf):
+    path = "test_ipf_dir/assoc.ipf"
+    write_assoc_ipf(path, " ", ",")
+    df = ipf.load(path, kwargs={"delim_whitespace": True})
+
+    nrecords, nfields = df.shape
+    assert isinstance(df, pd.DataFrame)
+    assert nrecords == 4
+    assert nfields == 5
+    assert df["time"].iloc[0] == pd.to_datetime("2018-01-01")
+    assert df["time"].iloc[1] == pd.to_datetime("2018-01-02")
+    assert df["level"].iloc[0] == df["level"].iloc[2] == 1.0
+    assert pd.isnull(df["level"].iloc[1])
+    assert pd.isnull(df["level"].iloc[3])
+
+
+def test_write_assoc_itype1():
+    times = [pd.to_datetime(s) for s in ["2018-01-01", "2018-02-01"]]
+    df = pd.DataFrame.from_dict(
+        {
+            "x": [1, 1, 2, 2],
+            "y": [3, 3, 4, 4],
+            "id": ["A1", "A1", "B2", "B2"],
+            "level": [0.1, 0.2, 0.3, 0.4],
+            "time": times + times,
+            "location": ["loc1", "loc1", "loc2", "loc2"],
+        }
+    )
+    _, first_df = list(df.groupby("id"))[0]
+    ipf.write_assoc("A1.txt", first_df, itype=1, nodata=-999.0)
+    df2 = ipf.read_associated("A1.txt")
+    pd.testing.assert_frame_equal(first_df, df2, check_like=True)
+
+    remove("A1.txt")
+
+
+def test_write_assoc_itype2():
+    df = pd.DataFrame.from_dict(
+        {
+            "x": [1, 1, 2, 2],
+            "y": [3, 3, 4, 4],
+            "id": ["A1", "A1", "B2", "B2"],
+            "litho": [0.1, np.nan, 0.3, np.nan],
+            "top": [0.0, -0.5, -0.3, -0.5],
+            "location": ["loc1", "loc1", "loc2", "loc2"],
+        }
+    )
+    _, first_df = list(df.groupby("id"))[0]
+    ipf.write_assoc("A1.txt", first_df, itype=2, nodata=-999.0)
+    df2 = ipf.read_associated("A1.txt")
+    pd.testing.assert_frame_equal(first_df, df2, check_like=True)
+
+    remove("A1.txt")
+
+
+def test_write():
+    df = pd.DataFrame.from_dict(
+        {
+            "X": [100.0, 553.0],
+            "Y": [435.0, 143.0],
+            "Z": [-32.3, -7.3],
+            "City of Holland": ["Amsterdam", "Den Bosch"],
+        }
+    )
+    ipf.write("basic.ipf", df)
+    df2 = ipf.read("basic.ipf")
+    pd.testing.assert_frame_equal(df, df2, check_like=True)
+
+    remove("basic.ipf")
+
+
+def test_is_single_value():
+    df = pd.DataFrame(
+        {
+            "A": np.arange(8),
+            "B": list("aabbbbcc"),
+            "id": list("11112222"),
+            "grp_const": [3, 3, 3, 3, 4, 4, 4, 4],
+        }
+    )
+    grouped = df.groupby("id")
+    assert not grouped["A"].apply(ipf._is_single_value).all()
+    assert not grouped["B"].apply(ipf._is_single_value).all()
+    assert grouped["grp_const"].apply(ipf._is_single_value).all()
+
+
+def test_save__assoc_itype1():
+    times = [pd.to_datetime(s) for s in ["2018-01-01", "2018-02-01"]]
+    df = pd.DataFrame.from_dict(
+        {
+            "x": [1, 1, 2, 2],
+            "y": [3, 3, 4, 4],
+            "id": ["A1", "A1", "B2", "B2"],
+            "level": [0.1, 0.2, 0.3, 0.4],
+            "time": times + times,
+            "location": ["loc1", "loc1", "loc2", "loc2"],
+        }
+    )
+
+    ipf.save("save.ipf", df, itype=1, nodata=-999.0)
+    assert Path("save.ipf").exists()
+    assert Path("A1.txt").exists()
+    assert Path("B2.txt").exists()
+    df2 = ipf.load("save.ipf")
+    pd.testing.assert_frame_equal(df, df2, check_like=True)
+
+    remove("save.ipf")
+    remove("A1.txt")
+    remove("B2.txt")
+
+
+def test_save__assoc_itype2_():
+    df = pd.DataFrame.from_dict(
+        {
+            "x": [1, 1, 2, 2],
+            "y": [3, 3, 4, 4],
+            "id": ["A1", "A1", "B2", "B2"],
+            "litho": ["z", np.nan, "k", np.nan],
+            "top": [0.0, -0.5, -0.3, -0.8],
+            "location": ["loc1", "loc1", "loc2", "loc2"],
+        }
+    )
+
+    ipf.save("save.ipf", df, itype=2, nodata=-999.0)
+    assert Path("save.ipf").exists()
+    assert Path("A1.txt").exists()
+    assert Path("B2.txt").exists()
+    df2 = ipf.load("save.ipf")
+    print(df)
+    print(df2)
+    pd.testing.assert_frame_equal(df, df2, check_like=True)
+
+    remove("save.ipf")
+    remove("A1.txt")
+    remove("B2.txt")
+
+
+def test_save__assoc_itype1__layers():
+    times = [pd.to_datetime(s) for s in ["2018-01-01", "2018-02-01"]]
+    df = pd.DataFrame.from_dict(
+        {
+            "x": [1, 1, 2, 2, 3, 3, 4, 4],
+            "y": [3, 3, 4, 4, 5, 5, 6, 6],
+            "id": ["A1", "A1", "B2", "B2", "C3", "C3", "D4", "D4"],
+            "level": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+            "time": times * 4,
+            "location": [
+                "loc1",
+                "loc1",
+                "loc2",
+                "loc2",
+                "loc3",
+                "loc3",
+                "loc4",
+                "loc4",
+            ],
+            "layer": [1, 1, 1, 1, 3, 3, 3, 3],
+        }
+    )
+
+    ipf.save("save.ipf", df, itype=1, nodata=-999.0)
+    assert Path("save_l1.ipf").exists()
+    assert Path("save_l3.ipf").exists()
+    assert Path("A1.txt").exists()
+    assert Path("B2.txt").exists()
+    assert Path("C3.txt").exists()
+    assert Path("D4.txt").exists()
+    df2 = ipf.load("save_l*.ipf")
+    pd.testing.assert_frame_equal(df, df2, check_like=True)
+
+    remove("save_l1.ipf")
+    remove("save_l3.ipf")
+    remove("A1.txt")
+    remove("B2.txt")
+    remove("C3.txt")
+    remove("D4.txt")
