1419094d057f262c4c412972d5c96582afd1ce9b
Huite Bootsma <huitebootsma@gmail.com>
2018-12-06 11:04:15 +0100

    test_util: add tests for year 9999

--- tests/test_util.py
+++ tests/test_util.py
@@ -49,3 +49,31 @@ def test_decompose_dateonly():
     )
     assert isinstance(d, OrderedDict)
     assert d == refd
+
+
+def test_compose_year9999():
+    d = {
+        "name": "head",
+        "directory": Path("path", "to"),
+        "extension": ".idf",
+        "layer": 5,
+        "time": cftime.DatetimeProlepticGregorian(9999, 2, 22, 9, 6, 57),
+    }
+    path = util.compose(d)
+    assert isinstance(path, Path)
+    targetpath = Path(d["directory"], "head_99990222090657_l5.idf")
+    assert path == targetpath
+
+
+def test_decompose_dateonly_year9999():
+    d = util.decompose("99990222090657.idf")
+    refd = OrderedDict(
+        [
+            ("extension", ".idf"),
+            ("directory", Path(".")),
+            ("name", "99990222090657"),
+            ("time", cftime.DatetimeProlepticGregorian(9999, 2, 22, 9, 6, 57)),
+        ]
+    )
+    assert isinstance(d, OrderedDict)
+    assert d == refd
