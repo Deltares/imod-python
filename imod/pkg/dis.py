889c4cab9f8b5a7ce3adb5aff9ccdb84882f1470
Huite Bootsma <Huite.Bootsma@deltares.nl>
2019-04-14 16:03:19 +0200

    render for dis

--- imod/pkg/dis.py
+++ imod/pkg/dis.py
@@ -7,7 +7,7 @@ class TimeDiscretization(Package):
     _pkg_id = "dis"
 
     _dis_mapping = (
-        ("perlen", "duration"),
+        ("perlen", "timestep_duration"),
         ("nstp", "n_timesteps"),
         ("sstr",  "transient"),
     )
@@ -41,13 +41,43 @@ class TimeDiscretization(Package):
         self["time"] = time
         self["timestep_duration"] = timestep_duration
         self["n_timesteps"] = n_timesteps
-        self["flow_transient"] = transient
+        self["transient"] = transient
         self["flow_timestep_multiplier"] = timestep_multiplier
         self["transport_timestep_multiplier"] = transport_timestep_multiplier
         self["transport_initial_timestep"] = transport_initial_timestep
-    
-    def _render_dis(self):
-        pass  # Same method
-    
+
+    def _compose_values_time(self, key, globaltimes):
+        da = self[key]
+        values = {}
+
+        if "time" in da.coords:
+            # TODO: get working for cftime
+            package_times = [
+                pd.to_datetime(t) for t in np.atleast_1d(da.coords["time"].values)
+            ]
+
+        for timestep, globaltime in enumerate(globaltimes):
+            if "time" in da.coords:
+                # forward fill
+                # TODO: do smart forward fill using the colon notation
+                time = list(filter(lambda t: t <= globaltime, package_times))[-1]
+                # Offset 0 counting in Python, add one
+                values[timestep + 1] = da.isel(time=timestep).values[()]
+            else:
+                values["?"] = da.values[()]
+        return values
+
+    def _render_dis(self, globaltimes):
+        d = {}
+        datavars = [t[1] for t in self._dis_mapping]
+        for varname in datavars:
+            d[varname] = self._compose_values_time(varname, globaltimes)
+        return self._dis_template.render(d)
+
     def _render_btn(self):
-        pass  # Same method
+        d = {}
+        datavars = [t[1] for t in self._dis_mapping]
+        for varname in datavars:
+            d[varname] = self._compose_values_time(varname, globaltimes)
+        return self._btn_template.render(d)
+
