c33f03879655dc93f3499d837ee110fbe9544147
Huite Bootsma <Huite.Bootsma@deltares.nl>
2019-04-22 23:16:13 +0200

    Add some preprocessing utils

--- imod/pre/__init__.py
+++ imod/pre/__init__.py
@@ -1,5 +1,7 @@
 from imod.pre import reproject
 from imod.pre import regrid
+from imod.pre import spatial
+from imod.pre import subsoil
 
 from imod.pre.reproject import reproject
 from imod.pre.regrid import regrid
