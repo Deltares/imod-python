bd8b5d9c3bfb36bbd6460fc478f90e0aa1e1ee1e
Huite Bootsma <Huite.Bootsma@deltares.nl>
2019-04-22 12:09:21 +0200

    Backward compatibity: don't use decompose in ipf.save. Cf. idf.save.

--- imod/ipf.py
+++ imod/ipf.py
@@ -482,8 +482,10 @@ def save(path, df, itype=None, assoc_ext="txt", nodata=1.0e20):
         Writes files.
     """
 
-    d = util.decompose(path)
-    d["extension"] = ".ipf"
+    if isinstance(path, str):
+        path = pathlib.Path(path)
+
+    d = {"extension": ".ipf", "name": path.stem, "directory": path.parent}
     d["directory"].mkdir(exist_ok=True, parents=True)
 
     colnames = _lower(list(df))
