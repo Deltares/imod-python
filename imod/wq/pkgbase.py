2079c7e0034da1f7b13db3f0a18ccd4371d3d857
Joeri van Engelen <Joeri.vanEngelen@deltares.nl>
2019-05-02 12:14:58 +0200

    Formatted with black

--- imod/wq/pkgbase.py
+++ imod/wq/pkgbase.py
@@ -123,7 +123,9 @@ class Package(xr.Dataset):
         values = {}
 
         if "time" in da.coords:
-            package_times = [timeutil.to_datetime(t) for t in np.atleast_1d(da.coords["time"].values)]
+            package_times = [
+                timeutil.to_datetime(t) for t in np.atleast_1d(da.coords["time"].values)
+            ]
             globaltimes = [timeutil.to_datetime(t) for t in np.atleast_1d(globaltimes)]
 
             starts_ends = timeutil.forcing_starts_ends(package_times, globaltimes)
@@ -212,9 +214,11 @@ class BoundaryCondition(Package):
             da = self[varname]
 
         if "time" in da.coords:
-            package_times = [timeutil.to_datetime(t) for t in np.atleast_1d(da.coords["time"].values)]
+            package_times = [
+                timeutil.to_datetime(t) for t in np.atleast_1d(da.coords["time"].values)
+            ]
             globaltimes = [timeutil.to_datetime(t) for t in np.atleast_1d(globaltimes)]
-            
+
             starts_ends = timeutil.forcing_starts_ends(package_times, globaltimes)
 
             for itime, (time, start_end) in enumerate(zip(package_times, starts_ends)):
