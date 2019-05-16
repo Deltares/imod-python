3df44c66eeca1f74a80dc92d9b06f3702e1c7525
Niels Mulder <Niels.Mulder@deltares.nl>
2019-05-16 13:55:30 +0200

    Add import to init, create test file

---
+++ tests/test_visualize/test_spatial.py
@@ -0,0 +1,42 @@
+import os
+
+import numpy as np
+import pytest
+import xarray as xr
+import matplotlib.pyplot as plt
+
+import imod
+
+
+@pytest.fixture(scope="module")
+def example_legend(request):
+    legend_content = """
+
+    """
+    
+    with open("example_legend.leg", "w") as f:
+        f.write(legend_content)
+
+    def teardown():
+        try:
+            os.remove("example_legend.leg")
+        except FileNotFoundError:
+            pass
+
+    request.addfinalizer(teardown)
+
+
+def test_read_legend(example_legend):
+    
+    colors, levels = imod.visualize.spatial.read_imod_legend("example_legend.leg")
+    
+    assert colors == ["#0000000", "#000"]
+    assert levels == [1., 2.]
+
+
+def test_plot_map():
+    figure = imod.visualize.spatial.plot_map()
+
+    assert isinstance(figure, plt.figure)
+
+
