efe6119a983c67fd51d914e8013b62cc4fc1f0dc
Martijn Visser <mgvisser@gmail.com>
2019-06-12 16:18:17 +0200

    switch back test
    `test_decompose_steady_state` already tests this

--- tests/test_util.py
+++ tests/test_util.py
@@ -36,12 +36,12 @@ def test_decompose():
 
 
 def test_decompose_short_date():
-    d = util.decompose("path/to/head_steady-state_l5.idf")
+    d = util.decompose("path/to/head_20180222_l5.idf")
     refd = {
         "extension": ".idf",
         "directory": pathlib.Path("path", "to"),
         "name": "head",
-        "time": "steady-state",
+        "time": datetime.datetime(2018, 2, 22),
         "layer": 5,
     }
     assert isinstance(d, dict)
