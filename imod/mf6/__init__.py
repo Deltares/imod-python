dc572bebf1168768c2092db38b5feca3b4797eb5
Huite <huitebootsma@gmail.com>
2019-08-09 16:07:20 +0200

    Update mf6 __init__

--- imod/mf6/__init__.py
+++ imod/mf6/__init__.py
@@ -2,8 +2,22 @@
 Create a structured Modflow 6 model.
 """
 
-# make classes directly available under imod.mf6
-from imod.mf6.drn import Drainage
-from imod.mf6.timedis import TimeDiscretization
-from imod.mf6.pkgbase import Package, BoundaryCondition
+from imod.mf6.chd import ConstantHead
 from imod.mf6.dis import StructuredDiscretization
+from imod.mf6.drn import Drainage
+from imod.mf6.evt import Evapotranspiration
+from imod.mf6.ghb import GeneralHeadBoundary
+from imod.mf6.ic import InitialConditions
+from imod.mf6.ims import (
+    Solution,
+    SolutionPresetComplex,
+    SolutionPresetModerate,
+    SolutionPresetSimple,
+)
+from imod.mf6.model import Modflow6
+from imod.mf6.npf import NodePropertyFlow
+from imod.mf6.oc import OutputControl
+from imod.mf6.rch import Recharge
+from imod.mf6.simulation import Modflow6Simulation
+from imod.mf6.sto import Storage
+from imod.mf6.timedis import TimeDiscretization
