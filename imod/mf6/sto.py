e54eac0f08c5b7aa0a1a8bcd56d073e4fc9ecaa3
Huite <huitebootsma@gmail.com>
2019-08-09 11:07:36 +0200

    Add package identifier

--- imod/mf6/sto.py
+++ imod/mf6/sto.py
@@ -4,6 +4,8 @@ from imod.mf6.pkgbase import Package
 
 
 class Storage(Package):
+    _pkg_id = "sto"
+
     def __init__(self, specific_storage, specific_yield, transient, convertible):
         self["specific_storage"] = specific_storage
         self["specific_yield"] = specific_yield
