8ad3539f1c39fef32c617ab88fc7b501c7df3eba
Huite <huitebootsma@gmail.com>
2019-08-10 17:13:15 +0200

    Add options block to ic even if it's always empty(?)

--- imod/mf6/ic.py
+++ imod/mf6/ic.py
@@ -9,3 +9,8 @@ class InitialConditions(Package):
         super(__class__, self).__init__()
         self["head"] = head
         self._initialize_template()
+
+    def render(self, directory, pkgname, *args, **kwargs):
+        d = {}
+        d["layered"], d["strt"] = self._compose_values(self["head"], directory)
+        return self._template.render(d)
