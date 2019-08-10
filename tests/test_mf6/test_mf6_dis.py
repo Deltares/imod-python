20cee495ab9fbe6f997b5f9a75e07e447ad8f1d9
Huite <huitebootsma@gmail.com>
2019-08-10 16:49:33 +0200

    Test: dis

--- tests/test_mf6/test_mf6_dis.py
+++ tests/test_mf6/test_mf6_dis.py
@@ -9,40 +9,57 @@ import xarray as xr
 
 import imod
 
-exdir = pathlib.Path("d:/repo/imod/mf6-dist/examples/ex02-tidal-sprint")
 
-# from ex02-tidal
-xorigin = 0.0
-yorigin = 0.0
-angrot = 0.0
-nlay = 3
-nrow = 15
-ncol = 10
+def test_render():
+    nlay = 3
+    nrow = 15
+    ncol = 15
+    shape = (nlay, nrow, ncol)
 
-dx = 500.0
-dy = -500.0
-top = 50.0
+    dx = 5000.0
+    dy = -5000.0
+    xmin = 0.0
+    xmax = dx * ncol
+    ymin = 0.0
+    ymax = abs(dy) * nrow
+    dims = ("layer", "y", "x")
 
-# setup ibound
-idomain = xr.DataArray(
-    data=np.full((nlay, nrow, ncol), 1.0),
-    coords={
-        "y": np.arange(yorigin + 0.5 * dy, yorigin + nrow * dy, dy),
-        # "x": np.arange(xorigin + 0.5 * dx, xorigin + ncol * dx, dx),
-        # test nonequidistant
-        "x": np.cumsum(np.arange(ncol)),
-        "dx": ("x", np.arange(ncol)),
-        "layer": np.arange(nlay, dtype=np.int),
-    },
-    dims=("layer", "y", "x"),
-)
+    layer = np.array([1, 2, 3])
+    y = np.arange(ymax, ymin, dy) + 0.5 * dy
+    x = np.arange(xmin, xmax, dx) + 0.5 * dx
+    coords = {"layer": layer, "y": y, "x": x}
+    idomain = xr.DataArray(np.ones(shape), coords=coords, dims=dims)
+    bottom = xr.DataArray([-200.0, -350.0, -450.0], {"layer": layer}, ("layer",))
 
-bottom = xr.DataArray(
-    data=np.array([5.0, -10.0, -100.0]),
-    coords={"layer": np.arange(nlay, dtype=np.int)},
-    dims=("layer"),
-)
+    dis = imod.mf6.StructuredDiscretization(top=200.0, bottom=bottom, idomain=idomain)
+    actual = dis.render("mymodel", "dis")
+    expected = textwrap.dedent(
+        """\
+            begin options
+              xorigin 0.0
+              yorigin 0.0
+            end options
 
-dis = imod.mf6.StructuredDiscretization(idomain=idomain, top=top, bottom=bottom)
+            begin dimensions
+              nlay 3
+              nrow 15
+              ncol 15
+            end dimensions
 
-print(dis.render("mymodel", "mydis"))
+            begin griddata
+              delr
+                constant 5000.0
+              delc
+                constant 5000.0
+              top
+                constant 200.0
+              botm layered
+                constant -200.0
+                constant -350.0
+                constant -450.0
+              idomain
+                open/close mymodel/dis.bin (binary)
+            end griddata"""
+    )
+    assert actual == expected
+
