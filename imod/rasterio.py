0c62b60d2843aa0111cd4669c8e199975b473f6c
Huite <huitebootsma@gmail.com>
2019-08-20 14:02:19 +0200

    Start implementing nD read for rasterio

--- imod/rasterio.py
+++ imod/rasterio.py
@@ -6,9 +6,14 @@ raster formats.
 Currently only :func:`imod.rasterio.write` is implemented.
 """
 
+import collections
+import functools
+import glob
 import pathlib
+import re
 
 import numpy as np
+import xarray as xr
 
 # since rasterio is a big dependency that is sometimes hard to install
 # and not always required, we made this an optional dependency
@@ -93,3 +98,57 @@ def write(path, da, driver=None, nodata=np.nan):
     with rasterio.Env():
         with rasterio.open(path, "w", **profile) as ds:
             ds.write(dafilled.values, 1)
+
+
+def ndconcat(das, dims):
+    """
+    Parameters
+    ----------
+    das : dict (of dicts) of lists, n levels deep. Bottoms out at a list.
+        E.g. {2000-01-01: [da1, da2], 2001-01-01: [da3, da4]}
+        for n = 2.
+    dims : tuple
+        Tuple of dimensions over which to concatenate. Has to be n elements long.
+        E.g. ("time", "layer") for n = 2.
+
+    Returns
+    -------
+    concatenated : xr.DataArray
+        Input concatenated over n dimensions.
+    """
+    if len(dims) == 1:  # base case
+        return xr.concat(das, dim=dims[0])
+    else:
+        dims_in = dims[1:]  # recursive case
+        out = [ndconcat(das_in, dims_in) for das_in in das.values()]
+        return xr.concat(out, dims[0])
+
+
+def initialize_groupby(ndims):
+    # In explicit form, say we have ndims=4
+    # Then, writing it out, we get:
+    # a = partial(defaultdict, list)
+    # b = partial(defaultdict, a)
+    # c = partial(defaultdict, b)
+    # d = defaultdict(c)
+    # This can obviously be written as a for loop, as below.
+    d = collections.defaultdict(list)
+    if ndims == 1:
+        return d
+    else:
+        for _ in range(ndims - 1):
+            d = functools.partial(collections.defaultdict, d)
+        return d
+
+
+def read(path, use_cftime=False, pattern=None):
+    if isinstance(path, list):
+        return _read(path, use_cftime, pattern)
+    elif isinstance(path, pathlib.Path):
+        path = str(path)
+
+    paths = [pathlib.Path(p) for p in glob.glob(path)]
+    n = len(paths)
+    if n == 0:
+        raise FileNotFoundError(f"Could not find any files matching {path}")
+    return _read(paths, use_cftime, pattern)
