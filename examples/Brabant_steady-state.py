3db4d18e1e98143ca699b63aca0c7a78fa4a3d7a
Martijn Visser <mgvisser@gmail.com>
2019-09-03 13:43:21 +0200

    format

--- examples/Brabant_steady-state.py
+++ examples/Brabant_steady-state.py
@@ -25,11 +25,12 @@ def dataframe_to_dataset(df, like, k, i, j, **kwargs):
     k = df[k].values.astype(np.int32)
     i = df[i].values.astype(np.int32)
     j = df[j].values.astype(np.int32)
-    for key, value in kwargs.items(): 
+    for key, value in kwargs.items():
         da = xr.full_like(like, np.nan)
         da.values[k, i, j] = df[value].values.astype(np.float64)
         ds[key] = da.dropna("layer", how="all")
     return ds
+
 
 # Do data correction
 # Get rid of nodata
@@ -57,7 +58,7 @@ filler_bot = xr.full_like(bot, 1.0e-3).cumsum("layer") * -1.0
 bot = bot.combine_first(filler_bot)
 
 # Create a boolean array
-is_active = (idomain == 1)
+is_active = idomain == 1
 kv = c_thickness / c  # m/d
 
 kh["layer"].values = 2 * kh["layer"].values - 1
@@ -75,9 +76,7 @@ evenheads["layer"].values *= 2
 startingheads = xr.concat([startingheads, evenheads], dim="layer").sortby("layer")
 
 eroded = idomain.copy()
-eroded.values = scipy.ndimage.morphology.binary_erosion(
-        eroded.values, np.ones((3, 3))
-)
+eroded.values = scipy.ndimage.morphology.binary_erosion(eroded.values, np.ones((3, 3)))
 is_boundary = (eroded == 0) & is_active
 # Startingheads array already has layer dimension
 constanthead = startingheads.where(is_boundary)
@@ -86,10 +85,12 @@ idomain = xr.full_like(k, idomain, dtype=np.int32)
 
 wells = pd.read_csv("data/wells/sq_list.csv")
 well_layer = wells["ilay"].values * 2 - 1
-dict_xy = imod.select.points_indices(da=k, x=wells["xcoordinate"].values, y=wells["ycoordinate"].values)
+dict_xy = imod.select.points_indices(
+    da=k, x=wells["xcoordinate"].values, y=wells["ycoordinate"].values
+)
 well_row = dict_ij["y"]
 well_column = dict_ij["x"]
-well_active = idomain.values[well_layer -1, well_row, well_column] == 1
+well_active = idomain.values[well_layer - 1, well_row, well_column] == 1
 
 well_layer = well_layer[well_active]
 well_row = well_row[well_active] + 1
@@ -99,14 +100,14 @@ well_rate = wells["q_assigned"].values[well_active]
 
 df = pd.read_csv("data/mf2005/riv_data.csv")
 river = dataframe_to_dataset(
-        df=df,
-        like=kd,
-        k="k",
-        i="i",
-        j="j",
-        conductance="cond0",
-        stage="stage0",
-        bottom="rbot0",
+    df=df,
+    like=kd,
+    k="k",
+    i="i",
+    j="j",
+    conductance="cond0",
+    stage="stage0",
+    bottom="rbot0",
 )
 river = river.where(is_active)
 
@@ -116,16 +117,16 @@ river = river.where(is_active)
 
 df = pd.read_csv("data/mf2005/ghb_data.csv")
 generalheadboundary = []
-for i in range(4): # captures > 90% of boundary elements
+for i in range(4):  # captures > 90% of boundary elements
     duplicated = df.duplicated(subset=["k", "i", "j"], keep="first")
     ds = dataframe_to_dataset(
-            df=df[~duplicated],
-            like=kd,
-            k="k",
-            i="i",
-            j="j",
-            conductance="cond0",
-            head="bhead0",
+        df=df[~duplicated],
+        like=kd,
+        k="k",
+        i="i",
+        j="j",
+        conductance="cond0",
+        head="bhead0",
     )
     ds = ds.where(is_active)
     ds["layer"] = 2 * ds["layer"].values - 1
@@ -137,13 +138,13 @@ drainage = []
 for i in range(4):
     duplicated = df.duplicated(subset=["k", "i", "j"], keep="first")
     ds = dataframe_to_dataset(
-            df=df[~duplicated],
-            like=kd,
-            k="k",
-            i="i",
-            j="j",
-            conductance="cond0",
-            elevation="elev0",
+        df=df[~duplicated],
+        like=kd,
+        k="k",
+        i="i",
+        j="j",
+        conductance="cond0",
+        elevation="elev0",
     )
     ds = ds.where(is_active)
     ds["layer"] = 2 * ds["layer"].values - 1
@@ -156,28 +157,19 @@ recharge = recharge.assign_coords(layer=1).where(is_active)
 # Initialize and fill the model
 gwf_model = imod.mf6.GroundwaterFlowModel()
 gwf_model["dis"] = imod.mf6.StructuredDiscretization(
-    top=top.sel(layer=1).fillna(0.0),
-    bottom=bot,
-    idomain=idomain,
+    top=top.sel(layer=1).fillna(0.0), bottom=bot, idomain=idomain
 )
-gwf_model["npf"] = imod.mf6.NodePropertyFlow(
-    k=k,
-    k22=k,
-    k33=k33,
-    icelltype=0,
-)
+gwf_model["npf"] = imod.mf6.NodePropertyFlow(k=k, k22=k, k33=k33, icelltype=0)
 gwf_model["ic"] = imod.mf6.InitialConditions(head=startingheads)
 
 for i, ds in enumerate(drainage):
     gwf_model[f"drn-{i+1}"] = imod.mf6.Drainage(
-        elevation=ds["elevation"],
-        conductance=ds["conductance"],
+        elevation=ds["elevation"], conductance=ds["conductance"]
     )
 
 for i, ds in enumerate(generalheadboundary):
     gwf_model[f"ghb-{i+1}"] = imod.mf6.GeneralHeadBoundary(
-        head=ds["head"],
-        conductance=ds["conductance"],
+        head=ds["head"], conductance=ds["conductance"]
     )
 gwf_model["riv"] = imod.mf6.River(
     stage=river["stage"],
@@ -185,10 +177,7 @@ gwf_model["riv"] = imod.mf6.River(
     bottom_elevation=river["bottom"],
 )
 gwf_model["wel"] = imod.mf6.Well(
-    layer=well_layer,
-    row=well_row,
-    column=well_column,
-    rate=well_rate,
+    layer=well_layer, row=well_row, column=well_column, rate=well_rate
 )
 gwf_model["rch"] = imod.mf6.Recharge(rate=recharge, print_input=True)
 gwf_model["chd"] = imod.mf6.ConstantHead(head=constanthead)
@@ -197,7 +186,9 @@ gwf_model["oc"] = imod.mf6.OutputControl(save_head=True, save_budget=False)
 # Attach it to a simulation
 simulation = imod.mf6.Modflow6Simulation("Brabant-steady")
 simulation["gwf_1"] = gwf_model
-simulation["ims"] = imod.mf6.SolutionPresetSimple(print_option="summary", csv_output=False, no_ptc=True)
+simulation["ims"] = imod.mf6.SolutionPresetSimple(
+    print_option="summary", csv_output=False, no_ptc=True
+)
 
 # Determine time discretization
 simulation.time_discretization(starttime="2000-01-01", endtime="2000-01-02")
@@ -206,4 +197,3 @@ simulation.time_discretization(starttime="2000-01-01", endtime="2000-01-02")
 simulation.write("Brabant-steady")
 
 print("Done!")
-
