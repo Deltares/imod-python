Make coordinates determination independent of sign of cellsize along a dimension. Fixes #90.

imod/prepare/common.py
@@ -272,6 +272,7 @@ def _coord(da, dim):
             dxs = np.full(da[dim].size, dx)
         else:  # array -> non-equidistant
             dxs = dx
+        _check_monotonic(dxs, dim)
 
     else:  # undefined -> equidistant
         dxs = np.diff(da[dim].values)
@@ -284,8 +285,15 @@ def _coord(da, dim):
             )
         dxs = np.full(da[dim].size, dx)
 
-    _check_monotonic(dxs, dim)
-    x0 = da[dim][0] - 0.5 * dxs[0]
+    # Check if the sign of dxs is correct for the coordinate values of x
+    x = da[dim]
+    dxs = np.abs(dxs)
+    if x.size > 1:
+        if x[1] < x[0]:
+            dxs = -1.0 * dxs
+
+    # Note: this works for both positive dx (increasing x) and negative dx
+    x0 = x[0] - 0.5 * dxs[0]
     x = np.full(dxs.size + 1, x0)
     x[1:] += np.cumsum(dxs)
     return x

tests/test_regrid.py
@@ -244,7 +244,7 @@ def test_regrid_coord():
     regridx = imod.prepare.common._coord(da, "x")
     assert np.allclose(regridx, np.arange(0.0, -5.0, -1.0))
 
-    # Non-equidistant, postive dx, negative dy/n
+    # Non-equidistant, postive dx, negative dy
     nrow, ncol = 3, 4
     dx = np.array([0.9, 1.1, 0.8, 1.2])
     dy = np.array([-1.3, -0.7, -1.0])
@@ -263,6 +263,12 @@ def test_regrid_coord():
     assert float(regridy.max()) == ymax
     assert np.allclose(np.diff(regridx), dx)
     assert np.allclose(np.diff(regridy), dy)
+
+    # Now test it if dy doesn't have the right sign
+    # it should automatically infer it based on y instead.
+    da["dy"].values *= -1.0
+    regridy2 = imod.prepare.common._coord(da, "y")
+    assert np.allclose(regridy, regridy2)
 
 
 def test_regrid_mean1d():