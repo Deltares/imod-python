a8032c533a72ea7854b30f2bffb5337031e080b3
Tess op den Kelder <tessopdenkelder@hotmail.com>
2019-11-01 15:00:24 +0100

    added docs drn mf6

--- imod/mf6/drn.py
+++ imod/mf6/drn.py
@@ -2,6 +2,31 @@ from imod.mf6.pkgbase import BoundaryCondition
 
 
 class Drainage(BoundaryCondition):
+    """
+    The Drain package is used to simulate head-dependent flux boundaries.
+    https://water.usgs.gov/ogw/modflow/mf6io.pdf#page=67
+
+    Parameters
+    ----------
+    elevation: array of floats (xr.DataArray)
+        elevation of the drain. (elev)
+    conductance: array of floats (xr.DataArray)
+        is the conductance of the drain. (cond)
+    print_input: ({True, False}, optional)
+        keyword to indicate that the list of drain information will be written
+        to the listing file immediately after it is read. Default is False.
+    print_flows: ({True, False}, optional)
+        Indicates that the list of drain flow rates will be printed to the
+        listing file for every stress period time step in which “BUDGET PRINT”
+        is specified in Output Control. If there is no Output Control option and
+        PRINT FLOWS is specified, then flow rates are printed for the last time
+        step of each stress period. Default is False.
+    save_flows: ({True, False}, optional)
+        Indicates that drain flow terms will be written to the file specified
+        with “BUDGET FILEOUT” in Output Control. Default is False.
+    observations: [Not yet supported.]
+        Default is None.
+    """
 
     __slots__ = (
         "elevation",
