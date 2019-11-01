b89875b4ad641039c9a0b4879a528c067c867f01
Tess op den Kelder <tessopdenkelder@hotmail.com>
2019-11-01 15:11:11 +0100

    add recharge docs mf6

--- imod/mf6/rch.py
+++ imod/mf6/rch.py
@@ -2,6 +2,34 @@ from imod.mf6.pkgbase import BoundaryCondition
 
 
 class Recharge(BoundaryCondition):
+    """
+    Recharge Package.
+     Any number of RCH Packages can be specified for a single groundwater flow
+     model.
+     https://water.usgs.gov/water-resources/software/MODFLOW-6/mf6io_6.0.4.pdf#page=79
+
+    Parameters
+    ----------
+    rate: array of floats (xr.DataArray)
+        is the recharge flux rate (LT −1). This rate is multiplied inside the
+        program by the surface area of the cell to calculate the volumetric
+        recharge rate. A time-series name may be specified.
+    print_input: ({True, False}, optional)
+        keyword to indicate that the list of recharge information will be written
+        to the listing file immediately after it is read. Default is False.
+    print_flows: ({True, False}, optional)
+        Indicates that the list of recharge flow rates will be printed to the
+        listing file for every stress period time step in which “BUDGET PRINT”
+        is specified in Output Control. If there is no Output Control option and
+        PRINT FLOWS is specified, then flow rates are printed for the last time
+        step of each stress period. Default is False.
+    save_flows: ({True, False}, optional)
+        Indicates that recharge flow terms will be written to the file specified
+        with “BUDGET FILEOUT” in Output Control. Default is False.
+    observations: [Not yet supported.]
+        Default is None.
+    """
+
     __slots__ = ("rate", "print_input", "print_flows", "save_flows", "observations")
     _pkg_id = "rch"
     _binary_data = ("rate",)
