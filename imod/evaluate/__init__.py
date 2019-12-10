b3180d802807babc9700cac14b02fb5bd71b2744
jdelsman <joostdelsman@gmail.com>
2019-12-10 15:30:44 +0100

    reformat

--- imod/evaluate/__init__.py
+++ imod/evaluate/__init__.py
@@ -1,2 +1,5 @@
 from imod.evaluate.budget import facebudget
-from imod.evaluate.constraints import stability_constraint_wel, stability_constraint_advection
+from imod.evaluate.constraints import (
+    stability_constraint_wel,
+    stability_constraint_advection,
+)
