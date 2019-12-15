86b1e868720d9ff36b8809621ba861ee78a28175
Huite <huite.bootsma@deltares.nl>
2019-12-15 22:14:28 +0100

    cross section plot for non-equidistant data; add tests

--- imod/visualize/cross_sections.py
+++ imod/visualize/cross_sections.py
@@ -4,7 +4,7 @@ import numpy as np
 import imod
 
 
-def cross_section(da, layers=False, ax=None, **kwargs):
+def cross_section(da, layers=False, **kwargs):
     """
     Wraps matplotlib.pcolormesh to draw cross-sections, drawing cell boundaries
     accurately.
@@ -12,34 +12,65 @@ def cross_section(da, layers=False, ax=None, **kwargs):
     Parameters
     ----------
     da : xr.DataArray
-        Two dimensional DataArray containing data of the cross section. The
-        first dimension must be "layer", and the second dimension will be used
-        as the x-axis for the cross-section.
-        Coordinates "top" and "bottom" must be present.
+        Two dimensional DataArray containing data of the cross section. One
+        dimension must be "layer", and the second dimension will be used as the
+        x-axis for the cross-section.
+
+        Coordinates "top" and "bottom" must be present, and must have the same
+        dimensions.
     layers : boolean, optional
         Whether to draw lines separating the layers.
-    ax : matplotlib.ax, optional
-        Matplotlib axes object on which the cross-section will be drawn.
     **kwargs
-        Other optional keyword argument for matplotlib.pcolormesh.
+        Other optional keyword arguments for matplotlib.pcolormesh.
 
     Returns
     -------
-    None    
+    fig : matplotlib.figure
+    ax : matplotlig.ax
     """
+    if len(da.dims) != 2:
+        raise ValueError("DataArray must be 2D")
+    if "layer" not in da.dims:
+        raise ValueError('DataArray must contain dimension "layer"')
+    if "top" not in da.coords:
+        raise ValueError('DataArray must contain coordinate "top"')
+    if "bottom" not in da.coords:
+        raise ValueError('DataArray must contain coordinate "bottom"')
+    if len(da["top"].dims) != 2:
+        raise ValueError('"top" coordinate be 2D')
+    if len(da["bottom"].dims) != 2:
+        raise ValueError('"bottom" coordinate be 2D')
+
+    dims = tuple(da.dims)
+    if not dims[0] == "layer":
+        # Switch 'm around
+        dims = (dims[1], dims[0])
+
+    # Ensure dimensions are in the right order
+    da = da.transpose(*dims, transpose_coords=True)
     data = da.values
     xcoord = da.dims[1]
     dx, xmin, xmax = imod.util.coord_reference(da[xcoord])
-    if dx < 0.0:
-        dx = abs(dx)
-        data = data[:, ::-1]
+    if isinstance(dx, (int, float)):
+        if dx < 0.0:
+            dx = abs(dx)
+            data = data[:, ::-1]
+        X = np.arange(xmin + dx, xmax, dx)
+    else:  # assuming dx is an array, non-equidistant dx
+        if (dx < 0.0).all():
+            dx = abs(dx)
+            data = data[:, ::-1]
+        elif (dx > 0.0).all():
+            pass
+        else:
+            raise ValueError(f"{xcoord} is not monotonic")
+        X = (xmin + dx[0]) + dx[1:]
 
     Y = np.vstack([da["top"].isel(layer=0).values, da["bottom"].values])
     Y = np.repeat(Y, 2, 1)
     nodata = np.isnan(Y)
     Y[nodata] = 0.0
 
-    X = np.arange(xmin + dx, xmax, dx)
     X = np.hstack([xmin, np.repeat(X, 2), xmax])
     X = np.full_like(Y, X)
 
@@ -47,15 +78,10 @@ def cross_section(da, layers=False, ax=None, **kwargs):
     C = np.full((nrow - 1, ncol - 1), np.nan)
     C[:, 0::2] = data
 
-    if ax is not None:
-        ax.pcolormesh(X, Y, C, **kwargs)
-        if layers:
-            Y[nodata] = np.nan
-            for y in Y:
-                ax.step(x=X[0], y=y)
-    else:
-        plt.pcolormesh(X, Y, C, **kwargs)
-        if layers:
-            Y[nodata] = np.nan
-            for y in Y:
-                plt.step(x=X[0], y=y)
+    fig, ax = plt.subplots()
+    ax.pcolormesh(X, Y, C, **kwargs)
+    if layers:
+        Y[nodata] = np.nan
+        for y in Y:
+            ax.step(x=X[0], y=y)
+    return fig, ax
---
+++ tests/test_visualize/test_visualize_cross_sections.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import pytest
+
+import imod
+
+
+@pytest.fixture(scope="function")
+def testda():
+    def _testda(equidistant):
+        if equidistant:
+            coords = {"layer": [1, 2], "x": [1.0, 2.0, 3.0]}
+        else:
+            dx = np.arange(1.0, 4.0)
+            x = dx.cumsum() - 0.5 * dx
+            coords = {"layer": [1, 2], "x": x, "dx": ("x", dx)}
+
+        dims = ("layer", "x")
+        da = xr.DataArray(np.random.randn(2, 3), coords, dims)
+        top = xr.full_like(da, 1.0)
+        bottom = xr.full_like(da, 0.0)
+        da = da.assign_coords(top=top)
+        da = da.assign_coords(bottom=bottom)
+        return da
+
+    return _testda
+
+
+@pytest.mark.parametrize("equidistant", [True, False])
+@pytest.mark.parametrize("transposed", [True, False])
+@pytest.mark.parametrize("transpose_coords", [True, False])
+def test_plot_cross_section(testda, equidistant, transposed, transpose_coords):
+    da = testda(equidistant)
+    if transposed:
+        da = da.transpose("x", "layer", transpose_coords=transpose_coords)
+    fig, ax = imod.visualize.cross_section(da, layers=True)
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(ax, plt.Axes)
