43ed9818cd7d9addcdadab8c78d9dcc00b7385f2
Huite <huitebootsma@gmail.com>
2020-01-23 18:38:01 +0100

    Add docs, error messages

--- imod/visualize/pyvista.py
+++ imod/visualize/pyvista.py
@@ -1,3 +1,39 @@
+"""
+This module creates unstructured grids from DataArrays.
+Directly creating pyvista.UnstructuredGrids is advantageous for several reasons.
+
+1. Structured (modflow) grids are rectilinear, so the pyvista.RectilinearGrid might
+seem obvious. However, nodata values are also present, and have to removed using
+a .treshold(). This means constructing every cell, followed by throwing most away.
+This also returns an unstructured grid, and is slower.
+
+2. Cells are rectangular, and they jump in the z dimension from one cell to the
+other:
+      __
+  __ |2 |
+ |1 ||__|
+ |__|
+
+In case of a rectilinear grid, the z value on the edge between cell 1 and 2 is
+linearly interpolated, rather than making a jump. To create jumps, double the number
+of cells is required, with dummy cells in between with width 0. This is clearly
+wasteful.
+
+The grid is constructed with:
+offset, cells, cell_type, points, values
+
+As presented here:
+https://github.com/pyvista/pyvista/blob/master/examples/00-load/create-unstructured-surface.py
+
+* offset: start of each cell in the cells array.
+* cells: number of points in the cell, then point number; for every cell.
+* cell_type: integers, informing vtk of the geometry type.
+* points are the coordinates (x, y, z) for every cell corner.
+* values: are the values from the data array. Can generally by used for coloring.
+
+The methods below construct pyvista.UnstructuredGrids for voxel models (z1d),
+"layer models" (z3d), and two dimensional data (e.g. a DEM).
+"""
 import numba
 import numpy as np
 import pyvista as pv
@@ -9,6 +45,22 @@ from imod import util
 
 @numba.njit
 def _create_hexahedra_z1d(data, x, y, z):
+    """
+    Parameters
+    ----------
+    data : np.array of size (nz, ny, nx)
+    x : np.array of size nx + 1
+    y: np.array of size ny + 1
+    z : np.array of size (nz + 1)
+
+    Returns
+    -------
+    offset : np.array of int
+    cells : np.array of int
+    cell_type : np.array of vkt enum
+    points : np.array of float
+    values : np.array of float
+    """
     nz, ny, nx = data.shape
     # First pass: count valid values
     n = 0
@@ -135,14 +187,13 @@ def _create_hexahedra_z3d(data, x, y, z3d):
 
 
 @numba.njit
-def _create_plane_surface(data, x, y):
+def _create_plane_surface(data, x, y, z):
     """
     Parameters
     ----------
-    data : np.array of size (nz, ny, nx)
+    data : np.array of size (ny, nx)
     x : np.array of size nx + 1
     y: np.array of size ny + 1
-    z : np.array of size (nz + 1, ny, nx)
 
     Returns
     -------
@@ -198,9 +249,13 @@ def _create_plane_surface(data, x, y):
 
 def grid_3d(da):
     """
-    2.44 ms for filtering
-    1.64 ms for this one
-    Should be much more efficient with sparse data.
+    Constructs a 3D PyVista representation of a DataArray.
+    DataArrays should be two-dimensional or three-dimensional:
+
+    * 2D: dimensions should be ``{"y", "x"}``. E.g. a DEM.
+    * 3D: dimensions should be ``{"z", "y", "x"}``, for a voxel model.
+    * 3D: dimensions should be ``{"layer", "y", "x"}``, with coordinates
+        ``"top"({"layer", "y", "x"})`` and ``"bottom"({"layer", "y", "x"})``.
 
     Parameters
     ----------
@@ -208,7 +263,29 @@ def grid_3d(da):
 
     Returns
     -------
-    pyvista.RectilinearGrid or pyvista.StructuredGrid
+    pyvista.UnstructuredGrid
+
+    Examples
+    --------
+
+    >>> grid = imod.visualize.grid_3d(da)
+
+    To plot the grid, call the ``.plot()`` method.
+
+    >>> grid.plot()
+
+    Use ``.assign_coords`` to assign tops and bottoms to layer models:
+
+    >>> top = imod.idf.open("top*.idf")
+    >>> bottom = imod.idf.open("bot*.idf")
+    >>> kd = imod.idf.open("kd*.idf")
+    >>> kd = kd.assign_coords(top=(("layer", "y", "x"), top))
+    >>> kd = kd.assign_coords(bottom=(("layer", "y", "x"), bottom))
+    >>> grid = imod.visualize.grid_3d(kd)
+    >>> grid.plot()
+
+    Refer to the PyVista documentation on how to customize plots:
+    https://docs.pyvista.org/index.html
     """
     # x and y dimension
     dx, xmin, xmax, dy, ymin, ymax = util.spatial_reference(da)
@@ -218,20 +295,43 @@ def grid_3d(da):
         dy = np.full(da.y.size, dy)
     nx = da.coords["x"].size
     ny = da.coords["y"].size
-    # TODO: x and y increasing...
+    # TODO: Currently assuming dx positive, dy negative
     x = np.full(nx + 1, xmin)
-    y = np.full(ny + 1, ymin)
+    y = np.full(ny + 1, ymax)
     x[1:] += dx.cumsum()
     y[1:] += dy.cumsum()
+
     # z dimension
     if "top" in da.coords and "bottom" in da.coords:
-        ztop = da.coords["top"].transpose("layer", "y", "x", transpose_coords=True)
-        zbot = da.coords["bottom"].transpose("layer", "y", "x", transpose_coords=True)
+        if not len(da.shape) == 3:
+            raise ValueError(
+                'Coordinates "top" and "bottom" are present, but data is not 3D.'
+            )
+        if not set(da.dims) == {"layer", "y", "x"}:
+            raise ValueError(
+                'Coordinates "top" and "bottom" are present, only dimensions allowed are: '
+                '{"layer", "y", "x"}'
+            )
+        da = da.transpose("layer", "y", "x", transpose_coords=True)
+        ztop = da.coords["top"]
+        zbot = da.coords["bottom"]
         z3d = np.vstack([np.expand_dims(ztop.isel(layer=1).values, 0), zbot.values])
         offset, cells, cell_type, points, values = _create_hexahedra_z3d(
             da.values, x, y, z3d
         )
     elif "z" in da.coords:
+        if not len(da.shape) == 3:
+            raise ValueError('Coordinate "z" is present, but data is not 3D.')
+        if not (set(da.dims) == {"layer", "y", "x"} or set(da.dims) == {"z", "y", "x"}):
+            raise ValueError(
+                'Coordinate "z" is present, only dimensions allowed are: '
+                '{"layer", "y", "x"} or {"z", "y", "x"}'
+            )
+        if "z" not in dims:
+            da = da.transpose("layer", "y", "x", transpose_coords=True)
+        else:
+            da = da.transpose("z", "y", "x", transpose_coords=True)
+
         dz, zmin, zmax = util.coord_reference(da["z"])
         nz = da.coords["z"].size
         z = np.full(nz + 1, zmin)
@@ -241,11 +341,16 @@ def grid_3d(da):
         offset, cells, cell_type, points, values = _create_hexahedra_z1d(
             da.values, x, y, z
         )
-    else:  # surface plot
-        if not da.dims == ("y", "x"):
-            raise ValueError()
+    elif set(da.dims) == ("y", "x"):
+        da = da.transpose("y", "x", transpose_coords=True)
         offset, cells, cell_type, points, values = _create_plane_surface(
             da.values, x, y
+        )
+    else:
+        raise ValueError(
+            'Incorrect coordinates and/or dimensions: Neither "z" nor "top" and '
+            '"bottom" is present in the DataArray, but dimension are not {"y", "x"} '
+            " either."
         )
 
     grid = pv.UnstructuredGrid(offset, cells, cell_type, points)
