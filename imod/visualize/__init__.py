287f67db3b86eb00fbab0a529f936ea0f42ca929
Huite Bootsma <huite.bootsma@deltares.nl>
2020-01-30 00:05:26 +0100

    Coordinates, docstrings, planes, animation
    * This should take care of coordinates, whether ascending or descending: the vertices_coordinates function.
    *  Add docstring on internal functions.
    * Specify dtype where necessary so numba doesn't crash on float32
    * Create specialized subclass for StaticGridAnimation3D, where location of cells does not change over time.

--- imod/visualize/__init__.py
+++ imod/visualize/__init__.py
@@ -8,5 +8,10 @@ plots that may be useful to evaluate groundwater models. All
 """
 
 from imod.visualize.cross_sections import cross_section
-from imod.visualize.pyvista import Animation_3d, grid_3d, line_3d
+from imod.visualize.pyvista import (
+    GridAnimation3D,
+    grid_3d,
+    line_3d,
+    StaticGridAnimation3D,
+)
 from imod.visualize.spatial import plot_map, read_imod_legend
