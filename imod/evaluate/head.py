2a54bb892d2a5489cd1f6b2214fdb37dd5c4596f
jdelsman <joost.delsman@deltares.nl>
2020-04-16 13:26:08 +0000

    added gxg calculation

--- imod/evaluate/head.py
+++ imod/evaluate/head.py
@@ -1,4 +1,6 @@
 import xarray as xr
+import pandas as pd
+import numpy as np
 
 
 def convert_pointwaterhead_freshwaterhead(
@@ -47,3 +49,112 @@ def convert_pointwaterhead_freshwaterhead(
         freshwaterhead = freshwaterhead.bfill(dim="layer")
 
     return freshwaterhead
+
+
+def calculate_gxg(head, below_surfacelevel=False):
+    """Function to calculate GxG groundwater characteristics from head time series.
+
+    GLG and GHG (average lowest and average highest groundwater level respectively) are
+    calculated as the average of the three lowest (GLG) or highest (GHG) head values per
+    hydrological year (april - april), for head values measured at a semi-monthly frequency
+    (14th and 28th of every month). GVG (average spring groundwater level) is calculated as
+    the average of groundwater level on 28th of March, 14th and 28th of April. Supplied head 
+    values are resampled to the 14/28 frequency. Hydrological years without all 14/28 dates 
+    present are discarded.
+
+    *Requires bottleneck.*
+
+    Parameters
+    ----------
+    head : xr.DataArray of floats
+        Head relative to sea level, in m, or m below surface level if `` is set to True. 
+        Must be of dimensions (``time``, ``y``, ``x``).
+    below_surfacelevel : boolean
+        False (default) if heads are relative to sea level. If True, heads are taken as m
+        below surface level.
+
+    Returns
+    -------
+    gxg : xr.Dataset 
+        Dataset containing `glg`, average lowest head, `ghg`, average highest head`, `gvg`
+        average spring head.
+
+    Examples
+    --------
+    Load the heads, and calculate groundwater characteristics after the year 2000:
+
+    >>> import imod
+    >>> heads = imod.idf.open("heads*.idf").sel(time=heads.time.dt.year >= 2000)
+    >>> gxg = imod.evaluate.calculate_gxg(heads)
+
+    Transform to meters below surface level by substracting from surface level:
+
+    >>> surflevel = imod.idf.open("surfacelevel.idf")
+    >>> gxg = surflevel - gxg
+
+    Or calculate from groundwater level relative to surface level directly:
+
+    >>> gwl = surflevel - heads
+    >>> gxg = imod.evaluate.calculate_gxg(gwl, below_surfacelevel=True)
+
+    """
+    assert head.ndim == 3
+    assert "time" in head.dims
+    assert "x" in head.dims
+    assert "y" in head.dims
+
+    # reindex to GxG frequency date_range: every 14th and 28th of the month
+    d13 = pd.DateOffset(days=13)
+    dr_gxg = (
+        pd.date_range(
+            start=pd.Timestamp(head.time.values[0]) - d13,
+            end=pd.Timestamp(head.time.values[-1]) - d13,
+            freq="SMS",
+        )
+        + d13
+    )
+    da_gxg = head.reindex(time=dr_gxg, method="nearest")
+
+    # calculate LG3, HG3 per hydrological year
+    dr_hydroyear = pd.date_range(
+        start=dr_gxg[0] - pd.DateOffset(months=1),
+        end=dr_gxg[-1] + pd.DateOffset(months=1),
+        freq="AS-APR",
+    )
+    annual = da_gxg.groupby_bins("time", bins=dr_hydroyear)
+    perturb = np.random.random((24, *head.isel(time=0).shape)) * 1e-6
+    glg = []
+    ghg = []
+    gvg = []
+    years = []
+    for y, da in annual:
+        if len(da.time) == 24:  # only include full-years
+            da = da.load() + perturb  # perturb to assure integer ranks
+            dar = da.rank(dim="time")
+            maxrank = dar.max()  # to allow for nodata values within timeseries
+            lg3 = da.where(dar <= 3).mean(dim="time")
+            hg3 = da.where(dar > maxrank - 3).mean(dim="time")
+            vg3 = da.isel(time=[0, 1, -1]).mean(dim="time")
+            glg.append(lg3)
+            ghg.append(hg3)
+            gvg.append(vg3)
+            years.append(y)
+
+    # average
+    glg = xr.concat(glg, dim="time").mean(dim="time")
+    ghg = xr.concat(ghg, dim="time").mean(dim="time")
+    gvg = xr.concat(gvg, dim="time").mean(dim="time")
+
+    gxg = xr.Dataset()
+    if not below_surfacelevel:
+        gxg["glg"] = glg
+        gxg["ghg"] = ghg
+    else:
+        gxg["glg"] = ghg
+        gxg["ghg"] = glg
+    gxg["gvg"] = gvg
+
+    gxg = gxg.assign_attrs(
+        GxG_period=f"{years[0].left:%Y-%m-%d} - {years[-1].right:%Y-%m-%d}"
+    )
+    return gxg
--- tests/test_evaluate/test_head.py
+++ tests/test_evaluate/test_head.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xarray as xr
+import pandas as pd
 
 import imod
 
@@ -30,3 +31,59 @@ def test_convert_pointwaterhead_freshwaterhead_da():
     fwh = xr.full_like(da, 4.075)
     fwh2 = imod.evaluate.convert_pointwaterhead_freshwaterhead(pwh, dens, z)
     assert fwh.equals(fwh2.round(5))
+
+
+def test_calculate_gxg():
+    data = (np.ones((1, 2, 49)) * np.arange(49)).T
+    coords = {
+        "time": pd.date_range("2000-04-01", periods=49, freq="SMS"),
+        "y": [0.5, 1.5],
+        "x": [0.5],
+    }
+    dims = ("time", "y", "x")
+    da = xr.DataArray(data, coords, dims)
+
+    coords_ref = {"y": [0.5, 1.5], "x": [0.5]}
+    dims_ref = ("y", "x")
+    glg_ref = (1 + 2 + 3 + 25 + 26 + 27) / 6
+    ghg_ref = (22 + 23 + 24 + 46 + 47 + 48) / 6
+    attrs = {"GxG_period": "2000-04-01 - 2002-04-01"}
+    glg_ref = xr.DataArray(np.ones((2, 1)) * glg_ref, coords_ref, dims_ref, attrs=attrs)
+    ghg_ref = xr.DataArray(np.ones((2, 1)) * ghg_ref, coords_ref, dims_ref, attrs=attrs)
+
+    gxg = imod.evaluate.calculate_gxg(da, False).round(5)
+    assert gxg["glg"].round(5).equals(glg_ref)
+    assert gxg["ghg"].round(5).equals(ghg_ref)
+
+    gxg = imod.evaluate.calculate_gxg(0 - da, True).round(5)
+    assert gxg["glg"].round(5).equals(0 - glg_ref)
+    assert gxg["ghg"].round(5).equals(0 - ghg_ref)
+
+
+def test_calculate_gxg_nan():
+    data = (np.ones((1, 2, 49)) * np.arange(49)).T
+    data[:, 1, :] = np.nan
+    data[-1, 0, :] = np.nan
+    coords = {
+        "time": pd.date_range("2000-04-01", periods=49, freq="SMS"),
+        "y": [0.5, 1.5],
+        "x": [0.5],
+    }
+    dims = ("time", "y", "x")
+    da = xr.DataArray(data, coords, dims)
+
+    coords_ref = {"y": [0.5, 1.5], "x": [0.5]}
+    dims_ref = ("y", "x")
+    glg_ref = np.array([[(1 + 2 + 3 + 25 + 26 + 27) / 6], [np.nan]])
+    ghg_ref = np.array([[(22 + 23 + 24 + 45 + 46 + 47) / 6], [np.nan]])
+    attrs = {"GxG_period": "2000-04-01 - 2002-04-01"}
+    glg_ref = xr.DataArray(glg_ref, coords_ref, dims_ref, attrs=attrs)
+    ghg_ref = xr.DataArray(ghg_ref, coords_ref, dims_ref, attrs=attrs)
+
+    gxg = imod.evaluate.calculate_gxg(da, False).round(5)
+    assert gxg["glg"].round(5).equals(glg_ref)
+    assert gxg["ghg"].round(5).equals(ghg_ref)
+
+    gxg = imod.evaluate.calculate_gxg(0 - da, True).round(5)
+    assert gxg["glg"].round(5).equals(0 - glg_ref)
+    assert gxg["ghg"].round(5).equals(0 - ghg_ref)
