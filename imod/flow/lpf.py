92c36c3f675a9a776ca2aacc706121c179453879
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-03-29 16:15:13 +0200

    Clarified docstrings

--- imod/flow/lpf.py
+++ imod/flow/lpf.py
@@ -1,7 +1,8 @@
 from imod.flow.pkgbase import Package
 
 class HorizontalHydraulicConductivity(Package):
-    """Specify horizontal hydraulic conductivity. 
+    """Specify horizontal hydraulic conductivity of the aquifers. 
+
     Assigning this package to a model means you chose MODFLOW 2005's 
     "Layer Property Flow (LPF)" schematization. 
     Assigning packages of the Block Centered Flow (BCF) as well
@@ -21,7 +22,10 @@ class HorizontalHydraulicConductivity(Package):
         self.dataset["k_horizontal"] = k_horizontal
 
 class VerticalHydraulicConductivity(Package):
-    """Specify vertical hydraulic conductivity. 
+    """Specify vertical hydraulic conductivity for aquitards (between BOT and TOP)
+    To specify the vertical hydraulic conductivity for aquifers, 
+    use "VerticalAnisotropy" in combination with HorizontalHydraulicConductivity.
+    
     Assigning this package to a model means you chose MODFLOW 2005's
     "Layer Property Flow (LPF)" schematization. 
     Assigning packages of the Block Centered Flow (BCF) as well
@@ -41,7 +45,7 @@ class VerticalHydraulicConductivity(Package):
         self.dataset["k_vertical"] = k_vertical
 
 class VerticalAnistropy(Package):
-    """Specify the vertical anisotropy, defined as the 
+    """Specify the vertical anisotropy for aquifers, defined as the 
     vertical hydraulic conductivity over the horizontal 
     hydraulic conductivity. 
 
