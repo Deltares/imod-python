faf3f9bd88744e1612c3d7f3e033082fafaa077a
Huite Bootsma <huitebootsma@gmail.com>
2021-04-01 14:33:43 +0200

    Write test for gen format, move to a separate folder for namespacing

--- imod/data_formats/gen/gen.py
+++ imod/data_formats/gen/gen.py
@@ -1,19 +1,19 @@
-from enum import IntEnum
 import io
+import warnings
+from enum import IntEnum
 from itertools import accumulate
 from pathlib import Path
-import struct
 from typing import List, Optional, Union
-import warnings
 
 import geopandas as gpd
 import numba
 import numpy as np
 import pandas as pd
-from scipy.io import FortranFile, FortranFormattingError
 import shapely.geometry as sg
+from scipy.io import FortranFile, FortranFormattingError
 
 
+# https://mail.python.org/pipermail/python-dev/2008-January/076194.html
 def monkeypatch_method(cls):
     def decorator(func):
         setattr(cls, func.__name__, func)
@@ -44,17 +44,44 @@ def write_char_record(self, string: str):
     nb.tofile(self._fp)
 
 
-def circle(xy: np.ndarray) -> sg.Polygon:
-    radius = np.sqrt((xy[1] - xy[0]) ** 2)
+def from_circle(xy: np.ndarray) -> sg.Polygon:
+    radius = np.sqrt(np.sum((xy[1] - xy[0]) ** 2))
     return sg.Point(xy[0]).buffer(radius)
 
 
-def point(xy: np.ndarray) -> sg.Polygon:
+def from_point(xy: np.ndarray) -> sg.Polygon:
     return sg.Point(xy[0])
 
 
-def rectangle(xy: np.ndarray) -> sg.Polygon:
+def from_rectangle(xy: np.ndarray) -> sg.Polygon:
     return sg.box(xy[0, 0], xy[0, 1], xy[1, 0], xy[1, 1])
+
+
+def to_circle(geometry: sg.Polygon) -> (np.ndarray, int):
+    xy = np.array([geometry.centroid.coords[0], geometry.exterior.coords[0]])
+    return xy, 2
+
+
+def to_rectangle(geometry: sg.Polygon) -> (np.ndarray, int):
+    xy = np.array(geometry.exterior)
+    if (geometry.area / geometry.minimum_rotated_rectangle.area) < 0.999:
+        raise ValueError("Feature_type is rectangle, but geometry is not a rectangular")
+    # First and third vertex will give (left, right) and (lower, upper)
+    return xy[[0, 2]], 2
+
+
+def to_polygon(geometry: sg.Polygon) -> (np.ndarray, int):
+    xy = np.array(geometry.exterior)
+    return xy, xy.shape[0]
+
+
+def to_point(geometry: sg.Point) -> (np.ndarray, int):
+    return np.array(geometry), 1
+
+
+def to_line(geometry: sg.LineString) -> (np.ndarray, int):
+    xy = np.array(geometry)
+    return xy, xy.shape[0]
 
 
 # From the iMOD User Manual
@@ -66,32 +93,32 @@ POLYGON = 1025
 RECTANGLE = 1026
 POINT = 1027
 LINE = 1028
+MAX_NAME_WIDTH = 11
 
-# From gen to geopandas:
-GEN_TO_GEOM = {
-    CIRCLE: circle,
-    POLYGON: sg.Polygon,
-    RECTANGLE: rectangle,
-    POINT: point,
-    LINE: sg.LineString,
-}
-GEN_TO_NAME = {
+# Map integers to strings
+GENTYPE_TO_NAME = {
     CIRCLE: "circle",
     POLYGON: "polygon",
     RECTANGLE: "rectangle",
     POINT: "point",
     LINE: "line",
 }
-
-# From geopandas to gen:
-# Circles, rectangles are not present in geopandas
-GEOM_TO_GEN = {
+NAME_TO_GENTYPE = {v: k for k, v in GENTYPE_TO_NAME.items()}
+# From gen itype to shapely geometry:
+GENTYPE_TO_GEOM = {
+    CIRCLE: from_circle,
+    POLYGON: sg.Polygon,
+    RECTANGLE: from_rectangle,
+    POINT: from_point,
+    LINE: sg.LineString,
+}
+# Infer gentype on the basis of shapely type
+GEOM_TO_GENTYPE = {
     sg.Polygon: POLYGON,
     sg.Point: POINT,
     sg.LineString: LINE,
 }
-NAME_TO_GEN = {k: v for k, v in GEN_TO_NAME.items()}
-# So a circle, rectangle is instead defined by the feature_type column:
+# Checking names with actual geometry types
 NAME_TO_GEOM = {
     "circle": sg.Polygon,
     "rectangle": sg.Polygon,
@@ -99,18 +126,27 @@ NAME_TO_GEOM = {
     "point": sg.Point,
     "line": sg.LineString,
 }
+GENTYPE_TO_VERTICES = {
+    CIRCLE: to_circle,
+    RECTANGLE: to_rectangle,
+    POLYGON: to_polygon,
+    POINT: to_point,
+    LINE: to_line,
+}
 
 
 def read(path: Union[str, Path]) -> gpd.GeoDataFrame:
     with warnings.catch_warnings(record=True):
-        warnings.filterwarnings("ignore", message="Given a dtype which is not unsigned.")
+        warnings.filterwarnings(
+            "ignore", message="Given a dtype which is not unsigned."
+        )
         with FortranFile(path, mode="r", header_dtype=HEADER_TYPE) as f:
             f.read_reals(dtype=FLOAT_TYPE)  # Skip the bounding box
             n_feature, n_column = f.read_ints(dtype=INT_TYPE)
             if n_column > 0:
                 widths = f.read_ints(dtype=INT_TYPE)
-                indices = [0] + list(accumulate(widths))
-                string = f.read_char_record()
+                indices = range(0, (n_column + 1) * MAX_NAME_WIDTH, MAX_NAME_WIDTH)
+                string = f.read_char_record()  # pylint:disable=no-member
                 names = [string[i:j].strip() for i, j in zip(indices[:-1], indices[1:])]
 
             xy = []
@@ -120,7 +156,7 @@ def read(path: Union[str, Path]) -> gpd.GeoDataFrame:
                 _, ftype = f.read_ints(dtype=INT_TYPE)
                 feature_type[i] = ftype
                 if n_column > 0:
-                    rows.append(f.read_char_record())
+                    rows.append(f.read_char_record())  # pylint:disable=no-member
                 f.read_reals(dtype=FLOAT_TYPE)  # skip the bounding box
                 xy.append(f.read_reals(dtype=FLOAT_TYPE).reshape((-1, 2)))
 
@@ -132,63 +168,41 @@ def read(path: Union[str, Path]) -> gpd.GeoDataFrame:
         )
     else:
         df = pd.DataFrame()
-    df["type"] = feature_type
-    df["type"] = df["type"].replace(GEN_TO_NAME)
+    df["feature_type"] = feature_type
+    df["feature_type"] = df["feature_type"].replace(GENTYPE_TO_NAME)
 
     geometry = []
     for ftype, geom in zip(feature_type, xy):
-        geometry.append(GEN_TO_GEOM[ftype](geom))
+        geometry.append(GENTYPE_TO_GEOM[ftype](geom))
 
     return gpd.GeoDataFrame(df, geometry=geometry)
 
 
-def polygon_to_circle(geometry: sg.Polygon) -> (np.ndarray, int):
-    xy = np.array(geometry.exterior)
-    center = np.mean(xy, axis=0)
-    xy = np.array([center, xy[0]])
-    return xy, 2
-
-
-def polygon_to_rectangle(geometry: sg.Polygon) -> (np.ndarray, int):
-    xy = np.array(geometry.exterior)
-    if (geometry.area / geometry.minimum_rotated_rectangle.area) < 0.999:
-        raise ValueError("Feature_type is rectangle, but geometry is not a rectangular")
-    return xy[:2], 2
-
-
 def vertices(geometry: Union[sg.Point, sg.Polygon, sg.LineString], ftype: str):
-    # Start by checking whether the feature type matches the geometry
     if ftype != "":
-        expected = NAME_TO_GEOM[ftype]
+        # Start by checking whether the feature type matches the geometry
+        try:
+            expected = NAME_TO_GEOM[ftype]
+        except KeyError as e:
+            raise ValueError(
+                f"feature_type should be one of {set(NAME_TO_GEOM.keys())}. Got instead {ftype}"
+            ) from e
         if not isinstance(geometry, expected):
             raise ValueError(
                 f"Feature type is {ftype}, expected {expected}. Got instead: {type(geometry)}"
             )
-        ftype = NAME_TO_GEN[ftype]
+        ftype: int = NAME_TO_GENTYPE[ftype]
     else:
-        ftype = GEOM_TO_GEN[type(geometry)]
+        try:
+            ftype: int = GEOM_TO_GENTYPE[type(geometry)]
+        except KeyError as e:
+            raise TypeError(
+                "Geometry type not allowed. Should be Polygon, Linestring, or Point."
+                f" Got {type(geometry)} instead."
+            ) from e
 
-    if ftype == CIRCLE:
-        xy, n_vertex = polygon_to_circle(geometry)
-    elif ftype == RECTANGLE:
-        xy, n_vertex = polygon_to_rectangle(geometry)
-
-    if isinstance(geometry, sg.Polygon):
-        xy = np.array(geometry.exterior)
-        n_vertex = xy.shape[0]
-    elif isinstance(geometry, sg.LineString):
-        xy = np.array(geometry)
-        n_vertex = xy.shape[0]
-    elif isinstance(geometry, sg.Point):
-        xy = np.array(geometry)
-        n_vertex = 1
-    else:
-        raise TypeError(
-            "Geometry type not allowed. Should be Polygon, Linestring, or Point."
-            f" Got {type(geometry)} instead."
-        )
-
-    return ftype, n_vertex, xy
+    xy, n_vertex = GENTYPE_TO_VERTICES[ftype](geometry)
+    return ftype, xy, n_vertex
 
 
 def write(
@@ -197,12 +211,12 @@ def write(
     feature_type: Optional[str] = None,
 ) -> None:
     df = pd.DataFrame(geodataframe.drop(columns="geometry")).astype("string")
-    n_feature, n_column = df.shape
     if feature_type is not None:
         types = df.pop(feature_type).str.lower()
     else:  # Create a dummy iterator
-        types = ("" for i in range(n_feature))
+        types = ("" for i in range(len(df)))
 
+    n_feature, n_column = df.shape
     # Truncate column names to 11 chars, then make everything at least 11 chars
     column_names = "".join([c[:11].ljust(11) for c in df])
     # Get the widths of the columns. Make room for at least 11
@@ -213,19 +227,21 @@ def write(
         widths.append(width)
 
     with warnings.catch_warnings(record=True):
-        warnings.filterwarnings("ignore", message="Given a dtype which is not unsigned.")
+        warnings.filterwarnings(
+            "ignore", message="Given a dtype which is not unsigned."
+        )
         with FortranFile(path, mode="w", header_dtype=HEADER_TYPE) as f:
             f.write_record(geodataframe.total_bounds.astype(FLOAT_TYPE))
             f.write_record(np.array([n_feature, n_column], dtype=INT_TYPE))
             if n_column > 0:
                 f.write_record(np.array(widths).astype(INT_TYPE))
-                f.write_char_record(column_names)
-            for geometry, (i, row), ftype in zip(
+                f.write_char_record(column_names)  # pylint:disable=no-member
+            for geometry, (_, row), ftype in zip(
                 geodataframe.geometry, df.iterrows(), types
             ):
-                ftype, n_vertex, xy = vertices(geometry, ftype)
+                ftype, xy, n_vertex = vertices(geometry, ftype)
                 f.write_record(np.array([n_vertex, ftype], dtype=INT_TYPE))
                 if n_column > 0:
-                    f.write_char_record("".join(row.values))
+                    f.write_char_record("".join(row.values))  # pylint:disable=no-member
                 f.write_record(np.array(geometry.bounds).astype(FLOAT_TYPE))
                 f.write_record(xy.astype(FLOAT_TYPE))
