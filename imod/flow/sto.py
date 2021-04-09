ba3891882d15d39e11ae8c5cebd951f39bddec2e
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-09 17:38:29 +0200

    Included specific storage package as discussed verbally.

--- imod/flow/sto.py
+++ imod/flow/sto.py
@@ -1,9 +1,8 @@
 from imod.flow.pkgbase import Package
 
-
 class StorageCoefficient(Package):
     """
-    Define the storage coefficient.
+    Storage coefficient [-].
     Be careful, this is not the same as the specific storage.
 
     From wikipedia (https://en.wikipedia.org/wiki/Specific_storage):
@@ -20,7 +19,7 @@ class StorageCoefficient(Package):
 
     Parameters
     ----------
-    storage_coefficient : xr.DataArray
+    storage_coefficient : float or xr.DataArray
         Storage coefficient, dims = ("layer", "y", "x").
 
     """
@@ -31,3 +30,26 @@ class StorageCoefficient(Package):
     def __init__(self, storage_coefficient):
         super(__class__, self).__init__()
         self.dataset["storage_coefficient"] = storage_coefficient
+
+class SpecificStorage(Package):
+    """
+    Specific storage [L-1].
+    Be careful, this is not the same as the storage coefficient.
+
+    From wikipedia (https://en.wikipedia.org/wiki/Specific_storage):
+    The specific storage is the amount of water that a portion 
+    of an aquifer releases from storage, per unit mass or volume of aquifer, 
+    per unit change in hydraulic head, while remaining fully saturated.
+
+    Parameters
+    ----------
+    specific_storage : float or xr.DataArray
+        Specific storage, dims = ("layer", "y", "x").
+    """
+
+    _pkg_id = "ssc"
+    _variable_order = ["specific_storage"]
+
+    def __init__(self, specific_storage):
+        super(__class__, self).__init__()
+        self.dataset["specific_storage"] = specific_storage
