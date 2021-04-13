0434e5e65491a5871b22f5ef97977449e70ee63d
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-13 14:24:09 +0200

    Refactored tests and added starting head test

---
+++ tests/fixtures/flow_basic_fixture.py
@@ -0,0 +1,69 @@
+import xarray as xr
+import pytest
+import numpy as np
+import pandas as pd
+
+@pytest.fixture(scope="module")
+def basic_dis():
+    """Basic model discretization"""
+
+    shape = nlay, nrow, ncol = 3, 9, 9
+
+    dx = 10.0
+    dy = -10.0
+    dz = np.array([5, 30, 100])
+    xmin = 0.0
+    xmax = dx * ncol
+    ymin = 0.0
+    ymax = abs(dy) * nrow
+    dims = ("layer", "y", "x")
+
+    layer = np.arange(1, nlay + 1)
+    y = np.arange(ymax, ymin, dy) + 0.5 * dy
+    x = np.arange(xmin, xmax, dx) + 0.5 * dx
+    coords = {"layer": layer, "y": y, "x": x}
+
+    ibound = xr.DataArray(np.ones(shape), coords=coords, dims=dims)
+
+    surface = 0.0
+    interfaces = np.insert((surface - np.cumsum(dz)), 0, surface)
+
+    bottom = xr.DataArray(interfaces[1:], coords={"layer": layer}, dims="layer")
+    top = xr.DataArray(interfaces[:-1], coords={"layer": layer}, dims="layer")
+
+    return ibound, top, bottom
+
+
+@pytest.fixture(scope="module")
+def three_days():
+    """Simple time discretization of three days"""
+
+    return pd.date_range(start='1/1/2018', end='1/3/2018', freq="D")
+
+
+@pytest.fixture(scope="module")
+def get_render_dict():
+    """
+    Helper function to return dict to render.
+
+    Fixture returns local helper function, so that the helper function
+    is only evaluated when called.
+    See: https://stackoverflow.com/a/51389067
+    """
+
+    def _get_render_dict(
+        package, directory, globaltimes, nlayer, composition=None, system_index=1
+        ):
+        
+        composition = package.compose(
+            directory, globaltimes, nlayer, composition=composition, system_index=system_index
+            )
+        
+        return dict(
+            pkg_id=package._pkg_id,
+            name=package.__class__.__name__,
+            variable_order=package._variable_order,
+            package_data=composition[package._pkg_id]
+        )
+
+    return _get_render_dict
--- tests/test_flow/test_flow_bas.py
+++ tests/test_flow/test_flow_bas.py
@@ -1,92 +1,25 @@
-from imod.flow import Top, Bottom, Boundary
-import pytest
-import numpy as np
-import xarray as xr
+from imod.flow import Top, Bottom, Boundary, StartingHead
+import pathlib
+import os
 
-
-@pytest.fixture(scope="module")
-def basic():
-    """Create basic model discretization"""
-
-    shape = nlay, nrow, ncol = 3, 9, 9
-
-    dx = 10.0
-    dy = -10.0
-    dz = np.array([5, 30, 100])
-    xmin = 0.0
-    xmax = dx * ncol
-    ymin = 0.0
-    ymax = abs(dy) * nrow
-    dims = ("layer", "y", "x")
-
-    layer = np.arange(1, nlay + 1)
-    y = np.arange(ymax, ymin, dy) + 0.5 * dy
-    x = np.arange(xmin, xmax, dx) + 0.5 * dx
-    coords = {"layer": layer, "y": y, "x": x}
-
-    ibound = xr.DataArray(np.ones(shape), coords=coords, dims=dims)
-
-    surface = 0.0
-    interfaces = np.insert((surface - np.cumsum(dz)), 0, surface)
-
-    bottom = xr.DataArray(interfaces[1:], coords={"layer": layer}, dims="layer")
-    top = xr.DataArray(interfaces[:-1], coords={"layer": layer}, dims="layer")
-
-    return ibound, top, bottom
-
-
-@pytest.fixture(scope="module")
-def get_package_attributes():
-    """Helper function to return relevant package attributes
-    for rendering as dictionary.
-
-    Fixture returns local helper function, so that the helper function
-    is only evaluated when called.
-    See: https://stackoverflow.com/a/51389067
-    """
-
-    def _get_package_attributes(package):
-        return dict(
-            pkg_id=package._pkg_id,
-            name=package.__class__.__name__,
-            variable_order=package._variable_order,
-        )
-
-    return _get_package_attributes
-
-
-@pytest.fixture(scope="module")
-def get_render_dict(get_package_attributes):
-    """
-    Helper function to return dict to render.
-
-    Fixture returns local helper function, so that the helper function
-    is only evaluated when called.
-    See: https://stackoverflow.com/a/51389067
-    """
-
-    def _get_render_dict(package):
-        composition_args = (".", None, 3)
-        composition = package.compose(*composition_args)
-        to_render = get_package_attributes(package)
-        to_render["package_data"] = composition[package._pkg_id]
-        return to_render
-
-    return _get_render_dict
-
-
-def test_boundary(basic, get_render_dict):
-    ibound, _, _ = basic
+def test_boundary(basic_dis, get_render_dict):
+    # Resolve in advance, so that comparisons have the same directory
+    # See e.g. https://github.com/omarkohl/pytest-datafiles/issues/6
+    directory = str(pathlib.Path(".").resolve())
+    
+    ibound, _, _ = basic_dis
     boundary = Boundary(ibound=ibound)
-    to_render = get_render_dict(boundary)
-    to_render["n_entry"] = 3
+    nlayer = len(boundary["layer"])
+    
+    to_render = get_render_dict(boundary, directory, None, nlayer)
+    to_render["n_entry"] = nlayer
 
     compare = (
         "0001, (bnd), 1, Boundary, ['ibound']\n"
         "001, 003\n"
-        "1, 2, 001, 1.000, 0.000, -9999., ibound_l1.idf\n"
-        "1, 2, 002, 1.000, 0.000, -9999., ibound_l2.idf\n"
-        "1, 2, 003, 1.000, 0.000, -9999., ibound_l3.idf"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}ibound_l1.idf\n"
+        f"1, 2, 002, 1.000, 0.000, -9999., {directory}{os.sep}ibound_l2.idf\n"
+        f"1, 2, 003, 1.000, 0.000, -9999., {directory}{os.sep}ibound_l3.idf"
     )
 
     rendered = boundary._render_projectfile(**to_render)
@@ -94,11 +27,13 @@ def test_boundary(basic, get_render_dict):
     assert rendered == compare
 
 
-def test_top(basic, get_render_dict):
-    _, top, _ = basic
+def test_top(basic_dis, get_render_dict):
+    _, top, _ = basic_dis
     top = Top(top=top)
-    to_render = get_render_dict(top)
-    to_render["n_entry"] = 3
+    nlayer = len(top["layer"])
+    
+    to_render = get_render_dict(top, ".", None, nlayer)
+    to_render["n_entry"] = nlayer
 
     compare = (
         "0001, (top), 1, Top, ['top']\n"
@@ -113,11 +48,13 @@ def test_top(basic, get_render_dict):
     assert rendered == compare
 
 
-def test_bot(basic, get_render_dict):
-    _, _, bottom = basic
+def test_bot(basic_dis, get_render_dict):
+    _, _, bottom = basic_dis
     bottom = Bottom(bottom=bottom)
-    to_render = get_render_dict(bottom)
-    to_render["n_entry"] = 3
+    nlayer = len(bottom["layer"])
+    
+    to_render = get_render_dict(bottom, ".", None, nlayer)
+    to_render["n_entry"] = nlayer
 
     compare = (
         "0001, (bot), 1, Bottom, ['bottom']\n"
@@ -128,5 +65,29 @@ def test_bot(basic, get_render_dict):
     )
 
     rendered = bottom._render_projectfile(**to_render)
+
+    assert rendered == compare
+
+def test_starting_head(basic_dis, get_render_dict):
+    # Resolve in advance, so that comparisons have the same directory
+    # See e.g. https://github.com/omarkohl/pytest-datafiles/issues/6
+    directory = str(pathlib.Path(".").resolve())
+    
+    ibound, _, _ = basic_dis
+    starting_head = StartingHead(starting_head=ibound)
+    nlayer = len(starting_head["layer"])
+    
+    to_render = get_render_dict(starting_head, directory, None, nlayer)
+    to_render["n_entry"] = nlayer
+
+    compare = (
+        "0001, (shd), 1, StartingHead, ['starting_head']\n"
+        "001, 003\n"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}starting_head_l1.idf\n"
+        f"1, 2, 002, 1.000, 0.000, -9999., {directory}{os.sep}starting_head_l2.idf\n"
+        f"1, 2, 003, 1.000, 0.000, -9999., {directory}{os.sep}starting_head_l3.idf"
+    )
+
+    rendered = starting_head._render_projectfile(**to_render)
 
     assert rendered == compare
