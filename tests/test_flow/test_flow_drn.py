7925c2987e6a587bf38eed8f7afb10e9ada3057b
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-15 13:44:29 +0200

    Added drain test

---
+++ tests/test_flow/test_flow_drn.py
@@ -0,0 +1,74 @@
+from imod.flow import Drain
+import numpy as np
+import xarray as xr
+import os
+
+import pathlib
+import pytest
+
+
+@pytest.fixture(scope="module")
+def drain(basic_dis, three_days):
+    ibound, _, _ = basic_dis
+    x = ibound.x.values
+
+    times = three_days
+
+    # Boundary_conditions
+    trend = np.ones(times.shape)
+    trend = np.cumsum(trend)
+
+    elevation = ibound.where(ibound.x.isin([x[0], x[-1]]))
+    elevation = xr.DataArray(trend, coords={"time": times}, dims=["time"]) * elevation
+
+    return Drain(conductance=10.0, elevation=elevation)
+
+
+def test_drain(get_render_dict, drain, three_days):
+    # Resolve in advance, so that comparisons have the same directory
+    # See e.g. https://github.com/omarkohl/pytest-datafiles/issues/6
+    directory = str(pathlib.Path(".").resolve())
+
+    nlayer = len(drain["layer"])
+    times = three_days
+
+    time_composed = {
+        "1": "2018-01-01 00:00:00",
+        "2": "2018-01-02 00:00:00",
+        "3": "2018-01-03 00:00:00",
+    }
+
+    to_render = get_render_dict(drain, directory, times, nlayer)
+    to_render["n_entry"] = nlayer
+    to_render["times"] = time_composed
+
+    compare = (
+        "0003, (drn), 1, Drain, ['conductance', 'elevation']\n"
+        "2018-01-01 00:00:00\n"
+        "002, 003\n"
+        f'1, 1, 001, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 002, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 003, 1.000, 0.000, 10.0, ""\n'
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180101000000_l1.idf\n"
+        f"1, 2, 002, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180101000000_l2.idf\n"
+        f"1, 2, 003, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180101000000_l3.idf\n"
+        "2018-01-02 00:00:00\n"
+        "002, 003\n"
+        f'1, 1, 001, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 002, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 003, 1.000, 0.000, 10.0, ""\n'
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180102000000_l1.idf\n"
+        f"1, 2, 002, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180102000000_l2.idf\n"
+        f"1, 2, 003, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180102000000_l3.idf\n"
+        "2018-01-03 00:00:00\n"
+        "002, 003\n"
+        f'1, 1, 001, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 002, 1.000, 0.000, 10.0, ""\n'
+        f'1, 1, 003, 1.000, 0.000, 10.0, ""\n'
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180103000000_l1.idf\n"
+        f"1, 2, 002, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180103000000_l2.idf\n"
+        f"1, 2, 003, 1.000, 0.000, -9999., {directory}{os.sep}elevation_20180103000000_l3.idf"
+    )
+    rendered = drain._render_projectfile(**to_render)
+
+    assert compare == rendered
