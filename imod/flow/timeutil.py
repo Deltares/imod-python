0cb1a3be224852eea316afbf835f0385d298df5e
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-16 09:55:52 +0200

    Conversion to list only if iterable of times

--- imod/flow/timeutil.py
+++ imod/flow/timeutil.py
@@ -9,6 +9,16 @@ contain the function in PkgGroups felt out of place.
 """
 
 import numpy as np
+import cftime
+
+
+def _to_list(t):
+    """Helper function to catch packages that have only one time step"""
+
+    if isinstance(t, np.datetime64) or isinstance(t, cftime.datetime):
+        return [t]
+    else:
+        return list(t)
 
 
 def insert_unique_package_times(package_mapping, times):
@@ -32,7 +42,7 @@ def insert_unique_package_times(package_mapping, times):
     first_times = {}
     for key, pkg in package_mapping:
         if pkg._hastime():
-            pkgtimes = list(pkg["time"].values)
+            pkgtimes = _to_list(pkg["time"].values)
             first_times[key] = sorted(pkgtimes)[0]
             for var in pkg.dataset.data_vars:
                 if "timemap" in pkg[var].attrs:
