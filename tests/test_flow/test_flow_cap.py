3c0449616abe8a096d5f3ad5b66809e8e24dc5af
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-21 15:42:23 +0200

    Added metaswap tests

---
+++ tests/test_flow/test_flow_cap.py
@@ -0,0 +1,141 @@
+from imod.flow import MetaSwap
+import pathlib
+import pytest
+import os
+from copy import deepcopy
+
+
+@pytest.fixture(scope="module")
+def metaswap(basic_dis):
+    ibound, _, _ = basic_dis
+
+    active = ibound.isel(layer=0)
+
+    d = {}
+    d["boundary"] = active
+    d["landuse"] = active
+    d["rootzone_thickness"] = 1.2
+    d["soil_physical_unit"] = active
+    d["meteo_station_number"] = active
+    d["surface_elevation"] = 0.0
+    d["sprinkling_type"] = active
+    d["sprinkling_layer"] = active
+    d["sprinkling_capacity"] = 1000.0
+    d["wetted_area"] = 30.0
+    d["urban_area"] = 30.0
+    d["ponding_depth_urban"] = 0.02
+    d["ponding_depth_rural"] = 0.005
+    d["runoff_resistance_urban"] = 1.5
+    d["runoff_resistance_rural"] = 1.5
+    d["runon_resistance_urban"] = 1.5
+    d["runon_resistance_rural"] = 1.5
+    d["infiltration_capacity_urban"] = 10.0
+    d["infiltration_capacity_rural"] = 2.0
+    d["perched_water_table"] = 0.5
+    d["soil_moisture_factor"] = 1.0
+    d["conductivity_factor"] = 1.0
+
+    d["extra_files"] = [
+        "fact_svat.inp",
+        "luse_svat.inp",
+        "mete_grid.inp",
+        "para_sim.inp",
+        "tiop_sim.inp",
+        "init_svat.inp",
+        "comp_post.inp",
+        "sel_key_svat_per.inp",
+    ]
+
+    return MetaSwap(**d)
+
+
+def test_metaswap_render(metaswap, get_render_dict):
+    # See e.g. https://github.com/omarkohl/pytest-datafiles/issues/6
+    directory = str(pathlib.Path(".").resolve())
+
+    metaswap.extra_files = [
+        os.path.join(directory, file) for file in metaswap.extra_files
+    ]
+
+    nlayer = 1
+    to_render = get_render_dict(metaswap, directory, None, nlayer)
+    to_render["n_entry"] = nlayer
+
+    compare = (
+        "0001, (cap), 1, MetaSwap, ['boundary', 'landuse', 'rootzone_thickness', 'soil_physical_unit', 'meteo_station_number', 'surface_elevation', 'sprinkling_type', 'sprinkling_layer', 'sprinkling_capacity', 'wetted_area', 'urban_area', 'ponding_depth_urban', 'ponding_depth_rural', 'runoff_resistance_urban', 'runoff_resistance_rural', 'runon_resistance_urban', 'runon_resistance_rural', 'infiltration_capacity_urban', 'infiltration_capacity_rural', 'perched_water_table', 'soil_moisture_factor', 'conductivity_factor']\n"
+        "022, 001\n"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}boundary_l1.idf\n"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}landuse_l1.idf\n"
+        '1, 1, 001, 1.000, 0.000, 1.2, ""\n'
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}soil_physical_unit_l1.idf\n"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}meteo_station_number_l1.idf\n"
+        '1, 1, 001, 1.000, 0.000, 0.0, ""\n'
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}sprinkling_type_l1.idf\n"
+        f"1, 2, 001, 1.000, 0.000, -9999., {directory}{os.sep}sprinkling_layer_l1.idf\n"
+        '1, 1, 001, 1.000, 0.000, 1000.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 30.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 30.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 0.02, ""\n'
+        '1, 1, 001, 1.000, 0.000, 0.005, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.5, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.5, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.5, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.5, ""\n'
+        '1, 1, 001, 1.000, 0.000, 10.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 2.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 0.5, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.0, ""\n'
+        '1, 1, 001, 1.000, 0.000, 1.0, ""\n'
+        "008,extra files\n"
+        f"{directory}{os.sep}fact_svat.inp\n"
+        f"{directory}{os.sep}luse_svat.inp\n"
+        f"{directory}{os.sep}mete_grid.inp\n"
+        f"{directory}{os.sep}para_sim.inp\n"
+        f"{directory}{os.sep}tiop_sim.inp\n"
+        f"{directory}{os.sep}init_svat.inp\n"
+        f"{directory}{os.sep}comp_post.inp\n"
+        f"{directory}{os.sep}sel_key_svat_per.inp"
+    )
+
+    rendered = metaswap._render_projectfile(**to_render)
+
+    assert rendered == compare
+
+
+def test_metaswap_pkgcheck(metaswap):
+    # Test if no ValueError is thrown, source:
+    # https://miguendes.me/how-to-check-if-an-exception-is-raised-or-not-with-pytest
+    try:
+        metaswap._pkgcheck()
+    except ValueError as exc:
+        assert False, f"'metaswap._pkgcheck()' raised an exception {exc}"
+
+
+def test_metaswap_pkgcheck_fail(metaswap):
+    metaswap_fail = deepcopy(metaswap)
+
+    metaswap_fail.dataset = metaswap_fail.dataset.expand_dims("layer")
+
+    with pytest.raises(ValueError):
+        metaswap_fail._pkgcheck()
+
+
+def test_check_extra_files_no_files(metaswap):
+    metaswap_fail = deepcopy(metaswap)
+
+    metaswap_fail.extra_files = []
+
+    with pytest.raises(ValueError):
+        metaswap_fail.check_extra_files()
+
+
+def test_check_extra_files_missing_files(metaswap):
+    metaswap_fail = deepcopy(metaswap)
+
+    metaswap_fail.extra_files = [
+        "fact_svat.inp",
+        "luse_svat.inp",
+    ]
+
+    with pytest.raises(ValueError):
+        metaswap_fail.check_extra_files()
