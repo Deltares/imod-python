9fd1a07e908890c5f1ec58274c4386b49efb5331
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-05-06 12:30:42 +0200

    * Boundary conditions make sense now. * added aditional comments * increased cellsize

--- examples/ImodflowProjectfile.py
+++ examples/ImodflowProjectfile.py
@@ -1,10 +1,5 @@
 """
-Create a iMODFLOW model with all the bells and whistles.
-
-The boundary condition data itself does not make too much sense
-(CHD everywhere? Overlapping GHB and CHD?)
-But it should be enough to give you a quickstart of 
-how to create an imododflow model with projectfile in imod-python.
+Create an iMODFLOW model with all the bells and whistles.
 """
 
 
@@ -18,6 +13,9 @@ import geopandas as gpd
 import pathlib
 from shapely.geometry import LineString
 
+####################
+# Create test data #
+####################
 # Path management
 name = "my_first_imodflow_model"
 wdir = pathlib.Path("./tests/")
@@ -25,8 +23,8 @@ wdir = pathlib.Path("./tests/")
 # Discretization
 shape = nlay, nrow, ncol = 3, 9, 9
 
-dx = 10.0
-dy = -10.0
+dx = 100.0
+dy = -100.0
 dz = np.array([5, 30, 100])
 xmin = 0.0
 xmax = dx * ncol
@@ -47,7 +45,7 @@ interfaces = np.insert((surface - np.cumsum(dz)), 0, surface)
 bottom = xr.DataArray(interfaces[1:], coords={"layer": layer}, dims="layer")
 top = xr.DataArray(interfaces[:-1], coords={"layer": layer}, dims="layer")
 
-## Create monthly timesteps
+## Create 1 year of monthly timesteps
 times = pd.date_range(start="1/1/2018", end="12/1/2018", freq="MS")
 
 # Layer properties
@@ -62,17 +60,37 @@ starting_head = ibound.copy()
 ## Create rising trend.
 trend = np.ones(times[:-1].shape)
 trend = np.cumsum(trend)
+trend_da = xr.DataArray(trend, coords={"time": times[:-1]}, dims=["time"])
 
-## Assign values only to edges of model domain.
-head = starting_head.where(starting_head.x.isin([x[0], x[-1]]))
+## Assign values only to edges of model x domain.
+###      x
+###   1000001
+### y 1000001
+###   1000001
+head_edge = starting_head.where(starting_head.x.isin([x[0], x[-1]]))
 ## Calculate outer product with rising trend.
-head = xr.DataArray(trend, coords={"time": times[:-1]}, dims=["time"]) * head
+head = trend_da * head_edge
 
 ## Create a DataArray for the periodic boundary condition
-head_periodic = head.isel(time=[3, 9])
-head_periodic = head_periodic.assign_coords(
-    time=head_periodic.time - np.timedelta64(365, "D")
-)
+## Assign values only to the center along x dimension
+###      x
+###   0001000
+### y 0001000
+###   0001000
+head_central = starting_head.where(starting_head.x == x[4]).sel(layer=1)
+## Create period times, we let these times start before the model starts
+## This is because iMOD only forward fills periods
+## Otherwise, in this case there wouldn't be a periodic boundary condition until april
+period_times = times[[3, 9]] - np.timedelta64(365, "D")
+## We are creating a summer and winter level.
+periods_da = xr.DataArray([4, 10], coords={"time": period_times}, dims=["time"])
+head_periodic = periods_da * head_central
+
+## Create dictionary to tell iMOD which period name corresponds to which date.
+timemap = {
+    period_times[0]: "summer",
+    period_times[1]: "winter",
+}
 
 # Wells
 wel_df = pd.DataFrame()
@@ -84,17 +102,25 @@ wel_df["time"] = np.tile(times[:-1], 2)[: len(x)]
 wel_df["layer"] = 2
 
 # Horizontal Flow Barrier
-line1 = LineString([(x[1], y[1]), (x[1], y[-2])])
-line2 = LineString([(x[4], y[1]), (x[4], y[-2])])
+## Create barriers between ditches in layer 1 and 2 (but not 3).
+###      x
+###   0100010
+### y 0100010
+###   0100010
+line1 = LineString([(x[2], ymin), (x[2], ymax])
+line2 = LineString([(x[7], ymin), (x[7], ymax])
 
 lines = np.array([line1, line2, line1, line2], dtype="object")
-hfb_layers = np.array([3, 3, 4, 4])
+hfb_layers = np.array([1, 1, 2, 2])
+id_name = ["left_upper", "right_upper", "left_lower", "right_lower"]
 
 hfb_gdf = gpd.GeoDataFrame(
-    geometry=lines, data=dict(layer=hfb_layers, resistance=100.0)
+    geometry=lines, data=dict(id_name=id_name, layer=hfb_layers, resistance=100.0)
 )
 
-# Build model
+###############
+# Build model #
+###############
 m = flow.ImodflowModel(name)
 m["pcg"] = flow.PreconditionedConjugateGradientSolver()
 
@@ -114,14 +140,10 @@ m["chd2"] = flow.ConstantHead(head=head)
 
 ## Create periodic boundary condition
 m["ghb"] = flow.GeneralHeadBoundary(head=head_periodic, conductance=10.0)
-timemap = {
-    head_periodic.time.values[0]: "summer",
-    head_periodic.time.values[1]: "winter",
-}
 m["ghb"].periodic_stress(timemap)
 
 ## Create second periodic boundary condition
-m["ghb2"] = flow.GeneralHeadBoundary(head=head_periodic + 10.0, conductance=10.0)
+m["ghb2"] = flow.GeneralHeadBoundary(head=head_periodic + 10.0, conductance=1.0)
 ### You also need to specify periodic stresses for second system.
 m["ghb2"].periodic_stress(timemap)
 
