2dd4e4ee8ac4032ae7ea7c10b3a2ba03b13420e3
Hofer-Julian <julianhofer@gnome.org>
2021-12-22 10:52:50 +0100

    continue

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,10 @@ from fixtures.flow_basic_fixture import (
     two_days,
     well_df,
 )
+<<<<<<< HEAD
 from fixtures.mf6_circle_fixture import circle_model, circle_result
 from fixtures.mf6_twri_disv_fixture import twri_disv_model
+=======
+>>>>>>> ed6629e (Add fixture import)
 from fixtures.mf6_twri_fixture import transient_twri_model, twri_model, twri_result
 from fixtures.msw_fixture import fixed_format_parser
