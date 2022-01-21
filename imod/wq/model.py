8ad1673d6f1023b1c7a9194657282124f9c41830
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-01-21 17:55:08 +0100

    comma in comment

--- imod/wq/model.py
+++ imod/wq/model.py
@@ -79,7 +79,7 @@ class Model(collections.UserDict):
 
             if pkgname == "bas6":
                 # da.sel() unsets dimensions for scalars, this messes with bas6
-                # package. because check_ibound is called upon initialization.
+                # package, because check_ibound is called upon initialization.
                 sel_dims = {
                     k: [v] if np.isscalar(v) else v for k, v in sel_dims.items()
                 }
