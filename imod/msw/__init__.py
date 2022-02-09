06c0f38684d4c7a1a71247c1e42c83a4e9c05ad6
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-02-09 16:56:32 +0100

    import MeteoGrid

--- imod/msw/__init__.py
+++ imod/msw/__init__.py
@@ -5,6 +5,7 @@ Create a MetaSWAP model.
 from imod.msw.coupler_mapping import CouplerMapping
 from imod.msw.grid_data import GridData
 from imod.msw.infiltration import Infiltration
+from imod.msw.meteo_grid import MeteoGrid
 from imod.msw.meteo_mapping import EvapotranspirationMapping, PrecipitationMapping
 from imod.msw.model import MetaSwapModel
 from imod.msw.sprinkling import Sprinkling
