560b7ba398aeba970ce1b8ec3dee97d573b44831
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-02-25 19:24:46 +0100

    Fix bugs, add TODOs and copy nodenr2svat as rechargeindex2svat

--- imod/couplers/metamod/metamod.py
+++ imod/couplers/metamod/metamod.py
@@ -1,6 +1,8 @@
+import shutil
+
 from imod.couplers.metamod.node_svat_mapping import NodeSvatMapping
 from imod.mf6 import Modflow6Simulation
-from imod.msw import MetaSwapModel
+from imod.msw import MetaSwapModel, GridData
 
 
 class MetaMod:
@@ -15,17 +17,40 @@ class MetaMod:
         The Modflow6 simulation that should be coupled.
     """
 
+    # TODO:
+    # - NodeSvatMapping: check with idomain if no coupling is done to inactive
+    #   cells.
+    # - rchindex2svat.dxc
+    # - wellindex2svat.dxc
+    # - a toml file
+
     def __init__(self, msw_model: MetaSwapModel, mf6_simulation: Modflow6Simulation):
         self.msw_model = msw_model
         self.mf6_simulation = mf6_simulation
 
     def write(self, directory):
-        self.msw_model.write(directory)
-        self.mf6_simulation.write(directory)
-        self.write_exchanges(directory)
+        # For some reason the Modflow 6 model has to be written first, before
+        # writing the MetaSWAP model. Else we get an Access Violation Error when
+        # running the coupler.
+        self.mf6_simulation.write(directory / "Modflow6")
+        self.msw_model.write(directory / "MetaSWAP")
+        # Exchange files should be in Modflow 6 model directory
+        self.write_exchanges(directory / "Modflow6")
 
     def write_exchanges(self, directory):
-        area = self.msw_model["grid_data"].dataset["area"]
-        active = self.msw_model["grid_data"].dataset["active"]
+        grid_data_key = [
+            pkgname
+            for pkgname, pkg in self.msw_model.items()
+            if isinstance(pkg, GridData)
+        ][0]
+
+        area = self.msw_model[grid_data_key].dataset["area"]
+        active = self.msw_model[grid_data_key].dataset["active"]
         grid_mapping = NodeSvatMapping(area, active)
         grid_mapping.write(directory)
+        # Copy nodenr2svat.dxc as rchindex2svat.dxc.
+        # This only works if no np.nan values were included in the
+        # array to create the Modflow6 package for rch_msw.
+        shutil.copy2(
+            directory / NodeSvatMapping._file_name, directory / "rchindex2svat.dxc"
+        )
