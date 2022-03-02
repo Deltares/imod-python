ee78082fc12f69f0c6679f883ffdebe9165de54f
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-02 14:35:38 +0100

    New mapping method changed order of mapped svats for wells, mapping stays the same.

--- tests/test_msw/test_coupler_mapping.py
+++ tests/test_msw/test_coupler_mapping.py
@@ -66,9 +66,9 @@ def test_simple_model_with_sprinkling(fixed_format_parser):
             msw.CouplerMapping._metadata_dict,
         )
 
-    assert_equal(results["mod_id"], np.array([20, 20, 14, 8, 2, 8, 2, 5]))
-    assert_equal(results["svat"], np.array([1, 3, 4, 2, 1, 2, 3, 4]))
-    assert_equal(results["layer"], np.array([3, 3, 2, 1, 1, 1, 1, 1]))
+    assert_equal(results["mod_id"], np.array([20, 8, 20, 14, 2, 8, 2, 5]))
+    assert_equal(results["svat"], np.array([1, 2, 3, 4, 1, 2, 3, 4]))
+    assert_equal(results["layer"], np.array([3, 1, 3, 2, 1, 1, 1, 1]))
 
 
 def test_simple_model_without_sprinkling(fixed_format_parser):
