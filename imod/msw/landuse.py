192ffb1fefd6f5b09749c8ff34a6ffb10d17562b
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-16 17:24:45 +0100

    finish init and fix typo

--- imod/msw/landuse.py
+++ imod/msw/landuse.py
@@ -48,9 +48,9 @@ class LanduseOptions(Package):
         Gift duration (d)
     rotational_period: array of floats (xr.DataArray)
         Rotational period (d)
-    start_sprinkling_seasion: array of floats (xr.DataArray)
+    start_sprinkling_season: array of floats (xr.DataArray)
         Day of year at which sprinkling season starts (d)
-    end_sprinkling_seasion: array of floats (xr.DataArray)
+    end_sprinkling_season: array of floats (xr.DataArray)
         Day of year at which sprinkling season ends (d)
     interception_option: array of integers (xr.DataAray)
         Choose interception model. 0=Rutter, 1=Von Hoyningen. NOTE: option
@@ -92,8 +92,8 @@ class LanduseOptions(Package):
         "gift": VariableMetaData(8, 1.0, 1000.0, float),
         "gift_duration": VariableMetaData(8, 0.01, 1000.0, float),
         "rotational_period": VariableMetaData(6, 1.0, 366.0, float),
-        "start_sprinkling_seasion": VariableMetaData(6, 0.0, 366.0, float),
-        "end_sprinkling_seasion": VariableMetaData(6, 0.0, 366.0, float),
+        "start_sprinkling_season": VariableMetaData(6, 0.0, 366.0, float),
+        "end_sprinkling_season": VariableMetaData(6, 0.0, 366.0, float),
         # Penman-Monteith: not supported
         "albedo": VariableMetaData(8, None, None, str),
         "rsc": VariableMetaData(8, None, None, str),
@@ -103,8 +103,11 @@ class LanduseOptions(Package):
         "kdir": VariableMetaData(8, None, None, str),
         # Interception
         "interception_option": VariableMetaData(6, 0, 2, int),
-        "interception_capacity_per_LAI": VariableMetaData(8, 0.0, 10.0, float),
+        "interception_capacity_per_LAI_Rutter": VariableMetaData(8, 0.0, 10.0, float),
         "interception_intercept": VariableMetaData(8, 0.0, 1.0, float),
+        "interception_capacity_per_LAI_VonHoyningen": VariableMetaData(
+            8, 0.0, 10.0, float
+        ),
         # Gash interception: not supported
         "pfree": VariableMetaData(8, None, None, str),
         "pstem": VariableMetaData(8, None, None, str),
@@ -118,5 +121,54 @@ class LanduseOptions(Package):
 
     _file_name = "luse_svat.inp"
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        landuse_name,
+        vegetation_index,
+        jarvis_o2_stress,
+        jarvis_drought_stress,
+        feddes_p1,
+        feddes_p2,
+        feddes_p3h,
+        feddes_p3l,
+        feddes_p4,
+        feddes_t3h,
+        feddes_t3l,
+        threshold_sprinkling,
+        fraction_evaporated_sprinkling,
+        gift,
+        gift_duration,
+        rotational_period,
+        start_sprinkling_season,
+        end_sprinkling_season,
+        interception_option,
+        interception_capacity_per_LAI,
+        interception_intercept,
+    ):
+
+        self.dataset["landuse_name"] = landuse_name
+        self.dataset["vegetation_index"] = vegetation_index
+        self.dataset["jarvis_o2_stress"] = jarvis_o2_stress
+        self.dataset["jarvis_drought_stress"] = jarvis_drought_stress
+        self.dataset["feddes_p1"] = feddes_p1
+        self.dataset["feddes_p2"] = feddes_p2
+        self.dataset["feddes_p3h"] = feddes_p3h
+        self.dataset["feddes_p3l"] = feddes_p3l
+        self.dataset["feddes_p4"] = feddes_p4
+        self.dataset["feddes_t3h"] = feddes_t3h
+        self.dataset["feddes_t3l"] = feddes_t3l
+        self.dataset["threshold_sprinkling"] = threshold_sprinkling
+        self.dataset["fraction_evaporated_sprinkling"] = fraction_evaporated_sprinkling
+        self.dataset["gift"] = gift
+        self.dataset["gift_duration"] = gift_duration
+        self.dataset["rotational_period"] = rotational_period
+        self.dataset["start_sprinkling_season"] = start_sprinkling_season
+        self.dataset["end_sprinkling_season"] = end_sprinkling_season
+        self.dataset["interception_option"] = interception_option
+        self.dataset[
+            "interception_capacity_per_LAI_Rutter"
+        ] = interception_capacity_per_LAI
+        self.dataset[
+            "interception_capacity_per_LAI_VonHoyningen"
+        ] = interception_capacity_per_LAI
+        self.dataset["interception_intercept"] = interception_intercept
