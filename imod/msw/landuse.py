e19e5cc3ec8c916b8988bc5f662e96651dba7336
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-17 15:38:08 +0100

    Complete landuse options

--- imod/msw/landuse.py
+++ imod/msw/landuse.py
@@ -76,8 +76,9 @@ class LanduseOptions(Package):
         "landuse_name": VariableMetaData(19, None, None, str),
         "vegetation_index": VariableMetaData(6, 0.0, 1e6, int),
         # Jarvis stress
-        "jarvis_o2_stress": VariableMetaData(6, 0.0, 1e6, float),
-        "jarvis_drought_stress": VariableMetaData(6, 0.0, 1e6, float),
+        # Columns 33-35 and 36-38, but both F6?
+        "jarvis_o2_stress": VariableMetaData(3, 0.0, 1e6, float),
+        "jarvis_drought_stress": VariableMetaData(3, 0.0, 1e6, float),
         # Feddes transpiration function
         "feddes_p1": VariableMetaData(8, -160.0, 100.0, float),
         "feddes_p2": VariableMetaData(8, -160.0, 100.0, float),
@@ -145,7 +146,7 @@ class LanduseOptions(Package):
         interception_capacity_per_LAI,
         interception_intercept,
     ):
-
+        super().__init__()
         self.dataset["landuse_name"] = landuse_name
         self.dataset["vegetation_index"] = vegetation_index
         self.dataset["jarvis_o2_stress"] = jarvis_o2_stress
@@ -172,3 +173,37 @@ class LanduseOptions(Package):
             "interception_capacity_per_LAI_VonHoyningen"
         ] = interception_capacity_per_LAI
         self.dataset["interception_intercept"] = interception_intercept
+
+    def _render(self, file):
+        dataframe = self.dataset.to_dataframe(
+            dim_order=("landuse_index",)
+        ).reset_index()
+
+        self._check_range(dataframe)
+
+        # Find missing columns
+        missing_keys = set(self._metadata_dict.keys()) ^ set(dataframe.columns)
+
+        # Add missing columns
+        for key in missing_keys:
+            dataframe[key] = ""
+
+        # Reorder columns to _metadata_dict order
+        dataframe = dataframe[list(self._metadata_dict.keys())]
+
+        return self.write_dataframe_fixed_width(file, dataframe)
+
+    def write(self, directory):
+        directory = pathlib.Path(directory)
+
+        filename = directory / self._file_name
+        with open(filename, "w") as f:
+            self._render(f)
+
+    def _pkgcheck(self):
+        dims = self.dataset.dims
+        dims_expected = ("landuse_index",)
+        if len(set(dims) - set(dims_expected)) > 0:
+            raise ValueError(
+                f"Please provide DataArrays with dimensions {dims_expected}"
+            )
