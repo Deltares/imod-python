f9959124472ca8282af9172829116a1776a427ac
Huite Bootsma <huitebootsma@gmail.com>
2022-03-25 14:50:46 +0100

    Review comments

--- imod/evaluate/head.py
+++ imod/evaluate/head.py
@@ -19,8 +19,6 @@ def convert_pointwaterhead_freshwaterhead(
     function then outputs the freshwaterhead for the uppermost underlying cell where
     the original point water head exceeds the cell centre.
 
-    *Requires bottleneck.*
-
     Parameters
     ----------
     pointwaterhead : float or xr.DataArray of floats
@@ -58,6 +56,10 @@ def _calculate_gxg(
 ) -> xr.DataArray:
     import bottleneck as bn
 
+    # Most efficient way of finding the three highest and three lowest is via a
+    # partition. See:
+    # https://bottleneck.readthedocs.io/en/latest/reference.html#bottleneck.partition
+
     def lowest3_mean(da: xr.DataArray):
         a = bn.partition(da.values, kth=2, axis=-1)
 
@@ -84,11 +86,12 @@ def _calculate_gxg(
     n_year = int(timesize / 24)
 
     # First and second date of March: 4, 5; first date of April: 6.
+    month_index = np.array([4, 5, 6])
     # Repeat this for every year in dataset, and increment by 24 per repetition.
     yearly_increments = (np.arange(n_year) * 24)[:, np.newaxis]
     # Broadcast to a full set
     gvg_index = xr.DataArray(
-        data=(np.array([4, 5, 6]) + yearly_increments), dims=("hydroyear", "bimonth")
+        data=(month_index + yearly_increments), dims=("hydroyear", "bimonth")
     )
     gvg_data = head_bimonthly.isel(time=gvg_index)
     # Filters years without 3 available measurments.
@@ -154,15 +157,13 @@ def calculate_gxg_points(
 
     GLG and GHG (average lowest and average highest groundwater level respectively) are
     calculated as the average of the three lowest (GLG) or highest (GHG) head values per
-    hydrological year (april - april), for head values measured at a semi-monthly frequency
+    Dutch hydrological year (april - april), for head values measured at a semi-monthly frequency
     (14th and 28th of every month). GVG (average spring groundwater level) is calculated as
     the average of groundwater level on 14th and 28th of March, and 14th of April. Supplied head
     values are resampled (nearest) to the 14/28 frequency.
 
     Hydrological years without all 24 14/28 dates present are discarded for glg and ghg.
     Years without the 3 dates for gvg are discarded.
-
-    *Requires bottleneck.*
 
     Parameters
     ----------
@@ -246,15 +247,13 @@ def calculate_gxg(
 
     GLG and GHG (average lowest and average highest groundwater level respectively) are
     calculated as the average of the three lowest (GLG) or highest (GHG) head values per
-    hydrological year (april - april), for head values measured at a semi-monthly frequency
+    Dutch hydrological year (april - april), for head values measured at a semi-monthly frequency
     (14th and 28th of every month). GVG (average spring groundwater level) is calculated as
     the average of groundwater level on 14th and 28th of March, and 14th of April. Supplied head
     values are resampled (nearest) to the 14/28 frequency.
 
     Hydrological years without all 24 14/28 dates present are discarded for glg and ghg.
     Years without the 3 dates for gvg are discarded.
-
-    *Requires bottleneck.*
 
     Parameters
     ----------
@@ -307,111 +306,4 @@ def calculate_gxg(
     head_bimonthly = head.reindex(time=dates, method="nearest", tolerance=tolerance)
 
     gxg = _calculate_gxg(head_bimonthly, below_surfacelevel)
-    return gxg
-
-
-def old_calculate_gxg(head, below_surfacelevel=False):
-    """Function to calculate GxG groundwater characteristics from head time series.
-
-    GLG and GHG (average lowest and average highest groundwater level respectively) are
-    calculated as the average of the three lowest (GLG) or highest (GHG) head values per
-    hydrological year (april - april), for head values measured at a semi-monthly frequency
-    (14th and 28th of every month). GVG (average spring groundwater level) is calculated as
-    the average of groundwater level on 28th of March, 14th and 28th of April. Supplied head
-    values are resampled (nearest) to the 14/28 frequency. Hydrological years without all 24
-    14/28 dates present are discarded.
-
-    *Requires bottleneck.*
-
-    Parameters
-    ----------
-    head : xr.DataArray of floats
-        Head relative to sea level, in m, or m below surface level if `below_surfacelevel` is
-        set to True. Must be of dimensions ``("time", "y", "x")``.
-    below_surfacelevel : boolean, optional
-        False (default) if heads are relative to sea level. If True, heads are taken as m
-        below surface level.
-
-    Returns
-    -------
-    gxg : xr.Dataset
-        Dataset containing `glg`: average lowest head, `ghg`: average highest head, and `gvg`:
-        average spring head.
-
-    Examples
-    --------
-    Load the heads, and calculate groundwater characteristics after the year 2000:
-
-    >>> import imod
-    >>> heads = imod.idf.open("head*.idf").sel(time=heads.time.dt.year >= 2000)
-    >>> gxg = imod.evaluate.calculate_gxg(heads)
-
-    Transform to meters below surface level by substracting from surface level:
-
-    >>> surflevel = imod.idf.open("surfacelevel.idf")
-    >>> gxg = surflevel - gxg
-
-    Or calculate from groundwater level relative to surface level directly:
-
-    >>> gwl = surflevel - heads
-    >>> gxg = imod.evaluate.calculate_gxg(gwl, below_surfacelevel=True)
-
-    """
-    if not head.dims == ("time", "y", "x"):
-        raise ValueError('Dimensions must be ("time", "y", "x")')
-
-    # reindex to GxG frequency date_range: every 14th and 28th of the month
-    d13 = pd.DateOffset(days=13)
-    dr_gxg = (
-        pd.date_range(
-            start=pd.Timestamp(head.time.values[0]) - d13,
-            end=pd.Timestamp(head.time.values[-1]) - d13,
-            freq="SMS",
-        )
-        + d13
-    )
-    da_gxg = head.reindex(time=dr_gxg, method="nearest")
-
-    # calculate LG3, HG3 per hydrological year
-    dr_hydroyear = pd.date_range(
-        start=dr_gxg[0] - pd.DateOffset(months=1),
-        end=dr_gxg[-1] + pd.DateOffset(months=1),
-        freq="AS-APR",
-    )
-    annual = da_gxg.groupby_bins("time", bins=dr_hydroyear)
-    perturb = np.random.random((24, *head.isel(time=0).shape)) * 1e-6
-    glg = []
-    ghg = []
-    gvg = []
-    years = []
-    for y, da in annual:
-        if len(da.time) == 24:  # only include full-years
-            da = da.load() + perturb  # perturb to assure integer ranks
-            dar = da.rank(dim="time")
-            maxrank = dar.max()  # to allow for nodata values within timeseries
-            lg3 = da.where(dar <= 3).mean(dim="time")
-            hg3 = da.where(dar > maxrank - 3).mean(dim="time")
-            vg3 = da.isel(time=[0, 1, -1]).mean(dim="time")
-            glg.append(lg3)
-            ghg.append(hg3)
-            gvg.append(vg3)
-            years.append(y)
-
-    # average
-    glg = xr.concat(glg, dim="time").mean(dim="time")
-    ghg = xr.concat(ghg, dim="time").mean(dim="time")
-    gvg = xr.concat(gvg, dim="time").mean(dim="time")
-
-    gxg = xr.Dataset()
-    if not below_surfacelevel:
-        gxg["glg"] = glg
-        gxg["ghg"] = ghg
-    else:
-        gxg["glg"] = ghg
-        gxg["ghg"] = glg
-    gxg["gvg"] = gvg
-
-    gxg = gxg.assign_attrs(
-        GxG_period=f"{years[0].left:%Y-%m-%d} - {years[-1].right:%Y-%m-%d}"
-    )
     return gxg
