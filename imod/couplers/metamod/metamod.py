86c3e113d40c39562558d492b82d2bfe91ef9ad3
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-25 17:37:53 +0100

    Remove unused import

--- imod/couplers/metamod/metamod.py
+++ imod/couplers/metamod/metamod.py
@@ -1,5 +1,3 @@
-import warnings
-
 from imod.couplers.metamod.node_svat_mapping import NodeSvatMapping
 from imod.couplers.metamod.rch_svat_mapping import RechargeSvatMapping
 from imod.couplers.metamod.wel_svat_mapping import WellSvatMapping
