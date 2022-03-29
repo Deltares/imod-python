1553083abeab704e183acf71a4274fbcf7276fff
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-29 12:11:13 +0200

    Add test for pkgcheck

--- tests/test_couplers/test_node_svat_mapping.py
+++ tests/test_couplers/test_node_svat_mapping.py
@@ -8,6 +8,8 @@ from numpy.testing import assert_equal
 from imod import mf6
 from imod.couplers import metamod
 
+import pytest
+
 
 def test_simple_model(fixed_format_parser):
     x = [1.0, 2.0, 3.0]
@@ -58,3 +60,44 @@ def test_simple_model(fixed_format_parser):
     assert_equal(results["mod_id"], np.array([2, 8, 2, 5]))
     assert_equal(results["svat"], np.array([1, 2, 3, 4]))
     assert_equal(results["layer"], np.array([1, 1, 1, 1]))
+
+
+def test_inactive_idomain_in_svat():
+    x = [1.0, 2.0, 3.0]
+    y = [1.0, 2.0, 3.0]
+    subunit = [0, 1]
+    dx = 1.0
+    dy = 1.0
+    # fmt: off
+    svat = xr.DataArray(
+        np.array(
+            [
+                [[0, 1, 0],
+                 [0, 0, 0],
+                 [0, 2, 0]],
+
+                [[0, 3, 0],
+                 [0, 4, 0],
+                 [0, 0, 0]],
+            ]
+        ),
+        dims=("subunit", "y", "x"),
+        coords={"subunit": subunit, "y": y, "x": x, "dx": dx, "dy": dy}
+    )
+    # fmt: on
+
+    like = xr.full_like(svat.sel(subunit=1, drop=True), 1.0).expand_dims(
+        layer=[1, 2, 3]
+    )
+
+    idomain = xr.full_like(like, 1)
+    idomain[:, 1, :] = 0
+
+    dis = mf6.StructuredDiscretization(
+        top=xr.full_like(like, 1.0),
+        bottom=xr.full_like(like, 0.0),
+        idomain=idomain,
+    )
+
+    with pytest.raises(ValueError):
+        metamod.node_svat_mapping.NodeSvatMapping(svat, dis)
