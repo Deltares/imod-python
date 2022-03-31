be9c389966b849960f40827a9b6162f09b11138b
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-03-31 14:50:30 +0200

    Add fixed format parser

--- imod/fixed_format.py
+++ imod/fixed_format.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 from numbers import Number
+from typing import Dict, Union
+from pathlib import Path
+
+import numpy as np
+import warnings
 
 
 @dataclass
@@ -24,3 +29,48 @@ def format_fixed_width(value, metadata):
 
     converted_value = metadata.dtype(value)
     return format_string.format(converted_value)
+
+
+def fixed_format_parser(
+    file: Union[str, Path], metadata_dict: Dict[str, VariableMetaData]
+):
+    """
+    Read fixed format file, using a metadata_dict from a MetaSWAP package.
+
+    Parameters
+    ----------
+    file: str or Path
+        Fixed format file to read, likely a MetaSWAP input file
+    metadata_dict: dict
+        Dictionary with the VariableMetaData. Access this dictionary in a
+        package by calling <pkg>._metadata_dict
+    """
+    results = {}
+    for key in metadata_dict:
+        results[key] = []
+
+    with open(file) as f:
+        lines = f.readlines()
+        for line in lines:
+            if line == "\n":
+                continue
+            troublesome = set()
+            for varname, metadata in metadata_dict.items():
+                # Take first part of line
+                value = line[: metadata.column_width]
+                # Convert to correct type
+                try:
+                    converted_value = metadata.dtype(value)
+                except ValueError:
+                    troublesome.add(varname)
+                    converted_value = np.nan
+                # Add to results
+                results[varname].append(converted_value)
+                # Truncate line
+                line = line[metadata.column_width :]
+        if len(troublesome) > 0:
+            warnings.warn(
+                f"Had trouble reading the variables: {troublesome}",
+                UserWarning,
+            )
+    return results
