25108c0a6f9feed03f9cccb66605ddceac60ee31
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-04-20 14:18:20 +0200

    Expand docstrings

--- imod/msw/pkgbase.py
+++ imod/msw/pkgbase.py
@@ -43,6 +43,10 @@ class MetaSwapPackage(abc.ABC):
         )
 
     def write(self, directory: Union[str, Path], index: np.ndarray, svat: xr.DataArray):
+        """
+        Write MetaSWAP package to its corresponding fixed format file. This has
+        the `.inp` extension.
+        """
         directory = Path(directory)
 
         filename = directory / self._file_name
@@ -50,6 +54,10 @@ class MetaSwapPackage(abc.ABC):
             self._render(f, index, svat)
 
     def _check_range(self, dataframe):
+        """
+        Check if provided data does not exceeds MetaSWAPs ranges. These ranges
+        are specified in the ``self._metadata_dict`` for each variable.
+        """
         for varname in dataframe:
             min_value = self._metadata_dict[varname].min_value
             max_value = self._metadata_dict[varname].max_value
@@ -61,6 +69,7 @@ class MetaSwapPackage(abc.ABC):
                 )
 
     def write_dataframe_fixed_width(self, file, dataframe):
+        """Write dataframe to fixed format file."""
         for row in dataframe.itertuples():
             for index, metadata in enumerate(self._metadata_dict.values()):
                 content = format_fixed_width(row[index + 1], metadata)
@@ -68,9 +77,17 @@ class MetaSwapPackage(abc.ABC):
             file.write("\n")
 
     def _index_da(self, da, index):
+        """
+        Helper method that converts a DataArray to a 1d numpy array, and
+        consequently applies boolean indexing.
+        """
         return da.values.ravel()[index]
 
     def _render(self, file, index, svat):
+        """
+        Collect to be written data in a DataFrame and call
+        ``self.write_dataframe_fixed_width``
+        """
         data_dict = {"svat": svat.values.ravel()[index]}
 
         subunit = svat.coords["subunit"]
@@ -94,6 +111,10 @@ class MetaSwapPackage(abc.ABC):
         return self.write_dataframe_fixed_width(file, dataframe)
 
     def _pkgcheck(self):
+        """
+        Method to do package checks. The base class version checks if provided
+        data has a subunit coordinate or not.
+        """
         for var in self._with_subunit:
             if "subunit" not in self.dataset[var].coords:
                 raise ValueError(
