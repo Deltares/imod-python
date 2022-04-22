566a07ae8bbca9f98f9a790c7a449612cddc93dd
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-04-22 14:35:59 +0200

    Support TOML files for iMOD Coupler 1.0 release.

--- imod/couplers/metamod/metamod.py
+++ imod/couplers/metamod/metamod.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional
 
+import os
 import tomli_w
 
 from imod.couplers.metamod.node_svat_mapping import NodeSvatMapping
@@ -12,7 +13,8 @@ from imod.msw import GridData, MetaSwapModel, Sprinkling
 
 class MetaMod:
     """
-    The MetaMod class creates the necessary input files for coupling MetaSWAP to MODFLOW 6.
+    The MetaMod class creates the necessary input files for coupling MetaSWAP to
+    MODFLOW 6.
 
     Parameters
     ----------
@@ -20,15 +22,68 @@ class MetaMod:
         The MetaSWAP model that should be coupled.
     mf6_simulation : Modflow6Simulation
         The Modflow6 simulation that should be coupled.
+    mf6_rch_pkgkey: str
+        Key of Modflow 6 recharge package to which MetaSWAP is coupled.
+    mf6_wel_pkgkey: str or None
+        Optional key of Modflow 6 well package to which MetaSWAP sprinkling is
+        coupled.
     """
 
     _toml_name = "metamod.toml"
     _modflow6_model_dir = "Modflow6"
     _metaswap_model_dir = "MetaSWAP"
 
-    def __init__(self, msw_model: MetaSwapModel, mf6_simulation: Modflow6Simulation):
+    def __init__(
+        self,
+        msw_model: MetaSwapModel,
+        mf6_simulation: Modflow6Simulation,
+        mf6_rch_pkgkey: str,
+        mf6_wel_pkgkey: Optional[str] = None,
+    ):
         self.msw_model = msw_model
         self.mf6_simulation = mf6_simulation
+        self.mf6_rch_pkgkey = mf6_rch_pkgkey
+        self.mf6_wel_pkgkey = mf6_wel_pkgkey
+
+        self.is_sprinkling = self._check_coupler_and_sprinkling()
+
+    def _check_coupler_and_sprinkling(self):
+        mf6_rch_pkgkey = self.mf6_rch_pkgkey
+        mf6_wel_pkgkey = self.mf6_wel_pkgkey
+
+        gwf_names = self._get_gwf_model_names()
+
+        # Assume only one groundwater flow model
+        # FUTURE: Support multiple groundwater flow models.
+        gwf_model = self.mf6_simulation[gwf_names[0]]
+
+        if mf6_rch_pkgkey not in gwf_model.keys():
+            raise ValueError(
+                f"No package named {mf6_rch_pkgkey} detected in Modflow 6 model. "
+                "iMOD_coupler requires a Recharge package."
+            )
+
+        sprinkling_key = self.msw_model._get_pkg_key(Sprinkling, optional_package=True)
+
+        sprinkling_in_msw = sprinkling_key is not None
+        sprinkling_in_mf6 = mf6_wel_pkgkey in gwf_model.keys()
+
+        if sprinkling_in_msw and not sprinkling_in_mf6:
+            raise ValueError(
+                f"No package named {mf6_wel_pkgkey} found in Modflow 6 model, "
+                "but Sprinkling package found in MetaSWAP. "
+                "iMOD Coupler requires a Well Package "
+                "to couple wells."
+            )
+        elif not sprinkling_in_msw and sprinkling_in_mf6:
+            raise ValueError(
+                f"Modflow 6 Well package {mf6_wel_pkgkey} specified for sprinkling, "
+                "but no Sprinkling package found in MetaSWAP model."
+            )
+        elif sprinkling_in_msw and sprinkling_in_mf6:
+            return True
+        else:
+            return False
 
     def write(
         self,
@@ -66,10 +121,23 @@ class MetaMod:
         # running the coupler.
         self.mf6_simulation.write(directory / self._modflow6_model_dir)
         self.msw_model.write(directory / self._metaswap_model_dir)
-        # Exchange files should be in Modflow 6 model directory
-        self.write_exchanges(directory / self._modflow6_model_dir)
 
-        self.write_toml(directory, modflow6_dll, metaswap_dll, metaswap_dll_dependency)
+        # Write exchange files
+        exchange_dir = directory / "exchanges"
+        exchange_dir.mkdir(mode=755, exist_ok=True)
+        self.write_exchanges(exchange_dir, self.mf6_rch_pkgkey, self.mf6_wel_pkgkey)
+
+        coupling_dict = self._get_coupling_dict(
+            exchange_dir, self.mf6_rch_pkgkey, self.mf6_wel_pkgkey
+        )
+
+        self.write_toml(
+            directory,
+            modflow6_dll,
+            metaswap_dll,
+            metaswap_dll_dependency,
+            coupling_dict,
+        )
 
     def write_toml(
         self,
@@ -77,6 +145,7 @@ class MetaMod:
         modflow6_dll: Union[str, Path],
         metaswap_dll: Union[str, Path],
         metaswap_dll_dependency: Union[str, Path],
+        coupling_dict: dict,
     ):
         """
         Write .toml file which configures the imod coupler run.
@@ -98,6 +167,8 @@ class MetaMod:
             [fmpich2.dll, mpich2mpi.dll, mpich2nemesis.dll, TRANSOL.dll]. You
             can obtain these by downloading `the last iMOD5 release
             <https://oss.deltares.nl/web/imod/download-imod5>`_
+        coupling_dict: dict
+            Dictionary with names of coupler packages and paths to mappings.
         """
         # force to Path
         directory = Path(directory)
@@ -107,24 +178,95 @@ class MetaMod:
         coupler_toml = {
             "timing": False,
             "log_level": "INFO",
-            "kernels": {
-                "modflow6": {
-                    "dll": str(modflow6_dll),
-                    "model": f".\\{self._modflow6_model_dir}",
+            "log_file": "imod_coupler.log",
+            "driver_type": "metamod",
+            "driver": {
+                "kernels": {
+                    "modflow6": {
+                        "dll": str(modflow6_dll),
+                        "work_dir": f".\\{self._modflow6_model_dir}",
+                    },
+                    "metaswap": {
+                        "dll": str(metaswap_dll),
+                        "work_dir": f".\\{self._metaswap_model_dir}",
+                        "dll_dep_dir": str(metaswap_dll_dependency),
+                    },
                 },
-                "metaswap": {
-                    "dll": str(metaswap_dll),
-                    "model": f".\\{self._metaswap_model_dir}",
-                    "dll_dependency": str(metaswap_dll_dependency),
-                },
+                "coupling": [coupling_dict],
             },
-            "exchanges": [{"kernels": ["modflow6", "metaswap"]}],
         }
 
         with open(toml_path, "wb") as f:
             tomli_w.dump(coupler_toml, f)
 
-    def write_exchanges(self, directory: Union[str, Path]):
+    def _get_gwf_model_names(self):
+        """
+        Get names of gwf models in mf6 simulation
+        """
+        return [
+            key
+            for key, value in self.mf6_simulation.items()
+            if value._pkg_id == "model"
+        ]
+
+    def _get_coupling_dict(
+        self,
+        directory: Union[str, Path],
+        mf6_rch_pkgkey: str,
+        mf6_wel_pkgkey: Optional[str],
+    ) -> dict:
+        """
+        Get dictionary with names of coupler packages and paths to mappings.
+
+        Parameters
+        ----------
+        directory: str or Path
+            Directory where .dxc files are written.
+        mf6_rch_pkgkey: str
+            Key of Modflow 6 recharge package to which MetaSWAP is coupled.
+        mf6_wel_pkgkey: str
+            Key of Modflow 6 well package to which MetaSWAP sprinkling is
+            coupled.
+
+        Returns
+        -------
+        coupling_dict: dict
+            Dictionary with names of coupler packages and paths to mappings.
+        """
+
+        coupling_dict = {}
+
+        gwf_names = self._get_gwf_model_names()
+
+        # Assume only one groundwater flow model
+        # FUTURE: Support multiple groundwater flow models.
+        coupling_dict["mf6_model"] = gwf_names[0]
+
+        coupling_dict[
+            "mf6_msw_node_map"
+        ] = f"./{directory.name}/{NodeSvatMapping._file_name}"
+
+        coupling_dict["mf6_msw_recharge_pkg"] = mf6_rch_pkgkey
+        coupling_dict[
+            "mf6_msw_recharge_map"
+        ] = f"./{directory.name}/{RechargeSvatMapping._file_name}"
+
+        coupling_dict["enable_sprinkling"] = self.is_sprinkling
+
+        if self.is_sprinkling:
+            coupling_dict["mf6_msw_well_pkg"] = mf6_wel_pkgkey
+            coupling_dict[
+                "mf6_msw_sprinkling_map"
+            ] = f"./{directory.name}/{WellSvatMapping._file_name}"
+
+        return coupling_dict
+
+    def write_exchanges(
+        self,
+        directory: Union[str, Path],
+        mf6_rch_pkgkey: str,
+        mf6_wel_pkgkey: Optional[str],
+    ):
         """
         Write exchange files (.dxc) which map MetaSWAP's svats to Modflow 6 node
         numbers, recharge ids, and well ids.
@@ -133,12 +275,15 @@ class MetaMod:
         ----------
         directory: str or Path
             Directory where .dxc files are written.
+        mf6_rch_pkgkey: str
+            Key of Modflow 6 recharge package to which MetaSWAP is coupled.
+        mf6_wel_pkgkey: str
+            Key of Modflow 6 well package to which MetaSWAP sprinkling is
+            coupled.
         """
-        gwf_names = [
-            key
-            for key, value in self.mf6_simulation.items()
-            if value._pkg_id == "model"
-        ]
+
+        gwf_names = self._get_gwf_model_names()
+
         # Assume only one groundwater flow model
         # FUTURE: Support multiple groundwater flow models.
         gwf_model = self.mf6_simulation[gwf_names[0]]
@@ -155,30 +300,12 @@ class MetaMod:
         grid_mapping = NodeSvatMapping(svat, dis)
         grid_mapping.write(directory, index, svat)
 
-        # FUTURE: Not necessary after iMOD Coupler refactoring
-        if "rch_msw" not in gwf_model.keys():
-            raise ValueError(
-                "No package named 'rch_msw' detected in Modflow 6 model. "
-                "iMOD_coupler requires a Recharge package with 'rch_msw' as name"
-            )
-
-        recharge = gwf_model["rch_msw"]
+        recharge = gwf_model[mf6_rch_pkgkey]
 
         rch_mapping = RechargeSvatMapping(svat, recharge)
         rch_mapping.write(directory, index, svat)
 
-        sprinkling_key = self.msw_model._get_pkg_key(Sprinkling, optional_package=True)
-
-        # FUTURE: Not necessary after iMOD Coupler refactoring
-        if (sprinkling_key is not None) and not ("wells_msw" in gwf_model.keys()):
-            raise ValueError(
-                "No package named 'wells_msw' found in Modflow 6 model, "
-                "but Sprinkling package found in MetaSWAP. "
-                "iMOD Coupler requires a Well Package named 'wells_msw' "
-                "to couple wells."
-            )
-        elif "wells_msw" in gwf_model.keys():
-            well = gwf_model["wells_msw"]
-
+        if self.is_sprinkling:
+            well = gwf_model[mf6_wel_pkgkey]
             well_mapping = WellSvatMapping(svat, well)
             well_mapping.write(directory, index, svat)
--- tests/test_couplers/test_metamod.py
+++ tests/test_couplers/test_metamod.py
@@ -20,7 +20,12 @@ except ModuleNotFoundError:
 def test_metamod_write(msw_model, coupled_mf6_model, tmp_path):
     output_dir = tmp_path / "metamod"
 
-    coupled_models = MetaMod(msw_model, coupled_mf6_model)
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
 
     coupled_models.write(output_dir, "./modflow6.dll", "./metaswap.dll", "./metaswap")
 
@@ -45,9 +50,18 @@ def test_metamod_write_exhange(
     output_dir = tmp_path / "exhanges"
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    coupled_models = MetaMod(msw_model, coupled_mf6_model)
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
 
-    coupled_models.write_exchanges(output_dir)
+    coupled_models.write_exchanges(
+        output_dir,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
 
     nodes_dxc = fixed_format_parser(
         output_dir / NodeSvatMapping._file_name,
@@ -78,14 +92,73 @@ def test_metamod_write_exhange(
 
 
 @pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
-def test_metamod_write_toml(msw_model, coupled_mf6_model, tmp_path):
+def test_metamod_write_exhange_no_sprinkling(
+    msw_model, coupled_mf6_model, fixed_format_parser, tmp_path
+):
+    # Remove sprinkling package
+    msw_model.pop("sprinkling")
+
     output_dir = tmp_path / "exhanges"
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    coupled_models = MetaMod(msw_model, coupled_mf6_model)
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+    coupled_models.write_exchanges(
+        output_dir,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+    nodes_dxc = fixed_format_parser(
+        output_dir / NodeSvatMapping._file_name,
+        NodeSvatMapping._metadata_dict,
+    )
+
+    rch_dxc = fixed_format_parser(
+        output_dir / RechargeSvatMapping._file_name,
+        RechargeSvatMapping._metadata_dict,
+    )
+
+    assert (output_dir / WellSvatMapping._file_name).exists() == False
+
+    assert_equal(nodes_dxc["mod_id"], np.array([2, 8, 2, 5]))
+    assert_equal(nodes_dxc["svat"], np.array([1, 2, 3, 4]))
+    assert_equal(nodes_dxc["layer"], np.array([1, 1, 1, 1]))
+
+    assert_equal(rch_dxc["rch_id"], np.array([1, 3, 1, 2]))
+    assert_equal(rch_dxc["svat"], np.array([1, 2, 3, 4]))
+    assert_equal(rch_dxc["layer"], np.array([1, 1, 1, 1]))
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_write_toml(msw_model, coupled_mf6_model, tmp_path):
+    output_dir = tmp_path
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
+
+    coupling_dict = {
+        "mf6_model": "GWF_1",
+        "mf6_msw_node_map": "./exchanges/nodenr2svat.dxc",
+        "mf6_msw_recharge_map": "./exchanges/rchindex2svat.dxc",
+        "mf6_msw_recharge_pkg": "rch_msw",
+        "enable_sprinkling": True,
+        "mf6_msw_well_pkg": "wells_msw",
+        "mf6_msw_sprinkling_map": "./exchanges/wellindex2svat.dxc",
+    }
 
     coupled_models.write_toml(
-        output_dir, "./modflow6.dll", "./metaswap.dll", "./metaswap"
+        output_dir, "./modflow6.dll", "./metaswap.dll", "./metaswap", coupling_dict
     )
 
     with open(output_dir / "metamod.toml", mode="rb") as f:
@@ -94,18 +167,136 @@ def test_metamod_write_toml(msw_model, coupled_mf6_model, tmp_path):
     dict_expected = {
         "timing": False,
         "log_level": "INFO",
-        "kernels": {
-            "modflow6": {
-                "dll": "./modflow6.dll",
-                "model": f".\\{coupled_models._modflow6_model_dir}",
+        "log_file": "imod_coupler.log",
+        "driver_type": "metamod",
+        "driver": {
+            "kernels": {
+                "modflow6": {
+                    "dll": "./modflow6.dll",
+                    "work_dir": f".\\{coupled_models._modflow6_model_dir}",
+                },
+                "metaswap": {
+                    "dll": "./metaswap.dll",
+                    "work_dir": f".\\{coupled_models._metaswap_model_dir}",
+                    "dll_dep_dir": "./metaswap",
+                },
             },
-            "metaswap": {
-                "dll": "./metaswap.dll",
-                "model": f".\\{coupled_models._metaswap_model_dir}",
-                "dll_dependency": "./metaswap",
-            },
+            "coupling": [coupling_dict],
         },
-        "exchanges": [{"kernels": ["modflow6", "metaswap"]}],
     }
 
     assert toml_dict == dict_expected
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_get_coupling_dict(msw_model, coupled_mf6_model, tmp_path):
+    output_dir = tmp_path / "exchanges"
+
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
+
+    dict_expected = {
+        "mf6_model": "GWF_1",
+        "mf6_msw_node_map": "./exchanges/nodenr2svat.dxc",
+        "mf6_msw_recharge_map": "./exchanges/rchindex2svat.dxc",
+        "mf6_msw_recharge_pkg": "rch_msw",
+        "enable_sprinkling": True,
+        "mf6_msw_well_pkg": "wells_msw",
+        "mf6_msw_sprinkling_map": "./exchanges/wellindex2svat.dxc",
+    }
+
+    coupled_dict = coupled_models._get_coupling_dict(
+        output_dir,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
+
+    assert dict_expected == coupled_dict
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_get_coupling_dict_no_sprinkling(
+    msw_model, coupled_mf6_model, tmp_path
+):
+    output_dir = tmp_path / "exchanges"
+
+    # Remove sprinkling package
+    msw_model.pop("sprinkling")
+
+    coupled_models = MetaMod(
+        msw_model,
+        coupled_mf6_model,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+    dict_expected = {
+        "mf6_model": "GWF_1",
+        "mf6_msw_node_map": "./exchanges/nodenr2svat.dxc",
+        "mf6_msw_recharge_map": "./exchanges/rchindex2svat.dxc",
+        "mf6_msw_recharge_pkg": "rch_msw",
+        "enable_sprinkling": False,
+    }
+
+    coupled_dict = coupled_models._get_coupling_dict(
+        output_dir,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+    assert dict_expected == coupled_dict
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_init_no_sprinkling_fail(msw_model, coupled_mf6_model):
+
+    # Remove sprinkling package
+    msw_model.pop("sprinkling")
+
+    with pytest.raises(ValueError):
+        MetaMod(
+            msw_model,
+            coupled_mf6_model,
+            mf6_rch_pkgkey="rch_msw",
+            mf6_wel_pkgkey="wells_msw",
+        )
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_init_no_mf6_well_fail(msw_model, coupled_mf6_model):
+
+    with pytest.raises(ValueError):
+        MetaMod(
+            msw_model,
+            coupled_mf6_model,
+            mf6_rch_pkgkey="rch_msw",
+            mf6_wel_pkgkey="does_not_exist",
+        )
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_init_no_mf6_well_fail2(msw_model, coupled_mf6_model):
+
+    with pytest.raises(ValueError):
+        MetaMod(
+            msw_model,
+            coupled_mf6_model,
+            mf6_rch_pkgkey="rch_msw",
+            mf6_wel_pkgkey=None,
+        )
+
+
+@pytest.mark.usefixtures("msw_model", "coupled_mf6_model")
+def test_metamod_init_no_mf6_rch_fail(msw_model, coupled_mf6_model):
+
+    with pytest.raises(ValueError):
+        MetaMod(
+            msw_model,
+            coupled_mf6_model,
+            mf6_rch_pkgkey="does_not_exist",
+            mf6_wel_pkgkey="wells_msw",
+        )
