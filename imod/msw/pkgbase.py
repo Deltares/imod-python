3cab6090b14c762fe123fe5dd8fcc91c04328964
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-04-25 13:28:36 +0200

    Reduce line length error messages

--- imod/msw/pkgbase.py
+++ imod/msw/pkgbase.py
@@ -31,15 +31,19 @@ class MetaSwapPackage(abc.ABC):
     def isel(self):
         raise NotImplementedError(
             f"Selection on packages not yet supported. "
-            f"To make a selection on the xr.Dataset, call {self._pkg_id}.dataset.isel instead. "
-            f"You can create a new package with a selection by calling {__class__.__name__}(**{self._pkg_id}.dataset.isel(**selection))"
+            f"To make a selection on the xr.Dataset, "
+            f"call {self._pkg_id}.dataset.isel instead. "
+            f"You can create a new package with a selection by calling: "
+            f"{__class__.__name__}(**{self._pkg_id}.dataset.isel(**selection))"
         )
 
     def sel(self):
         raise NotImplementedError(
             f"Selection on packages not yet supported. "
-            f"To make a selection on the xr.Dataset, call {self._pkg_id}.dataset.sel instead. "
-            f"You can create a new package with a selection by calling {__class__.__name__}(**{self._pkg_id}.dataset.sel(**selection))"
+            f"To make a selection on the xr.Dataset, "
+            f"call {self._pkg_id}.dataset.sel instead. "
+            f"You can create a new package with a selection by calling: "
+            f"{__class__.__name__}(**{self._pkg_id}.dataset.sel(**selection))"
         )
 
     def write(self, directory: Union[str, Path], index: np.ndarray, svat: xr.DataArray):
