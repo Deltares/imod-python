527483fe15b787686d868b0cfca66b8488540ebd
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-04-28 14:23:08 +0200

    review comments

---
+++ tests/test_mf6/test_mf6_advection.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pathlib
+import imod
+import imod.mf6.model
+
+
+def test_advection_upstream():
+
+    directory = pathlib.Path("mymodel")
+    globaltimes = [np.datetime64("2000-01-01")] 
+    a = imod.mf6.AdvectionUpstream()
+    actual = a.render(directory, "adv", globaltimes, True)
+    expected = 'begin options\n  scheme upstream\n\nend options'
+    assert actual == expected
+
+def test_advection_central():
+
+    directory = pathlib.Path("mymodel")
+    globaltimes = [np.datetime64("2000-01-01")] 
+    a = imod.mf6.AdvectionUpstream()
+    actual = a.render(directory, "adv", globaltimes, True)
+    expected = 'begin options\n  scheme central\n\nend options'
+    assert actual == expected
+    
+
+def test_advection_TVD():
+
+    directory = pathlib.Path("mymodel")
+    globaltimes = [np.datetime64("2000-01-01")] 
+    a = imod.mf6.AdvectionTVD()
+    actual = a.render(directory, "adv", globaltimes, True)
+    expected = 'begin options\n  scheme TVD\n\nend options'
+    assert actual == expected
