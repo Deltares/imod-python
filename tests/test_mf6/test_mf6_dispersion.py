2137acb9898e9dbad90c17abbc2d6e143a5c925f
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-04-28 14:23:08 +0200

    review comments

---
+++ tests/test_mf6/test_mf6_dispersion.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pathlib
+import imod
+import imod.mf6.model
+
+def test_difdisp_default():
+    directory = pathlib.Path("mymodel")
+    globaltimes = [np.datetime64("2000-01-01")]      
+    disp = imod.mf6.Dispersion(False, False, 1e-4, 1, 10) 
+    actual =disp.render(directory, "dsp", globaltimes, True)
+    expected=textwrap.dedent(
+      '''\
+      begin options
+      end options
+
+      begin griddata
+        diffc
+          constant 0.0001
+
+        alh
+          constant 1
+
+        ath1
+          constant 10
+      end griddata''')
+      
+    assert actual == expected
+
+
+def test_difdisp_options():
+  directory = pathlib.Path("mymodel")
+  globaltimes = [np.datetime64("2000-01-01")]      
+  disp= imod.mf6.Dispersion(True, True, 1e-4, 1, 10, 1,2,3) 
+  actual =disp.render(directory, "dsp", globaltimes, True)
+  expected=textwrap.dedent(
+      '''\
+      begin options
+       XT3D_OFF
+       XT3D_RHS
+      end options
+
+      begin griddata
+        diffc
+          constant 0.0001
+
+        alh
+          constant 1
+
+        ath1
+          constant 10
+
+        alv
+          constant 1
+
+        ath2
+          constant 2
+
+        atv
+          constant 3
+      end griddata''')
+      
+   
+  print(actual)
+  assert actual == expected
