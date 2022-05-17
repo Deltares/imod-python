de430e1fa8de6b26b79c4696da6b29fe87ff2bb6
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-05-17 16:36:44 +0200

    working on adding tests

--- imod/tests/fixtures/mf6_flow_with_tranpsort_fixture.py
+++ imod/tests/fixtures/mf6_flow_with_tranpsort_fixture.py
@@ -2,16 +2,29 @@ import numpy as np
 import pytest
 import xarray as xr
 
-def get_data_array(nlay, nrow, ncol, globaltimes, dx, dy, xmin, ymin):
+
+globaltimes = [np.datetime64("2000-01-01"),np.datetime64("2000-01-02"),np.datetime64("2000-01-03")]
+
+class grid_dimensions:
+    nlay= 3
+    nrow= 15
+    ncol= 15
+    dx= 5000
+    dy= -5000
+    xmin= 0
+    ymin= 0
+
+
+def get_data_array(dimensions, globaltimes):
     ntimes = len(globaltimes)
-    shape = (ntimes, nlay, nrow, ncol)        
+    shape = (ntimes, dimensions.nlay, dimensions.nrow, dimensions.ncol)        
     dims = ("time", "layer", "y", "x")
 
     layer = np.array([1, 2, 3])
-    xmax = dx * ncol
-    ymax = abs(dy) * nrow    
-    y = np.arange(ymax, ymin, dy) + 0.5 * dy
-    x = np.arange(xmin, xmax, dx) + 0.5 * dx
+    xmax = dimensions.dx * dimensions.ncol
+    ymax = abs(dimensions.dy) * dimensions.nrow    
+    y = np.arange(ymax, dimensions.ymin, dimensions.dy) + 0.5 * dimensions.dy
+    x = np.arange(dimensions.xmin, xmax, dimensions.dx) + 0.5 * dimensions.dx
     coords = {"time": globaltimes, "layer": layer, "y": y, "x": x}
 
     # Discretization data
@@ -19,9 +32,8 @@ def get_data_array(nlay, nrow, ncol, globaltimes, dx, dy, xmin, ymin):
 
 @pytest.fixture(scope="session")
 def head_fc():
-
-    globaltimes = [np.datetime64("2000-01-01"),np.datetime64("2000-01-02"),np.datetime64("2000-01-03")]
-    idomain = get_data_array(3, 15, 15, globaltimes, 5000, -5000, 0, 0)
+    
+    idomain = get_data_array(grid_dimensions(), globaltimes)
 
     # Constant head
     head = xr.full_like(idomain, np.nan)
@@ -30,9 +42,35 @@ def head_fc():
 @pytest.fixture(scope="session")
 def concentration_fc():
 
-    globaltimes = [np.datetime64("2000-01-01"),np.datetime64("2000-01-02"),np.datetime64("2000-01-03")]
-    idomain = get_data_array(3, 15, 15, globaltimes, 5000, -5000, 0, 0)
+    idomain = get_data_array(grid_dimensions (), globaltimes)
     idomain = idomain.expand_dims(species = ["salinity",  "temperature"])
 
     concentration = xr.full_like(idomain, np.nan)
     return concentration
+
+@pytest.fixture(scope="session")
+def conductance_fc():
+    globaltimes = [np.datetime64("2000-01-01"),np.datetime64("2000-01-02"),np.datetime64("2000-01-03")]
+    idomain = get_data_array(grid_dimensions(), globaltimes)
+
+    # Constant head
+    conductance = xr.full_like(idomain, np.nan)
+    return conductance
+
+@pytest.fixture(scope="session")
+def elevation_fc():
+
+    idomain = get_data_array(grid_dimensions(), globaltimes)
+
+    # Constant head
+    elevation = xr.full_like(idomain, np.nan)
+    return elevation
+
+@pytest.fixture(scope="session")
+def rate_fc():
+
+    idomain = get_data_array(grid_dimensions(), globaltimes)
+
+    # Constant head
+    elevation = xr.full_like(idomain, np.nan)
+    return elevation
