9b59571cc651fe115e50813e3d87121404dad8ff
Joeri van Engelen <joerivanengelen@hotmail.com>
2022-06-28 10:09:10 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- imod/mf6/buy.py
+++ imod/mf6/buy.py
@@ -4,7 +4,7 @@ from imod.mf6.pkgbase import Package
 class Buoyancy(Package):
     """
         Buoyancy package. This package must be included when performing variable
-        denisty simulation.
+        density simulation.
 
         Parameters
         ----------
