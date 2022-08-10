6acf718897608362d64bd8437c2700f5e0dcc93b
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-08-10 17:11:08 +0200

    fixed tests

--- imod/tests/test_mf6/test_mf6_lake_api.py
+++ imod/tests/test_mf6/test_mf6_lake_api.py
@@ -83,12 +83,12 @@ def test_lake_api(basic_dis):
         end packagedata
 
         begin connectiondata
-          0 1 5 0.0  0.2 0.4    0.6 0.5
-          0 1 6 0.0  0.2 0.4    0.6 0.5
-          0 1 6 0.0  0.2 0.4    0.6 0.5
           1 1 5 0.0  0.2 0.4    0.6 0.5
           1 1 6 0.0  0.2 0.4    0.6 0.5
           1 1 6 0.0  0.2 0.4    0.6 0.5
+          2 1 5 0.0  0.2 0.4    0.6 0.5
+          2 1 6 0.0  0.2 0.4    0.6 0.5
+          2 1 6 0.0  0.2 0.4    0.6 0.5
         end connectiondata
 
         begin outlets
