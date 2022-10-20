7d01a9ec3879181e30c2ff7ce80282269ad761cb
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-10-20 13:26:22 +0200

    Add support for transmissivity and vertical resistance packages

--- imod/flow/conductivity.py
+++ imod/flow/conductivity.py
@@ -88,3 +88,57 @@ class VerticalAnisotropy(Package):
         self._check_if_nan_in_active_cells(
             active_cells=active_cells, vars_to_check=vars_to_check
         )
+
+
+class Transmissivity(Package):
+    """
+    Transmissivity [L2/T] of the aquifers, between TOP and BOT.
+
+    This variable behaves similar to the transmissivity in MODFLOW 2005's "Block
+    Centred Flow (BCF)" schematization. Note that values are trimmed internally
+    to be minimal 0 m2/day by iMODFLOW.
+
+    Parameters
+    ----------
+    transmissivity : xr.DataArray
+        Transmissivity, dims ``("layer", "y", "x")``.
+    """
+
+    _pkg_id = "kdw"
+    _variable_order = ["transmissivity"]
+
+    def __init__(self, transmissivity=None):
+        super().__init__()
+        self.dataset["transmissivity"] = transmissivity
+
+    def _pkgcheck(self, active_cells=None):
+        vars_to_check = ["transmissivity"]
+        self._check_if_nan_in_active_cells(
+            active_cells=active_cells, vars_to_check=vars_to_check
+        )
+
+
+class VerticalResistance(Package):
+    """
+    Vertical resistance [T] for aquitards (Between BOT and TOP).
+
+    This variable is the inverse of the "VCONT" variable in MODFLOW 2005's
+    "Block Centred Flow (BCF)" schematization. Note that values are trimmed
+    internally for minimal 0.001 days by iMODFLOW. Also not that iMODFLOW's
+    internal scaling cannot deal with value nonzero nodata values, therefore,
+    for scaling, it is important to assign a 0 as the nodata value for
+    VerticalResistance.
+
+    Parameters
+    ----------
+    Resistance : xr.DataArray
+        Resistance, dims ``("layer", "y", "x")``.
+
+    """
+
+    _pkg_id = "vcw"
+    _variable_order = ["resistance"]
+
+    def __init__(self, resistance=None):
+        super().__init__()
+        self.dataset["resistance"] = resistance
