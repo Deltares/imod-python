80f95ef3573056478667dce23676f68847e91860
luitjan <luitjan.slooten@deltares.nl>
2023-04-20 18:20:52 +0200

    cleanup

--- imod/tests/fixtures/package_sanity_structured.py
+++ imod/tests/fixtures/package_sanity_structured.py
@@ -55,39 +55,8 @@ def get_vertices_discretization():
         top=10.0, bottom=bottom, idomain=idomain
     )
 
-def get_idomain():
-    nlay = 3
-    nrow = 15
-    ncol = 15
-    ntimes = 3
-    globaltimes = np.array(
-        [
-            "2000-01-01",
-            "2000-01-02",
-            "2000-01-03",
-        ],
-        dtype="datetime64[ns]",
-    )
-    shape = (ntimes, nlay, nrow, ncol)
-
-    dx = 5000.0
-    dy = -5000.0
-    xmin = 0.0
-    xmax = dx * ncol
-    ymin = 0.0
-    ymax = abs(dy) * nrow
-    dims = ("time", "layer", "y", "x")
-
-    layer = np.array([1, 2, 3])
-    y = np.arange(ymax, ymin, dy) + 0.5 * dy
-    x = np.arange(xmin, xmax, dx) + 0.5 * dx
-    coords = {"time": globaltimes, "layer": layer, "y": y, "x": x}
-
-    # Discretization data
-    return xr.DataArray(np.ones(shape), coords=coords, dims=dims)
-
 def state():
-    idomain = get_idomain()
+    idomain = get_structured_grid_array(np.float64)
 
     # Constant cocnentration
     concentration = xr.full_like(idomain, np.nan)
@@ -138,12 +107,12 @@ def drainage():
 
 
 def elevation_fc():
-    idomain = get_idomain()
+    idomain =  get_structured_grid_array(np.float64)
 
     elevation = xr.full_like(idomain, np.nan)
     return elevation
 def conductance_fc():
-    idomain = get_idomain()
+    idomain = get_structured_grid_array(np.float64)
 
     # Constant head
     conductance = xr.full_like(idomain, np.nan)
