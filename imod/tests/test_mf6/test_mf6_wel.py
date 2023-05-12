4a15864c82a4fcbb59e9efd2608e53e35687bdab
JoerivanEngelen <joerivanengelen@hotmail.com>
2023-05-12 13:17:38 +0200

    Add comments

--- imod/tests/test_mf6/test_mf6_wel.py
+++ imod/tests/test_mf6/test_mf6_wel.py
@@ -137,13 +137,15 @@ def test_to_mf6_pkg__high_lvl_transient(basic_dis, well_high_lvl_test_data_trans
 
 
 def test_clip_box__high_lvl_stationary(well_high_lvl_test_data_stationary):
+    # Arrange
     wel = imod.mf6.Well(*well_high_lvl_test_data_stationary)
 
-    # Test clipping x & y without specified time
+    # Act & Assert
+    ## Test clipping x & y without specified time
     ds = wel.clip_box(x_min=52.0, x_max=76.0, y_max=67.0).dataset
     assert dict(ds.dims) == {"index": 3, "species": 2}
 
-    # Test clipping with z
+    ## Test clipping with z
     ds = wel.clip_box(z_max=-2.0).dataset
     assert dict(ds.dims) == {"index": 4, "species": 2}
     ds = wel.clip_box(z_min=-8.0).dataset
@@ -151,24 +153,26 @@ def test_clip_box__high_lvl_stationary(well_high_lvl_test_data_stationary):
 
 
 def test_clip_box__high_lvl_transient(well_high_lvl_test_data_transient):
+    # Arrange
     wel = imod.mf6.Well(*well_high_lvl_test_data_transient)
 
-    # Test clipping x & y without specified time
+    # Act & Assert
+    ## Test clipping x & y without specified time
     ds = wel.clip_box(x_min=52.0, x_max=76.0, y_max=67.0).dataset
     assert dict(ds.dims) == {"index": 3, "time": 5, "species": 2}
 
-    # Test clipping with z
+    ## Test clipping with z
     ds = wel.clip_box(z_max=-2.0).dataset
     assert dict(ds.dims) == {"index": 4, "time": 5, "species": 2}
     ds = wel.clip_box(z_min=-8.0).dataset
     assert dict(ds.dims) == {"index": 4, "time": 5, "species": 2}
 
-    # Test clipping with specified time
+    ## Test clipping with specified time
     timestr = "2000-01-03"
     ds = wel.clip_box(x_min=52.0, x_max=76.0, y_max=67.0, time_min=timestr).dataset
     assert dict(ds.dims) == {"index": 3, "time": 3, "species": 2}
 
-    # Test clipping with specified time inbetween timesteps
+    ## Test clipping with specified time inbetween timesteps
     timestr = "2000-01-03 18:00:00"
     ds = wel.clip_box(x_min=52.0, x_max=76.0, y_max=67.0, time_min=timestr).dataset
     assert dict(ds.dims) == {"index": 3, "time": 3, "species": 2}
