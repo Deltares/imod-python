4d0183cb2cfb527e70695639747dd66a7e4ecc63
JoerivanEngelen <joerivanengelen@hotmail.com>
2023-05-17 15:35:02 +0200

    Deal with review comments

--- imod/mf6/pkgbase_mf6.py
+++ imod/mf6/pkgbase_mf6.py
@@ -6,7 +6,14 @@ instead of x, y coordinates.
 
 We plan to split up the present attributes of the classes in pkgbase.py
 (Package and BoundaryCondition) into low-level and high-level classes.
+
+The high-level class contains grids with x, y, z coordinates, closely linked to
+GIS systems. The low-level classes contain a dataset based on cellid,
+consisting of layer, row, and column, closely resembling input for Modflow6.
 """
+
+import abc
+from pathlib import Path
 
 import numpy as np
 import xarray as xr
@@ -15,26 +22,27 @@ from imod.mf6.pkgbase import BoundaryCondition
 
 
 # FUTURE:
-# Make Mf6Bc inherit Mf6Pkg, not BoundaryCondition.
-# Move functions from BoundaryCondition or Package,
+# TODO: Make Mf6BoundaryCondition inherit Mf6Package, not BoundaryCondition.
+# TODO: Move functions from BoundaryCondition or Package,
 # which are required to work with low-level data to either Mf6Bc or Mf6Pkg
-class Mf6Bc(BoundaryCondition):
+class Mf6BoundaryCondition(abc.ABC, BoundaryCondition):
     """
-    Low-level package to share methods for specific modflow 6 packages with
-    time component.
+    Package resembling input for Modflow 6 List Input. This abstract base class
+    methods for specific modflow 6 packages with time component.
 
     It is not meant to be used directly, only to inherit from, to implement new
     packages.
 
     This class only supports `list input
     <https://water.usgs.gov/water-resources/software/MODFLOW-6/mf6io_6.0.4.pdf#page=19>`_,
-    not the array input which is used in :class:`Mf6Pkg`.
+    not the array input which is used in :class:`Mf6Package`.
     """
 
+    # TODO: Method can be moved outside class.
     # TODO: Rename to `to_struct_array`
     # NOTE: This function should be part of Mf6Pkg, now defined here,
     #   to ensure function is overrided.
-    def to_sparse(self, ds):
+    def to_sparse(self, ds: xr.Dataset):
         data_vars = [var for var in ds.data_vars if var != "cellid"]
         cellid_names = ds.coords["nmax_cellid"].values
         nrow = ds.coords["ncellid"].size
@@ -51,7 +59,7 @@ class Mf6Bc(BoundaryCondition):
             recarr[var] = ds[var]
         return recarr
 
-    def write_datafile(self, outpath, ds, binary):
+    def write_datafile(self, outpath: Path, ds: xr.Dataset, binary: bool):
         """
         Writes a modflow6 binary data file
         """
@@ -63,7 +71,7 @@ class Mf6Bc(BoundaryCondition):
             self._write_textfile(outpath, sparse_data)
 
     @classmethod
-    def from_file(cls, path) -> "Mf6Bc":
+    def from_file(cls, path) -> "Mf6BoundaryCondition":
         """
         Instantiate class from modflow 6 file
         """
@@ -83,13 +91,13 @@ def remove_inactive(ds: xr.Dataset, active: xr.DataArray) -> xr.Dataset:
         Grid with active cells.
     """
 
-    def unstack_columns(a):
+    def unstack_columns(array):
         # Unstack columns:
         # https://stackoverflow.com/questions/64097426/is-there-unstack-in-numpy
         # Make sure to use tuples, since these get the special treatment
         # which we require for the indexing:
         # https://numpy.org/doc/stable/user/basics.indexing.html#dealing-with-variable-numbers-of-indices-within-programs
-        return tuple(np.moveaxis(a, -1, 0))
+        return tuple(np.moveaxis(array, -1, 0))
 
     if "cellid" not in ds.data_vars:
         raise ValueError("Missing variable 'cellid' in dataset")
