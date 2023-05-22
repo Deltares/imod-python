7fe3ac4333f854bf61eb23add062bd21d2212a4d
JoerivanEngelen <joerivanengelen@hotmail.com>
2023-05-22 15:03:27 +0200

    Add typedispatching

--- imod/select/grid.py
+++ imod/select/grid.py
@@ -3,6 +3,7 @@ from typing import List, Union
 import numpy as np
 import xarray as xr
 import xugrid as xu
+from fastcore import typedispatch
 from scipy.ndimage import binary_dilation
 
 from imod.mf6.validation import BOUNDARY_DIMS_SCHEMA
@@ -14,6 +15,13 @@ def reduce_grid_except_dims(
 ) -> Union[xr.DataArray, xu.UgridDataArray]:
     to_reduce = {dim: 0 for dim in grid.dims if dim not in preserve_dims}
     return grid.isel(**to_reduce)
+
+
+def __validate_grid(grid):
+    # Validate if required dimensions are present
+    schemata = [BOUNDARY_DIMS_SCHEMA, DTypeSchema(np.bool_)]
+    for schema in schemata:
+        schema.validate(grid)
 
 
 def grid_boundary_xy(
@@ -34,23 +42,29 @@ def grid_boundary_xy(
     {xarray.DataArray, xugrid.UgridDataArray}
         2d grid with locations of grid boundaries
     """
+    return __grid_boundary_xy(grid)
 
-    # Validate if required dimensions are present
-    schemata = [BOUNDARY_DIMS_SCHEMA, DTypeSchema(np.bool_)]
-    for schema in schemata:
-        schema.validate(grid)
 
-    if isinstance(grid, xu.UgridDataArray):
-        like_2d = reduce_grid_except_dims(grid, [grid.grid.face_dimension])
-        zeros_grid = xu.zeros_like(like_2d)
-        return zeros_grid.ugrid.binary_dilation(border_value=1)
-    elif isinstance(grid, xr.DataArray):
-        like_2d = reduce_grid_except_dims(grid, ["x", "y"])
-        boundary_grid = xr.zeros_like(like_2d)
-        boundary_grid.values = binary_dilation(boundary_grid, border_value=1)
-        return boundary_grid
-    else:
-        raise TypeError("Grid should be of type DataArray or UgridDataArray.")
+@typedispatch
+def __grid_boundary_xy(grid: object) -> None:
+    raise TypeError("Grid should be of type DataArray or UgridDataArray.")
+
+
+@typedispatch
+def __grid_boundary_xy(grid: xr.DataArray) -> xr.DataArray:
+    __validate_grid(grid)
+    like_2d = reduce_grid_except_dims(grid, ["x", "y"])
+    boundary_grid = xr.zeros_like(like_2d)
+    boundary_grid.values = binary_dilation(boundary_grid, border_value=1)
+    return boundary_grid
+
+
+@typedispatch
+def __grid_boundary_xy(grid: xu.UgridDataArray) -> xu.UgridDataArray:
+    __validate_grid(grid)
+    like_2d = reduce_grid_except_dims(grid, [grid.grid.face_dimension])
+    zeros_grid = xu.zeros_like(like_2d)
+    return zeros_grid.ugrid.binary_dilation(border_value=1)
 
 
 def active_grid_boundary_xy(
