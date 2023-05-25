2f9f428cb53570dd266b3d27a49e70fbf3a898a0
JoerivanEngelen <joerivanengelen@hotmail.com>
2023-05-25 16:06:59 +0200

    Private function get single underscore

--- imod/select/grid.py
+++ imod/select/grid.py
@@ -10,14 +10,14 @@ from imod.mf6.validation import BOUNDARY_DIMS_SCHEMA
 from imod.schemata import DTypeSchema
 
 
-def reduce_grid_except_dims(
+def _reduce_grid_except_dims(
     grid: Union[xr.DataArray, xu.UgridDataArray], preserve_dims: List[str]
 ) -> Union[xr.DataArray, xu.UgridDataArray]:
     to_reduce = {dim: 0 for dim in grid.dims if dim not in preserve_dims}
     return grid.isel(**to_reduce)
 
 
-def __validate_grid(grid):
+def _validate_grid(grid):
     # Validate if required dimensions are present
     schemata = [BOUNDARY_DIMS_SCHEMA, DTypeSchema(np.bool_)]
     for schema in schemata:
@@ -42,27 +42,27 @@ def grid_boundary_xy(
     {xarray.DataArray, xugrid.UgridDataArray}
         2d grid with locations of grid boundaries
     """
-    return __grid_boundary_xy(grid)
+    return _grid_boundary_xy(grid)
 
 
 @typedispatch
-def __grid_boundary_xy(grid: object) -> None:
+def _grid_boundary_xy(grid: object) -> None:
     raise TypeError("Grid should be of type DataArray or UgridDataArray.")
 
 
 @typedispatch
-def __grid_boundary_xy(grid: xr.DataArray) -> xr.DataArray:
-    __validate_grid(grid)
-    like_2d = reduce_grid_except_dims(grid, ["x", "y"])
+def _grid_boundary_xy(grid: xr.DataArray) -> xr.DataArray:
+    _validate_grid(grid)
+    like_2d = _reduce_grid_except_dims(grid, ["x", "y"])
     boundary_grid = xr.zeros_like(like_2d)
     boundary_grid.values = binary_dilation(boundary_grid, border_value=1)
     return boundary_grid
 
 
 @typedispatch
-def __grid_boundary_xy(grid: xu.UgridDataArray) -> xu.UgridDataArray:
-    __validate_grid(grid)
-    like_2d = reduce_grid_except_dims(grid, [grid.grid.face_dimension])
+def _grid_boundary_xy(grid: xu.UgridDataArray) -> xu.UgridDataArray:
+    _validate_grid(grid)
+    like_2d = _reduce_grid_except_dims(grid, [grid.grid.face_dimension])
     zeros_grid = xu.zeros_like(like_2d)
     return zeros_grid.ugrid.binary_dilation(border_value=1)
 
