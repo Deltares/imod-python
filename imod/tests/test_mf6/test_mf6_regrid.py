1b9fa441b8e5192ebdccc397015c4147f3fee603
luitjan <luitjan.slooten@deltares.nl>
2023-05-25 14:22:48 +0200

    added unstructured discretization to test

--- imod/tests/test_mf6/test_mf6_regrid.py
+++ imod/tests/test_mf6/test_mf6_regrid.py
@@ -2,11 +2,10 @@ import numpy as np
 import pytest
 import xarray as xr
 import xugrid as xu
-
 import imod
 
 
-def grid_structured(dtype, value, cellsize) -> xr.DataArray:
+def grid_data_structured(dtype, value, cellsize) -> xr.DataArray:
     """
     This function creates a dataarray with scalar values for a grid of configurable cell size.
     """
@@ -27,9 +26,9 @@ def grid_structured(dtype, value, cellsize) -> xr.DataArray:
     return da
 
 
-def grid_structured_layered(dtype, value, cellsize) -> xr.DataArray:
+def grid_data_structured_layered(dtype, value, cellsize) -> xr.DataArray:
     """
-    This function creates a dataarray with scalar values for a grid of configurable cell size. The vlaues are
+    This function creates a dataarray with scalar values for a grid of configurable cell size. The values are
     multiplied with the layer index.
     """
     horizontal_range = 10
@@ -50,52 +49,78 @@ def grid_structured_layered(dtype, value, cellsize) -> xr.DataArray:
         da.loc[dict(layer=ilayer)] = layer_value
     return da
 
+def grid_data_unstructured(dtype, value, cellsize) -> xu.UgridDataArray:
+    """
+    This function creates a dataarray with scalar values for a grid of configurable cell size. 
+    First a regular grid is constructed and then this is converted to an ugrid dataarray.
+    """        
+    return  xu.UgridDataArray.from_structured(grid_data_structured(dtype, value, cellsize))
 
-structured_grid_packages = [
+def grid_data_unstructured_layered(dtype, value, cellsize) -> xu.UgridDataArray:
+    """
+    This function creates a dataarray with scalar values for a grid of configurable cell size. The values are
+    multiplied with the layer index. First a regular grid is constructed and then this is converted to an ugrid dataarray.
+    """    
+    return  xu.UgridDataArray.from_structured(grid_data_structured_layered(dtype, value, cellsize))
+
+def create_package_instances(is_structured):
+    grid_data_function = grid_data_structured if is_structured else grid_data_unstructured
+    grid_data_function_layered = grid_data_structured_layered if is_structured else  grid_data_unstructured_layered
+
+    packages = [
     imod.mf6.NodePropertyFlow(
-        icelltype=grid_structured(np.int_, 1, 5.0),
-        k=grid_structured(np.float64, 12, 5.0),
+        icelltype=grid_data_function(np.int_, 1, 5.0),
+        k=grid_data_function(np.float64, 12, 5.0),
         k22=3.0,
     ),
-    imod.mf6.StructuredDiscretization(
-        top=20.0,
-        bottom=grid_structured_layered(np.float_, -1, 5.0),
-        idomain=grid_structured(np.int_, 1, 5.0),
-    ),
+
     imod.mf6.SpecificStorage(
-        specific_storage=grid_structured(np.float_, 1.0e-4, 5.0),
-        specific_yield=grid_structured(np.float_, 0.15, 5.0),
+        specific_storage=grid_data_function(np.float_, 1.0e-4, 5.0),
+        specific_yield=grid_data_function(np.float_, 0.15, 5.0),
         convertible=0,
         transient=False,
     ),
     imod.mf6.StorageCoefficient(
-        storage_coefficient=grid_structured(np.float_, 1.0e-4, 5.0),
-        specific_yield=grid_structured(np.float_, 0.15, 5.0),
-        convertible=grid_structured(np.int32, 0, 5.0),
+        storage_coefficient=grid_data_function(np.float_, 1.0e-4, 5.0),
+        specific_yield=grid_data_function(np.float_, 0.15, 5.0),
+        convertible=grid_data_function(np.int32, 0, 5.0),
         transient=True,
     ),
     imod.mf6.Drainage(
-        elevation=grid_structured(np.float_, 1.0e-4, 5.0),
-        conductance=grid_structured(np.float_, 1.0e-4, 5.0),
+        elevation=grid_data_function(np.float_, 1.0e-4, 5.0),
+        conductance=grid_data_function(np.float_, 1.0e-4, 5.0),
         print_input=True,
         print_flows=True,
         save_flows=True,
     ),
     imod.mf6.ConstantHead(
-        grid_structured(np.float_, 1.0e-4, 5.0),
+        grid_data_function(np.float_, 1.0e-4, 5.0),
         print_input=True,
         print_flows=True,
         save_flows=True,
-    ),
-]
-
+        ),
+    ]
+    if is_structured:
+        packages.append(imod.mf6.StructuredDiscretization(
+        top=20.0,
+        bottom=grid_data_function_layered(np.float_, -1, 5.0),
+        idomain=grid_data_function(np.int_, 1, 5.0),
+    ))
+    else:
+        packages.append(imod.mf6.VerticesDiscretization(
+        top=20.,
+        bottom=grid_data_function_layered(np.float_, -1, 5.0),
+        idomain=grid_data_function(np.int_, 1, 5.0),
+    ))
+    return packages
 
 def test_regrid_structured():
     """
     This test regrids a structured grid to another structured grid of the same size.
     Some of the arrays are entered as grids and others as scalars
     """
-    new_grid = grid_structured(np.float64, 12, 2.5)
+    structured_grid_packages =create_package_instances(is_structured=  True)
+    new_grid = grid_data_structured(np.float64, 12, 2.5)
     new_packages = []
     for package in structured_grid_packages:
         new_packages.append(package.regrid_like(new_grid))
@@ -109,12 +134,36 @@ def test_regrid_structured():
         assert len(errors) == 0
 
 
+def test_regrid_unstructured():
+    """
+    This test regrids a structured grid to another structured grid of the same size.
+    Some of the arrays are entered as grids and others as scalars
+    """
+    unstructured_grid_packages =create_package_instances(is_structured=  False)    
+    new_grid = grid_data_unstructured(np.float64, 12, 2.5)
+    new_packages = []
+    for package in unstructured_grid_packages:
+        new_packages.append(package.regrid_like(new_grid))
+
+    new_idomain = new_packages[0].dataset["icelltype"]
+    for new_package in new_packages:
+        #package write validation crashes for VerticesDiscretization so we skip that one
+        if type(new_package).__name__ is not "VerticesDiscretization":
+            errors = new_package._validate(
+                new_package._write_schemata,
+                idomain=new_idomain,
+            )            
+            assert len(errors) == 0
+        else:
+            continue 
+
+
 def test_regrid_structured_missing_dx_and_dy():
     """
     In imod-python it is not mandatory for data-arrays to have a dx and dy coordinate, but for
     xugrid this is mandatory
     """
-    icelltype = grid_structured(np.int_, 1, 0.5)
+    icelltype = grid_data_structured(np.int_, 1, 0.5)
     icelltype = icelltype.drop_vars(["dx", "dy"])
     package = imod.mf6.NodePropertyFlow(
         icelltype=icelltype,
@@ -122,7 +171,7 @@ def test_regrid_structured_missing_dx_and_dy():
         k22=3.0,
     )
 
-    new_grid = grid_structured(np.float64, 12, 0.25)
+    new_grid = grid_data_structured(np.float64, 12, 0.25)
 
     with pytest.raises(
         ValueError,
