4040d4d088a0da3278be8c74dfaa4684be1ee195
luitjan <luitjan.slooten@deltares.nl>
2023-06-01 12:57:08 +0200

    both tests work

--- imod/tests/fixtures/mf6_regridding_fixture.py
+++ imod/tests/fixtures/mf6_regridding_fixture.py
@@ -1,6 +1,9 @@
 import numpy as np
 import xarray as xr
 import xugrid as xu
+import imod
+import pytest
+from typing import Union
 
 def grid_data_structured(dtype, value, cellsize) -> xr.DataArray:
     """
@@ -64,3 +67,69 @@ def grid_data_unstructured_layered(dtype, value, cellsize) -> xu.UgridDataArray:
     return xu.UgridDataArray.from_structured(
         grid_data_structured_layered(dtype, value, cellsize)
     )
+
+def make_model_from_idomain(grid_data_function, cellsize: float):
+    gwf_model = imod.mf6.GroundwaterFlowModel()
+
+    grid_data_function(np.float64, 1, cellsize)
+    constant_head = grid_data_function(np.float64, 1, cellsize)
+    gwf_model["chd"] = imod.mf6.ConstantHead(
+        constant_head, print_input=True, print_flows=True, save_flows=True
+    )
+    gwf_model["ic"] = imod.mf6.InitialConditions(start=0.0)
+
+    icelltype = grid_data_function(np.int32, 1, cellsize)
+    k = grid_data_function(np.float64, 1.23, cellsize)
+    k33 = k.copy()
+
+    gwf_model["npf"] = imod.mf6.NodePropertyFlow(
+        icelltype=icelltype,
+        k=k,
+        k33=k33,
+        save_flows=True,
+    )
+    gwf_model["sto"] = imod.mf6.SpecificStorage(
+        specific_storage=grid_data_function(np.float64, 0.002, cellsize),
+        specific_yield=0.15,
+        transient=False,
+        convertible=0,
+    )
+    gwf_model["oc"] = imod.mf6.OutputControl(save_head="all", save_budget="all")
+    rch_rate_all =grid_data_function(np.float64, 0.002, cellsize)
+    rch_rate = rch_rate_all.sel(layer=[1])
+
+  
+    gwf_model["rch"] = imod.mf6.Recharge(rch_rate)
+
+    return gwf_model
+
+
+@pytest.fixture(scope="session")
+def structured_flow_model() -> imod.mf6.GroundwaterFlowModel:
+    cellsize = 2.0
+
+    idomain = grid_data_structured(np.int32, 1, cellsize)
+    gwf_model = make_model_from_idomain(grid_data_structured, cellsize)
+
+    bottom =grid_data_structured_layered(np.float64, -1.0,cellsize)    
+
+    gwf_model["disv"] = imod.mf6.StructuredDiscretization(
+        top=10.0, bottom=bottom, idomain=idomain
+    )
+    return gwf_model
+
+
+
+@pytest.fixture(scope="session")
+def unstructured_flow_model() -> imod.mf6.GroundwaterFlowModel:
+    cellsize = 2.0
+
+    idomain = grid_data_unstructured(np.int32, 1, cellsize)
+    gwf_model = make_model_from_idomain(grid_data_unstructured, cellsize)
+
+    bottom =grid_data_unstructured_layered(np.float64, -1.0,cellsize)    
+    
+    gwf_model["disv"] = imod.mf6.VerticesDiscretization(
+        top=10.0, bottom=bottom, idomain=idomain
+    )
+    return gwf_model
--- imod/tests/test_mf6/test_mf6_regrid_model.py
+++ imod/tests/test_mf6/test_mf6_regrid_model.py
@@ -3,62 +3,25 @@ import imod
 import xarray as xr
 import xugrid as xu
 import numpy as np
-from imod.tests.fixtures.mf6_regridding_fixture import grid_data_structured, grid_data_structured_layered
+from imod.tests.fixtures.mf6_regridding_fixture import grid_data_structured, grid_data_unstructured
 
-def test_regrid_model():
-    cellsize = 2.0
-
-    idomain = grid_data_structured(np.int32, 1, cellsize)
-
-    icelltype = xr.full_like(idomain, 0)
-    k = xr.full_like(idomain, 1.0, dtype=float)
-    k33 = k.copy()
-    rch_rate = xr.full_like(idomain.sel(layer=1), 0.001, dtype=float)
-    bottom =grid_data_structured_layered(np.float64, -1.0,cellsize)
-    # %%
-    # All the data above have been constants over the grid. For the constant head
-    # boundary, we'd like to only set values on the external border. We can
-    # `py:method:xugrid.UgridDataset.binary_dilation` to easily find these cells:
-
-    chd_location = xr.zeros_like(idomain.sel(layer=2), dtype=bool)
-    constant_head = xr.full_like(idomain.sel(layer=2), 1.0, dtype=float).where(chd_location)
-
-
-    # %%
-    # Write the model
-    # ---------------
-    #
-    # The first step is to define an empty model, the parameters and boundary
-    # conditions are added in the form of the familiar MODFLOW packages.
-
-    gwf_model = imod.mf6.GroundwaterFlowModel()
-    gwf_model["disv"] = imod.mf6.StructuredDiscretization(
-        top=10.0, bottom=bottom, idomain=idomain
-    )
-    gwf_model["chd"] = imod.mf6.ConstantHead(
-        constant_head, print_input=True, print_flows=True, save_flows=True
-    )
-    gwf_model["ic"] = imod.mf6.InitialConditions(start=0.0)
-    gwf_model["npf"] = imod.mf6.NodePropertyFlow(
-        icelltype=icelltype,
-        k=k,
-        k33=k33,
-        save_flows=True,
-    )
-    gwf_model["sto"] = imod.mf6.SpecificStorage(
-        specific_storage=1.0e-5,
-        specific_yield=0.15,
-        transient=False,
-        convertible=0,
-    )
-    gwf_model["oc"] = imod.mf6.OutputControl(save_head="all", save_budget="all")
-    gwf_model["rch"] = imod.mf6.Recharge(rch_rate)
-
-#make new grid
+def test_regrid_structured_model_to_structured_model(structured_flow_model):
+ 
     finer_idomain = grid_data_structured(np.int32, 1, 0.4)
-    new_gwf_model = gwf_model.regrid_like(finer_idomain)
-    assert len(new_gwf_model.items()) == len (gwf_model.items())
+
+    new_gwf_model = structured_flow_model.regrid_like(finer_idomain)
+
+    assert len(new_gwf_model.items()) == len (structured_flow_model.items())
     validation_result =  new_gwf_model._validate("test_model")
     assert not validation_result.has_errors()
 
 
+def test_regrid_unstructured_model_to_unstructured_model(unstructured_flow_model):
+ 
+    finer_idomain = grid_data_unstructured(np.int32, 1, 0.4)
+
+    new_gwf_model = unstructured_flow_model.regrid_like(finer_idomain)
+
+    assert len(new_gwf_model.items()) == len (unstructured_flow_model.items())
+    validation_result = new_gwf_model._validate("test_model")
+    assert not validation_result.has_errors()
