41331cf42d239f44cf23efe7b3fa903daff36494
luitjan <luitjan.slooten@deltares.nl>
2023-06-02 12:17:05 +0200

    merge from 371

--- imod/tests/fixtures/mf6_regridding_fixture.py
+++ imod/tests/fixtures/mf6_regridding_fixture.py
@@ -22,9 +22,11 @@ def grid_data_structured(dtype, value, cellsize) -> xr.DataArray:
 
     coords = {"layer": layer, "y": y, "x": x, "dx": cellsize, "dy": cellsize}
 
-    da = xr.DataArray(np.ones(shape, dtype=dtype) * value, coords=coords, dims=dims)
+    structured_grid_data = xr.DataArray(
+        np.ones(shape, dtype=dtype) * value, coords=coords, dims=dims
+    )
 
-    return da
+    return structured_grid_data
 
 
 def grid_data_structured_layered(dtype, value, cellsize) -> xr.DataArray:
@@ -44,11 +46,13 @@ def grid_data_structured_layered(dtype, value, cellsize) -> xr.DataArray:
 
     coords = {"layer": layer, "y": y, "x": x, "dx": cellsize, "dy": cellsize}
 
-    da = xr.DataArray(np.ones(shape, dtype=dtype), coords=coords, dims=dims)
+    unstructured_grid_data = xr.DataArray(
+        np.ones(shape, dtype=dtype), coords=coords, dims=dims
+    )
     for ilayer in range(1, nlayer + 1):
         layer_value = ilayer * value
-        da.loc[dict(layer=ilayer)] = layer_value
-    return da
+        unstructured_grid_data.loc[dict(layer=ilayer)] = layer_value
+    return unstructured_grid_data
 
 
 def grid_data_unstructured(dtype, value, cellsize) -> xu.UgridDataArray:
@@ -69,7 +73,6 @@ def grid_data_unstructured_layered(dtype, value, cellsize) -> xu.UgridDataArray:
     return xu.UgridDataArray.from_structured(
         grid_data_structured_layered(dtype, value, cellsize)
     )
-
 
 def make_model_from_idomain(grid_data_function, cellsize: float):
     gwf_model = imod.mf6.GroundwaterFlowModel()
