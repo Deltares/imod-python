0e6082760d850e48ff4383ad0d4939f69ff076da
luitjan <luitjan.slooten@deltares.nl>
2023-06-05 11:59:28 +0200

    review comments

--- imod/tests/fixtures/mf6_regridding_fixture.py
+++ imod/tests/fixtures/mf6_regridding_fixture.py
@@ -40,21 +40,8 @@ def grid_data_structured_layered(
     This function creates a dataarray with scalar values for a grid of configurable cell size. The values are
     multiplied with the layer index.
     """
-    horizontal_range = 10
-    y = np.arange(horizontal_range, -cellsize, -cellsize)
-    x = np.arange(0, horizontal_range + cellsize, cellsize)
-
-    nlayer = 3
-
-    shape = nlayer, len(x), len(y)
-    dims = ("layer", "y", "x")
-    layer = np.arange(1, nlayer + 1)
-
-    coords = {"layer": layer, "y": y, "x": x, "dx": cellsize, "dy": cellsize}
-
-    unstructured_grid_data = xr.DataArray(
-        np.ones(shape, dtype=dtype), coords=coords, dims=dims
-    )
+    unstructured_grid_data = grid_data_structured(dtype, value, cellsize)
+    nlayer = unstructured_grid_data.coords["layer"].max()
     for ilayer in range(1, nlayer + 1):
         layer_value = ilayer * value
         unstructured_grid_data.loc[dict(layer=ilayer)] = layer_value
