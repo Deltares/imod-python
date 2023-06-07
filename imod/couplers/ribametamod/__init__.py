2a68287722a205c9d5df602184562477040e7f70
Hofer-Julian <julianhofer@gnome.org>
2023-06-07 12:25:17 +0200

    Add ribametamod

---
+++ imod/couplers/ribametamod/__init__.py
@@ -0,0 +1,6 @@
+from imod.couplers.ribametamod.ribametamod import RibaMetaMod
+from imod.couplers.metamod.node_svat_mapping import NodeSvatMapping
+from imod.couplers.metamod.rch_svat_mapping import RechargeSvatMapping
+from imod.couplers.metamod.wel_svat_mapping import WellSvatMapping
+
+__all__ = ["RibaMetaMod", "NodeSvatMapping", "RechargeSvatMapping", "WellSvatMapping"]
