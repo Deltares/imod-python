466bfaa8abddd902e42cf1a0c6a364e81bd743e1
Sunny Titus <sunny.titus@deltares.nl>
2023-06-08 08:54:59 +0000

    Add ClippedBoundaryConditionCreator class
    This commit adds a ClippedBoundaryConditionCreator class. This class is used to create a ConstantHeadBoundary condition based on already existing ConstantHeadBoundary conditions. It determines which boundaries don't have a BC yet and assigns a ConstantHeadBoundary condition based on a `state_for_boundary` input
    
    In #420 the ClippedBoundaryConditionCreator is hooked up to the main code

---
+++ imod/tests/test_mf6/test_mf6_clipped_boundary_condition_creator.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+import xugrid as xu
+
+from imod.mf6 import ConstantHead
+from imod.mf6.clipped_boundary_condition_creator import ClippedBoundaryConditionCreator
+
+
+def _get_boundaries(idomain):
+    return xu.zeros_like(idomain.sel(layer=1), dtype=bool).ugrid.binary_dilation(
+        border_value=True
+    )
+
+
+class TestClippedBoundaryConditionCreator:
+    @pytest.mark.parametrize("n_clipped_cells", [15, 0, 36])
+    def test_create(self, basic_unstructured_dis, n_clipped_cells):
+        # Arrange.
+        idomain, _, _ = basic_unstructured_dis
+
+        head_value_original_domain = 1.0
+        head_value_clipped_domain = 2.0
+
+        original_boundary_location = _get_boundaries(idomain)
+        original_boundary_location[
+            original_boundary_location.size - n_clipped_cells :
+        ] = False
+
+        original_boundary_values = xu.full_like(
+            idomain, head_value_original_domain, dtype=float
+        ).where(original_boundary_location)
+
+        original_boundary_constant_head_pkg = ConstantHead(
+            original_boundary_values,
+            print_input=True,
+            print_flows=True,
+            save_flows=True,
+        )
+
+        clipped_boundary_values = xu.full_like(
+            idomain, head_value_clipped_domain, dtype=float
+        )
+
+        # Act.
+        constant_head_pkg_clipped_domain = ClippedBoundaryConditionCreator.create(
+            idomain, clipped_boundary_values, [original_boundary_constant_head_pkg]
+        )
+
+        # Assert.
+        result_clipped_head = constant_head_pkg_clipped_domain["head"]
+
+        for layer_index in range(1, 15):
+            number_clipped_head_locations = np.count_nonzero(
+                ~np.isnan(result_clipped_head.sel(layer=layer_index))
+            )
+            assert number_clipped_head_locations is n_clipped_cells
