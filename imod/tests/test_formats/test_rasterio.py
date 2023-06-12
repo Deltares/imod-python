cf8816332d630914bc5b6bf106df42eb57c6212d
Huite Bootsma <huitebootsma@gmail.com>
2023-06-12 11:49:01 +0000

    Lighten dependencies: Move imports and create fallback ascii write function

--- imod/tests/test_formats/test_rasterio.py
+++ imod/tests/test_formats/test_rasterio.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import pytest
 import rasterio
@@ -66,3 +68,64 @@ def test_saveopen__nodata_dtype(test_da, tmp_path):
     daback = imod.rasterio.open(tmp_path / "onlyminus1_i32.tif").load()
     assert daback.dtype == np.int32
     assert (daback == -1).all()
+
+
+@pytest.mark.parametrize(
+    "value,nodata,dtype,precision,digits",
+    [
+        (1, -999, np.int32, None, None),
+        (1.0, -999.0, np.float32, None, None),
+        (1.0, np.nan, np.float32, None, None),
+        (1.0, -999.9, np.float32, None, None),
+        (1.0, -999.99, np.float32, None, None),
+        (1.01, -999.99, np.float32, None, None),
+        # Decimal precision
+        (1, -999, np.int32, 3, None),
+        (1.0, -999.99, np.float32, 2, None),
+        (1.0, np.nan, np.float32, 2, None),
+        (1.01, np.nan, np.float32, 2, None),
+        # Significant digits
+        (1, -999, np.int32, None, 3),
+        (1.0, -999.99, np.float32, None, 4),
+        (1.0, np.nan, np.float32, None, 5),
+        (1.01, np.nan, np.float32, None, 5),
+    ],
+)
+def test_rasterio_ascii(test_da, tmp_path, value, nodata, dtype, precision, digits):
+    def write_rasterio(path, a, profile):
+        with rasterio.Env():
+            with rasterio.open(path, "w", **profile) as ds:
+                ds.write(a, 1)
+        return
+
+    def assert_equal_content(path_a, path_b):
+        assert Path(path_a).exists()
+        assert Path(path_b).exists()
+        with open(path_a, "r") as f:
+            content_a = f.read()
+        with open(path_b, "r") as f:
+            content_b = f.read()
+        assert content_a == content_b
+
+    da = xr.full_like(test_da, value)
+    profile = {
+        "transform": imod.util.transform(da),
+        "driver": "AAIGrid",
+        "height": da.y.size,
+        "width": da.x.size,
+        "count": 1,
+        "dtype": dtype,
+        "nodata": nodata,
+    }
+    if precision is not None:
+        profile["decimal_precision"] = precision
+    if digits is not None:
+        profile["significant_digits"] = digits
+
+    path_a = tmp_path / "expected.asc"
+    path_b = tmp_path / "actual.asc"
+
+    write_rasterio(path_a, da.values, profile)
+    imod.rasterio.write_aaigrid(path_b, da.values, profile)
+
+    assert_equal_content(path_a, path_b)
