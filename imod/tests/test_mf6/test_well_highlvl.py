065ed04d512dfc3225efd294acaad76e96845f28
Sunny Titus <sunny.titus@deltares.nl>
2023-06-21 10:08:09 +0200

    Fix failing unit test

--- imod/tests/test_mf6/test_well_highlvl.py
+++ imod/tests/test_mf6/test_well_highlvl.py
@@ -45,10 +45,8 @@ def test_write_well(tmp_path: Path):
         tmp_path / "packagename" / "wel.dat", sep=":-\\s*", engine="python"
     )
 
-    # TODO: The following output is wrong, but it is what the write function currently generates.
-    # issue gitlab  #432 was created to fix this.
     reference_output = np.array(
-        [["1 2 1 2"], ["1 2 1 2"], ["1 2 1 2"], ["1 2 1 2"], ["1 2 1 2"], ["1 2 1 2"]],
+        [["1 2 1 1"], ["1 2 1 2"], ["1 2 2 2"], ["2 2 1 1"], ["2 2 2 2"], ["3 2 2 1"]],
         dtype=object,
     )
     assert (df.values == reference_output).all()
