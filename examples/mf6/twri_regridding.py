f954f4bb1e3f2657a90c8d1851a0dad8ac18f58a
luitjan <luitjan.slooten@deltares.nl>
2023-06-27 16:18:39 +0200

    review comments

--- examples/mf6/twri_regridding.py
+++ examples/mf6/twri_regridding.py
@@ -7,11 +7,11 @@ More information about this model can be found in an example dedicated to buildi
 
 In overview, we'll set the following steps:
 * we build a new grid, onto which we want to regrid the twri model
-* we regrid the model then in 3 different ways
-* first by regridding the simulation itself. This automatically regrids the model and all the packages in the model using
+* we regrid the model in 3 different ways
+** first by regridding the simulation itself. This automatically regrids the model and all the packages in the model using
 default regridding methods for each field in each package
-* then we show how a model within a simulation can be regridded, also using default regridding methods
-* finally we show how regridding can be done package per package. We illustrate that for one package, and we use a non-default
+** second, we show how a model within a simulation can be regridded, also using default regridding methods
+** third, we show how regridding can be done package per package. We illustrate that for one package, and we use a non-default
 regridding method for the horizontal conductivity field
 
 """
@@ -27,13 +27,13 @@ from example_models import create_twri_simulation
 from imod.mf6.regridding_utils import RegridderType
 
 # %%
-# now we create the twri simulation itself. It yields a simulation of a flow problem, with a grid of 3 layers and 15 cells in both x and y directions.
+# Now we create the twri simulation itself. It yields a simulation of a flow problem, with a grid of 3 layers and 15 cells in both x and y directions.
 # To better illustrate the regridding, we replace the K field with a lognormal random K field. The original k-field is a constant per layer.
 simulation = create_twri_simulation()
 
 idomain = simulation["GWF_1"]["dis"]["idomain"]
 heterogeneous_k = xr.zeros_like(idomain, dtype=np.double)
-heterogeneous_k.values = np.random.lognormal(-2, 2, heterogeneous_k.shape)
+heterogeneous_k.values = np.random.lognormal(4.0, 2.0, heterogeneous_k.shape)
 simulation["GWF_1"]["npf"]["k"] = heterogeneous_k
 
 # %%
@@ -42,7 +42,7 @@ fig, ax = plt.subplots()
 heterogeneous_k.sel(layer=1).plot(y="y", yincrease=False, ax=ax)
 
 # %%
-# now we create a new grid for this simulation. It has 3 layers,  45 rows and 20 columns.
+# Now we create a new grid for this simulation. It has 3 layers,  45 rows and 20 columns.
 # The length of the domain is slightly different from the input grid. That had a coordinate difference between the first and last cellcentre on the
 # x axis and y axis of  15*5000 = 75000 on both axes, but the new grid that is  75020 on the x axis and  75015 on the y axis
 
@@ -66,7 +66,7 @@ coords = {"layer": layer, "y": y, "x": x, "dx": dx, "dy": dy}
 target_grid = xr.DataArray(np.ones(shape, dtype=int), coords=coords, dims=dims)
 
 # %%
-# a first way to regrid the twri model is to regrid the whole simulation object. This is the most straightforward method,
+# A first way to regrid the twri model is to regrid the whole simulation object. This is the most straightforward method,
 # and it uses default regridding methods for each input field. To see which ones are used, look at the _regrid_method
 # class attribute of the relevant package. For example the _regrid_method attribute  of the NodePropertyFlow package
 # specifies that field "k" uses an OVERLAP regridder in combination with the averaging function "geometric_mean".
@@ -80,7 +80,7 @@ fig, ax = plt.subplots()
 regridded_k_1.sel(layer=1).plot(y="y", yincrease=False, ax=ax)
 
 # %%
-# a second way to regrid  twri  is to regrid the groundwater flow model.
+# A second way to regrid  twri  is to regrid the groundwater flow model.
 
 model = simulation["GWF_1"]
 new_model = model.regrid_like(target_grid)
@@ -91,7 +91,7 @@ regridded_k_2.sel(layer=1).plot(y="y", yincrease=False, ax=ax)
 
 
 # %%
-# finally, we can regrid package per package. This allows us to choose the regridding method as well.
+# Finally, we can regrid package per package. This allows us to choose the regridding method as well.
 # in this example we'll regrid the npf package manually and the rest of the packages using default methods.
 
 regridder_types = {"k": (RegridderType.CENTROIDLOCATOR, None)}
@@ -104,4 +104,4 @@ new_model["npf"] = npf_regridded
 regridded_k_3 = new_model["npf"]["k"]
 fig, ax = plt.subplots()
 regridded_k_3.sel(layer=1).plot(y="y", yincrease=False, ax=ax)
-pass  # break here to see the plots
+pass  # Break here to see the plots
