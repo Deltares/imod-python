5192e498ccff56cd05f861d50c08cb508f0f22f7
luitjan <luitjan.slooten@deltares.nl>
2023-07-03 12:05:53 +0200

    created test

--- imod/tests/test_mf6/test_mf6_hfb.py
+++ imod/tests/test_mf6/test_mf6_hfb.py
@@ -3,67 +3,28 @@ import geopandas as gpd
 import numpy as np
 import shapely
 import xugrid as xu
+import pytest
 
-def test_hfb_resistance(tmp_path, circle_model):
+@pytest.mark.parametrize("hfb_specialization",[imod.mf6.HorizontalFlowBarrierResistance, imod.mf6.HorizontalFlowBarrierMultiplier, imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic] )
+def test_hfb_writing_1layer(hfb_specialization, tmp_path, unstructured_flow_model):
     
-    ibound= circle_model["GWF_1"]["disv"]["idomain"]
-    x = [10.0, 20.0, 20.0, 30.0, 30.0, 40.0, 40.0, 50.0]
-    y = x[::-1]
-
-    indices = np.repeat(np.arange(4), 2)
+    idomain= unstructured_flow_model["disv"]["idomain"]
+    # x = [9.0, 3.0, 3.0, 5.0, 5.0, 7.0, 7.0, 9.0]
+    #y = x[::-1]
+    x = [0. ,2. ,2., 3., 3., 5.]
+    y = [5., 5. ,5., 5., 5., 5.]
+    indices = np.repeat(np.arange(3), 2)
     linestrings = shapely.linestrings(x, y, indices=indices)
     lines = gpd.GeoDataFrame(geometry=linestrings)
-    lines["resistance"] = 10.0
+    lines["linedata"] = 10.0
 
     ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
 
-    c =ugrid_1d["resistance"]
-    c=c.expand_dims("layer")
-    c=c.assign_coords(layer=[1])
+    line_as_dataarray =ugrid_1d["linedata"]
+    line_as_dataarray=line_as_dataarray.expand_dims("layer")
+    line_as_dataarray=line_as_dataarray.assign_coords(layer=[1])
 
-    hfb = imod.mf6.HorizontalFlowBarrierResistance(c, ibound )
+    hfb = hfb_specialization(line_as_dataarray, idomain )
     hfb.render(tmp_path, "hfb", None, False)
     hfb.write(tmp_path, "hfb", None, False)
-    
-def test_hfb_multiplier(tmp_path, circle_model):
-    
-    idomain= circle_model["GWF_1"]["disv"]["idomain"]
-    x = [10.0, 20.0, 20.0, 30.0, 30.0, 40.0, 40.0, 50.0]
-    y = x[::-1]
-
-    indices = np.repeat(np.arange(4), 2)
-    linestrings = shapely.linestrings(x, y, indices=indices)
-    lines = gpd.GeoDataFrame(geometry=linestrings)
-    lines["resistance"] = 10.0
-
-    ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
-
-    c =ugrid_1d["resistance"]
-    c=c.expand_dims("layer")
-    c=c.assign_coords(layer=[1])
-
-    hfb = imod.mf6.HorizontalFlowBarrierMultiplier(c, idomain )
-    hfb.render(tmp_path, "hfb", None, False)
-    hfb.write(tmp_path, "hfb", None, False)
-    
-def test_hfb_hydraulic_characteristic(tmp_path, circle_model):
-
-    idomain= circle_model["GWF_1"]["disv"]["idomain"]
-    x = [10.0, 20.0, 20.0, 30.0, 30.0, 40.0, 40.0, 50.0]
-    y = x[::-1]
-
-    indices = np.repeat(np.arange(4), 2)
-    linestrings = shapely.linestrings(x, y, indices=indices)
-    lines = gpd.GeoDataFrame(geometry=linestrings)
-    lines["resistance"] = 10.0
-
-    ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
-
-    c =ugrid_1d["resistance"]
-    c=c.expand_dims("layer")
-    c=c.assign_coords(layer=[1])
-
-    hfb = imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic(c, idomain )
-    hfb.render(tmp_path, "hfb", None, False)
-    hfb.write(tmp_path, "hfb", None, False)
-    
+ 
