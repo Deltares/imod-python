3f4c2a9486d8b733e3ccf72c69e8fc89e1988202
luitjan <luitjan.slooten@deltares.nl>
2023-07-05 13:31:15 +0200

    created test for hfb

--- imod/tests/test_mf6/test_mf6_hfb.py
+++ imod/tests/test_mf6/test_mf6_hfb.py
@@ -1,18 +1,25 @@
-import imod
 import geopandas as gpd
 import numpy as np
+import pytest
 import shapely
 import xugrid as xu
-import pytest
 
-@pytest.mark.parametrize("hfb_specialization",[imod.mf6.HorizontalFlowBarrierResistance, imod.mf6.HorizontalFlowBarrierMultiplier, imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic] )
+import imod
+
+
+@pytest.mark.parametrize(
+    "hfb_specialization",
+    [
+        imod.mf6.HorizontalFlowBarrierResistance,
+        imod.mf6.HorizontalFlowBarrierMultiplier,
+        imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic,
+    ],
+)
 def test_hfb_writing_1layer(hfb_specialization, tmp_path, unstructured_flow_model):
-    
-    idomain= unstructured_flow_model["disv"]["idomain"]
-    # x = [9.0, 3.0, 3.0, 5.0, 5.0, 7.0, 7.0, 9.0]
-    #y = x[::-1]
-    x = [0. ,2. ,2., 3., 3., 5.]
-    y = [5., 5. ,5., 5., 5., 5.]
+    idomain = unstructured_flow_model["disv"]["idomain"]
+
+    x = [0.0, 2.0, 2.0, 3.0, 3.0, 5.0]
+    y = [5.1, 51.0, 5.1, 5.1, 5.1, 5.1]
     indices = np.repeat(np.arange(3), 2)
     linestrings = shapely.linestrings(x, y, indices=indices)
     lines = gpd.GeoDataFrame(geometry=linestrings)
@@ -20,11 +27,10 @@ def test_hfb_writing_1layer(hfb_specialization, tmp_path, unstructured_flow_mode
 
     ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
 
-    line_as_dataarray =ugrid_1d["linedata"]
-    line_as_dataarray=line_as_dataarray.expand_dims("layer")
-    line_as_dataarray=line_as_dataarray.assign_coords(layer=[1])
+    line_as_dataarray = ugrid_1d["linedata"]
+    line_as_dataarray = line_as_dataarray.expand_dims("layer")
+    line_as_dataarray = line_as_dataarray.assign_coords(layer=[1])
 
-    hfb = hfb_specialization(line_as_dataarray, idomain )
+    hfb = hfb_specialization(line_as_dataarray, idomain)
     hfb.render(tmp_path, "hfb", None, False)
     hfb.write(tmp_path, "hfb", None, False)
- 
