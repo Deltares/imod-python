5b1e9112ecdd7f88d76ef62ab1c64aaaa3408c5b
Sunny Titus <sunny.titus@deltares.nl>
2023-07-10 17:06:15 +0200

    ﻿Cleanup PkgBase, Package and Well classes. Move methods to where they are being used. Make methods private if they are only used within a class

--- imod/mf6/wel.py
+++ imod/mf6/wel.py
@@ -422,7 +422,7 @@ class Well(BoundaryCondition):
         ds_vars = self.__create_dataset_vars(wells_assigned, wells_df, ds["cellid"])
         ds = ds.assign(**dict(ds_vars.items()))
 
-        ds = remove_inactive(ds, active)
+        ds = _remove_inactive(ds, active)
 
         return Mf6Wel(**ds)
 
@@ -757,3 +757,35 @@ class WellDisVertices(DisVerticesBoundaryCondition):
         # The super method will select in the time dimension without issues.
         new = super().clip_box(time_min=time_min, time_max=time_max)
         return new
+
+
+def _remove_inactive(ds: xr.Dataset, active: xr.DataArray) -> xr.Dataset:
+    """
+    Drop list-based input cells in inactive cells.
+
+    Parameters
+    ----------
+    ds: xr.Dataset
+        Dataset with list-based input. Needs "cellid" variable.
+    active: xr.DataArray
+        Grid with active cells.
+    """
+
+    def unstack_columns(array):
+        # Unstack columns:
+        # https://stackoverflow.com/questions/64097426/is-there-unstack-in-numpy
+        # Make sure to use tuples, since these get the special treatment
+        # which we require for the indexing:
+        # https://numpy.org/doc/stable/user/basics.indexing.html#dealing-with-variable-numbers-of-indices-within-programs
+        return tuple(np.moveaxis(array, -1, 0))
+
+    if "cellid" not in ds.data_vars:
+        raise ValueError("Missing variable 'cellid' in dataset")
+    if "ncellid" not in ds.dims:
+        raise ValueError("Missing dimension 'ncellid' in dataset")
+
+    cellid_zero_based = ds["cellid"].values - 1
+    cellid_indexes = unstack_columns(cellid_zero_based)
+    valid = active.values[cellid_indexes].astype(bool)
+
+    return ds.loc[{"ncellid": valid}]
