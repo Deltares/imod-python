94d19abb0b51b6048fe6df7fbfc6dc2bd0714bbe
Joeri van Engelen <joerivanengelen@hotmail.com>
2023-07-21 18:42:01 +0200

    Better define tests

--- imod/tests/test_mf6/test_mf6_hfb.py
+++ imod/tests/test_mf6/test_mf6_hfb.py
@@ -3,8 +3,35 @@ import numpy as np
 import pytest
 import shapely
 import xugrid as xu
+import textwrap
+import pathlib
 
 import imod
+
+
+@pytest.fixture
+def hfb_data_one_layer():
+    """
+    Line at cell edges of unstructured flow model
+    """
+
+    x = [-1.0, 1.0, 1.0, 3.0, 3.0, 5.0]
+    y = [5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
+    indices = np.repeat(np.arange(3), 2)
+    linestrings = shapely.linestrings(x, y, indices=indices)
+    lines = gpd.GeoDataFrame(geometry=linestrings)
+    lines["linedata"] = 10.0
+
+    # TODO: This doesn't work?
+    ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
+    # But this does?
+    ugrid_1d, lines_snapped = xu.snap_to_grid(lines, idomain.sel(layer=1), 0.2)
+
+    line_as_dataarray = ugrid_1d["linedata"]
+    line_as_dataarray = line_as_dataarray.expand_dims("layer")
+    line_as_dataarray = line_as_dataarray.assign_coords(layer=[1])
+
+    return line_as_dataarray
 
 
 @pytest.mark.parametrize(
@@ -15,22 +42,64 @@ import imod
         imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic,
     ],
 )
-def test_hfb_writing_1layer(hfb_specialization, tmp_path, unstructured_flow_model):
+def test_hfb_render_one_layer(
+    hfb_specialization, tmp_path, unstructured_flow_model, hfb_data_one_layer
+):
+    directory = pathlib.Path("mymodel")
+
+    # Arrange
     idomain = unstructured_flow_model["disv"]["idomain"]
+    hfb = hfb_specialization(hfb_data_one_layer, idomain)
 
-    x = [0.0, 2.0, 2.0, 3.0, 3.0, 5.0]
-    y = [5.1, 5.1, 5.1, 5.1, 5.1, 5.1]
-    indices = np.repeat(np.arange(3), 2)
-    linestrings = shapely.linestrings(x, y, indices=indices)
-    lines = gpd.GeoDataFrame(geometry=linestrings)
-    lines["linedata"] = 10.0
+    expected = textwrap.dedent(
+        """\
+        begin options
 
-    ugrid_1d = xu.UgridDataset.from_geodataframe(lines)
+        end options
 
-    line_as_dataarray = ugrid_1d["linedata"]
-    line_as_dataarray = line_as_dataarray.expand_dims("layer")
-    line_as_dataarray = line_as_dataarray.assign_coords(layer=[1])
+        begin dimensions
+        maxhfb 3
+        end dimensions
 
-    hfb = hfb_specialization(line_as_dataarray, idomain)
-    hfb.render(tmp_path, "hfb", None, False)
+        begin period 1
+        open/close mymodel/hfb/hfb.dat
+        end period
+        """
+    )
+
+    # Act
+    actual = hfb.render(directory, "hfb", None, False)
+
+    # Assert
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "hfb_specialization",
+    [
+        imod.mf6.HorizontalFlowBarrierResistance,
+        imod.mf6.HorizontalFlowBarrierMultiplier,
+        imod.mf6.HorizontalFlowBarrierHydraulicCharacteristic,
+    ],
+)
+def test_hfb_writing_one_layer(
+    hfb_specialization, tmp_path, unstructured_flow_model, hfb_data_one_layer
+):
+    # Arrange
+    idomain = unstructured_flow_model["disv"]["idomain"]
+    hfb = hfb_specialization(hfb_data_one_layer, idomain)
+
+    expected_hfb_data = np.array(
+        [
+            [1.0, 13.0, 1.0, 19.0, 0.1],
+            [1.0, 14.0, 1.0, 20.0, 0.1],
+            [1.0, 15.0, 1.0, 21.0, 0.1],
+        ]
+    )
+
+    # Act
     hfb.write(tmp_path, "hfb", None, False)
+
+    # Assert
+    data = np.loadtxt(tmp_path / "hfb" / "hfb.dat")
+    np.testing.assert_almost_equal(data, expected_hfb_data)
