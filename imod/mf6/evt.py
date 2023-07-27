a413c90f63c1894c299ff7293c012b50368c6705
luitjan <luitjan.slooten@deltares.nl>
2023-07-27 17:25:35 +0200

    isort

--- imod/mf6/evt.py
+++ imod/mf6/evt.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from imod.mf6.boundary_condition import BoundaryCondition
+from imod.mf6.regridding_utils import RegridderType
 from imod.mf6.validation import BOUNDARY_DIMS_SCHEMA
 from imod.schemata import (
     AllInsideNoDataSchema,
@@ -12,7 +13,6 @@ from imod.schemata import (
     IndexesSchema,
     OtherCoordsSchema,
 )
-from imod.mf6.regridding_utils import RegridderType
 
 
 class Evapotranspiration(BoundaryCondition):
