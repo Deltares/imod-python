7cb0b020159d200c3310c406b378fe09b899b9b8
Joeri van Engelen <joerivanengelen@hotmail.com>
2023-09-05 17:01:00 +0000

    Add method to clip well outside grid

--- imod/mf6/mf6_wel_adapter.py
+++ imod/mf6/mf6_wel_adapter.py
@@ -17,6 +17,10 @@ import numpy as np
 from imod.mf6.boundary_condition import BoundaryCondition
 from imod.schemata import DTypeSchema
 
+# FUTURE: There was an idea to autogenerate modflow 6 adapters.
+# This was relevant:
+# https://github.com/Deltares/xugrid/blob/main/xugrid/core/wrap.py#L90
+
 
 class Mf6Wel(BoundaryCondition):
     """
