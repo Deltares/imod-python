a11457c45b63c78066e08485fc0c170891cb8f48
Sunny Titus <sunny.titus@deltares.nl>
2023-09-08 14:30:26 +0200

    Gitlab #529 Deactive cells that don't belong in a sliced submodel
    When a structured grid is sliced, it may still contain cells that don't belong to the active domain.
    In this commit those cells are deactivated.
    The create_domain_slices method has been renamed to create_partition_info and now provides a list of SubmodelPartitionInfo. This datastruct consists next to the domain slices also the label_id and the active idomain. The latter is used in the _slice_package method to deactivate cells that don't belong in the sliced domain.
    Further more the unit tests have been cleaned and the unit test for the structured grid as been updated

--- imod/tests/test_mf6/test_mf6_simulation.py
+++ imod/tests/test_mf6/test_mf6_simulation.py
@@ -8,9 +8,11 @@ import xugrid as xu
 
 import imod
 from imod.mf6.model import Modflow6Model
+from imod.mf6.modelsplitter import SubmodelPartitionInfo
 from imod.mf6.statusinfo import StatusInfo
 from imod.mf6.utilities.simulation_utilities import get_models, get_packages
 from imod.schemata import ValidationError
+from imod.typing.grid import ones_like
 
 
 def roundtrip(simulation, tmpdir_factory, name):
@@ -154,15 +156,35 @@ class TestModflow6Simulation:
         domain1 = submodel_labels.coords['mesh2d_nFaces'].where(submodel_labels == 0).dropna('mesh2d_nFaces')
         domain2 = submodel_labels.coords['mesh2d_nFaces'].where(submodel_labels == 1).dropna('mesh2d_nFaces')
 
-        expected_slice_model_calls = [({'mesh2d_nFaces': domain1.values}, model_mock1),
-                                      ({'mesh2d_nFaces': domain1.values}, model_mock2),
-                                      ({'mesh2d_nFaces': domain2.values}, model_mock1),
-                                      ({'mesh2d_nFaces': domain2.values}, model_mock2)]
+        expected_slice_model_calls = [(SubmodelPartitionInfo(label_id=0, slice={'mesh2d_nFaces': domain1.values},
+                                                             active_domain=ones_like(domain1)), model_mock1),
+                                      (SubmodelPartitionInfo(label_id=0, slice={'mesh2d_nFaces': domain1.values},
+                                                             active_domain=ones_like(domain1)), model_mock2),
+                                      (SubmodelPartitionInfo(label_id=1, slice={'mesh2d_nFaces': domain2.values},
+                                                             active_domain=ones_like(domain2)), model_mock1),
+                                      (SubmodelPartitionInfo(label_id=1, slice={'mesh2d_nFaces': domain2.values},
+                                                             active_domain=ones_like(domain2)), model_mock2)]
 
         for expected_call in expected_slice_model_calls:
             assert any(
-                np.array_equal(expected_call[0]['mesh2d_nFaces'], call_args[0][0]['mesh2d_nFaces'])
+                compare_submodel_partition_info(expected_call[0], call_args[0][0])
                 and (expected_call[1] is call_args[0][1])
                 for call_args in slice_model_mock.call_args_list
             )
         # fmt: on
+
+
+def compare_submodel_partition_info(
+    first: SubmodelPartitionInfo, second: SubmodelPartitionInfo
+):
+    return (
+        (first.label_id == second.label_id)
+        and compare_dict_with_array(first.slice, second.slice)
+        and np.array_equal(first.active_domain, second.active_domain)
+    )
+
+
+def compare_dict_with_array(first, second):
+    if first.keys() != second.keys():
+        return False
+    return all(np.array_equal(first[key], second[key]) for key in first)
