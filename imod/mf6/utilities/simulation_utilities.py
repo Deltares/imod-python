0f795d525cbec875e1d4f26e7f4d88a9d03260f2
Sunny Titus <sunny.titus@deltares.nl>
2023-09-19 21:12:07 +0200

    Fix flake8 error

--- imod/mf6/utilities/simulation_utilities.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING, Dict
-
-from imod.mf6.model import Modflow6Model
-from imod.mf6.package import Package
-
-if TYPE_CHECKING:
-    from imod.mf6 import Modflow6Simulation
