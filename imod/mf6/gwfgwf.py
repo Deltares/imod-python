f3fd268ed7d5ac41c22f375fffaf24c56bbd3e54
Sunny Titus <sunny.titus@deltares.nl>
2023-09-26 08:39:40 +0000

    Gitlab #539 Create GWF-GWF exchanges for structured grids
    In this commit the GWF-GWF exchanges are created for a simulation with a structured grid that is partitioned using a domain_label array. A separate `ExchangeCreator` class has been created to do so. In this class xarray's diff method is used to find the boundaries of the subdomain. With this information a connected cells dataframe can be created. The dataframe contains information about the connected cells in terms of the global grid. Therefor a mapping between the global grid cells and local grid cells also are being determined. With those 2 inplace the derivation of the exchanges can be created.
    
    This commit does not write the exchange files. This will be done in #540 This commit does not compute the geometric coefficients. That will be done in #325 This commit only works for structured grids. Unstructured support will be added in #541

---
+++ imod/mf6/gwfgwf.py
@@ -0,0 +1,14 @@
+class Exchange:
+    pass
+
+
+class GWFGWF(Exchange):
+    _type = "GWF6-GWF6"
+
+    def __init__(self, model_id1, model_id2, cell_id1, cell_id2, layer):
+        self._filename = ""
+        self._model_name1 = model_id1
+        self._model_name2 = model_id2
+        self._cell_id1 = cell_id1
+        self._cell_id2 = cell_id2
+        self._layer = layer
