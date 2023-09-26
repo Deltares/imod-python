d15b20cee32a19ee18e76f5ebd2c281296c7867a
Sunny Titus <sunny.titus@deltares.nl>
2023-09-26 10:11:46 +0200

    Apply review comments

--- imod/mf6/exchange_creator.py
+++ imod/mf6/exchange_creator.py
@@ -1,13 +1,28 @@
+from typing import List
+
 import numpy as np
 import pandas as pd
 import xarray as xr
 
 from imod.mf6.gwfgwf import GWFGWF
+from imod.mf6.modelsplitter import PartitionInfo
 from imod.mf6.utilities.grid_utilities import get_active_domain_slice
+from imod.typing.grid import GridDataArray
 
 
 class ExchangeCreator:
-    def __init__(self, submodel_labels, partition_info):
+    """
+    Creates the GroundWaterFlow to GroundWaterFlow exchange definitions as a function of a submodel label array and a
+    PartitionInfo object. At the moment only structured grids are supported
+
+    The submodel_labels array should have the same topology as the domain being partitioned. The array will be used
+    to determine the connectivity of the submodels after the split operation has been performed.
+
+    """
+
+    def __init__(
+        self, submodel_labels: GridDataArray, partition_info: List[PartitionInfo]
+    ):
         self._submodel_labels = submodel_labels
 
         self._global_cell_indices = _to_cell_idx(submodel_labels)
@@ -16,7 +31,27 @@ class ExchangeCreator:
             self._create_global_cellidx_to_local_cellid_mapping(partition_info)
         )
 
-    def create_exchanges(self, model_name, layers):
+    def create_exchanges(self, model_name: str, layers: GridDataArray):
+        """
+        Create GroundWaterFlow-GroundWaterFlow exchanges based on the submodel_labels array provided in the class
+        constructor. The layer parameter is used to extrude the cell connection through all the layers. An exchange
+        contains:
+        - the model names of the connected submodel
+        - the local cell id of the first model
+        - the local cell id of the second model
+        - the layer on which the connected cells reside
+
+         For each connection between submodels only a single exchange is created. So if an exchange of model1 to
+         model2 is created then no exchange for model2 to model1 will be created.
+
+         Add the moment the geometric coefficients aren't computed.
+
+
+        Returns
+        -------
+        a list of GWFGWF-exchanges
+
+        """
         layers = layers.to_dataframe()
 
         exchanges = []
@@ -68,7 +103,7 @@ class ExchangeCreator:
 
         return pd.merge(connected_cells_along_x, connected_cells_along_y, how="outer")
 
-    def _find_connected_cells_along_axis(self, axis_label):
+    def _find_connected_cells_along_axis(self, axis_label: str):
         diff1 = self._submodel_labels.diff(f"{axis_label}", label="lower")
         diff2 = self._submodel_labels.diff(f"{axis_label}", label="upper")
 
@@ -97,7 +132,9 @@ class ExchangeCreator:
 
         return connected_cell_info
 
-    def _create_global_cellidx_to_local_cellid_mapping(self, partition_info):
+    def _create_global_cellidx_to_local_cellid_mapping(
+        self, partition_info: List[PartitionInfo]
+    ):
         global_to_local_idx = _create_global_to_local_idx(
             partition_info, self._global_cell_indices
         )
@@ -113,7 +150,9 @@ class ExchangeCreator:
         return mapping
 
 
-def _create_global_to_local_idx(partition_info, global_cell_indices):
+def _create_global_to_local_idx(
+    partition_info: List[PartitionInfo], global_cell_indices: GridDataArray
+):
     global_to_local_idx = {}
     for submodel_partition_info in partition_info:
         local_cell_indices = _get_local_cell_indices(submodel_partition_info)
@@ -156,14 +195,14 @@ def _local_cell_idx_to_id(partition_info):
     return local_cell_idx_to_id
 
 
-def _get_local_cell_indices(submodel_partition_info):
+def _get_local_cell_indices(submodel_partition_info: PartitionInfo):
     domain_slice = get_active_domain_slice(submodel_partition_info.active_domain)
     local_domain = submodel_partition_info.active_domain.sel(domain_slice)
 
     return _to_cell_idx(local_domain)
 
 
-def _to_cell_idx(idomain):
+def _to_cell_idx(idomain: GridDataArray):
     index = np.arange(idomain.size).reshape(idomain.shape)
     domain_index = xr.zeros_like(idomain)
     domain_index.values = index
