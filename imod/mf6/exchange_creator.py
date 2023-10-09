313c30490b0ed12be5df52b8a9135c32aff3d4b1
Sunny Titus <sunny.titus@deltares.nl>
2023-10-09 21:04:15 +0000

    Gitlab #573 Clean up exchange creator
    In this commit the exchange creator and its derived classes are cleaned up. The following has been done:
    
    * Generalized `_local_cell_idx_to_id` method to remove code duplication
    * Generalized `_to_xarray` methods to remove code duplication.
    * Move `_adjust_gridblock_indexing` to the base class and convert it to a free function
    * Removed unused `_find_connected_cells_along_axis` from the structured child class

--- imod/mf6/exchange_creator.py
+++ imod/mf6/exchange_creator.py
@@ -8,25 +8,64 @@ import xarray as xr
 from imod.mf6.gwfgwf import GWFGWF
 from imod.mf6.modelsplitter import PartitionInfo
 from imod.mf6.utilities.grid_utilities import get_active_domain_slice, to_cell_idx
-from imod.typing.grid import GridDataArray, is_unstructured
+from imod.typing.grid import GridDataArray
+
+
+def _adjust_gridblock_indexing(connected_cells: xr.Dataset) -> xr.Dataset:
+    """
+    adjusts the gridblock numbering from 0-based to 1-based.
+    """
+    connected_cells["cell_id1"].values = connected_cells["cell_id1"].values + 1
+    connected_cells["cell_id2"].values = connected_cells["cell_id2"].values + 1
+    return connected_cells
 
 
 class ExchangeCreator(abc.ABC):
     """
-    Creates the GroundWaterFlow to GroundWaterFlow exchange package (gwfgwf) as a function of a submodel label array and a
-    PartitionInfo object. This file contains the cell indices of coupled cells. With coupled cells we mean cells that are adjacent but
-    that are located in different subdomains.  At the moment only structured grids are supported, for unstructured grids the geometric information
-    is still set to default values.
+    Creates the GroundWaterFlow to GroundWaterFlow exchange package (gwfgwf) as a function of a submodel label array
+    and a PartitionInfo object. This file contains the cell indices of coupled cells. With coupled cells we mean
+    cells that are adjacent but that are located in different subdomains.  At the moment only structured grids are
+    supported, for unstructured grids the geometric information is still set to default values.
 
     The submodel_labels array should have the same topology as the domain being partitioned. The array will be used
     to determine the connectivity of the submodels after the split operation has been performed.
 
     """
 
-    @classmethod
-    def _to_xarray(cls, connected_cells: pd.DataFrame) -> xr.Dataset:
+    def _to_xarray(self, connected_cells: pd.DataFrame) -> xr.Dataset:
+        """
+        converts a panda dataframe with exchange data to a xarray dataset. The
+        dataframe must have columns called cell_id1 and cell_id2: indices of
+        cells that are part of the exchange boundary (the subdomain boundary, on
+        both sides of the boundary)
+        """
+        dataset = connected_cells.to_xarray()
+
+        coord_names1 = np.char.add(self._coordinate_names, "_1")
+        dataset["cell_id1"] = xr.DataArray(
+            np.array(list(zip(*connected_cells["cell_id1"]))).T,
+            dims=("index", "cell_dims1"),
+            coords={"cell_dims1": coord_names1},
+        )
+
+        coord_names2 = np.char.add(self._coordinate_names, "_2")
+        dataset["cell_id2"] = xr.DataArray(
+            np.array(list(zip(*connected_cells["cell_id2"]))).T,
+            dims=("index", "cell_dims2"),
+            coords={"cell_dims2": coord_names2},
+        )
+        return dataset
+
+    @property
+    @abc.abstractmethod
+    def _coordinate_names(self):
+        """
+        abstract method that returns the names of the coordinates e.g. ["row", "column"] for structured grids or
+        ["cell_index"] for usntructured grids
+        """
         raise NotImplementedError
 
+    @abc.abstractmethod
     def _find_connected_cells(self) -> pd.DataFrame:
         """
         abstract method that creates a dataframe with the cell indices and subdomain indices of cells that are on the
@@ -35,12 +74,7 @@ class ExchangeCreator(abc.ABC):
         """
         raise NotImplementedError
 
-    def _adjust_gridblock_indexing(self, connected_cells: xr.Dataset) -> xr.Dataset:
-        """
-        abstract method that adjusts the gridblock numbering from 0-based to 1-based.
-        """
-        return connected_cells
-
+    @abc.abstractmethod
     def _compute_geometric_information(self) -> pd.DataFrame:
         """
         abstract method that computes the geometric information needed for the gwfgwf files such as
@@ -49,11 +83,13 @@ class ExchangeCreator(abc.ABC):
         raise NotImplementedError
 
     @classmethod
+    @abc.abstractmethod
     def _create_global_to_local_idx(
         cls, partition_info: List[PartitionInfo], global_cell_indices: GridDataArray
     ) -> Dict[int, pd.DataFrame]:
         """
-        abstract method that creates for each partition a maping from global cell indices to local cells in that partition.
+        abstract method that creates for each partition a mapping from global cell indices to local cells in that
+        partition.
         """
         raise NotImplementedError
 
@@ -133,7 +169,7 @@ class ExchangeCreator(abc.ABC):
 
                 connected_cells_dataset = self._to_xarray(connected_cells)
 
-                self._adjust_gridblock_indexing(connected_cells_dataset)
+                _adjust_gridblock_indexing(connected_cells_dataset)
 
                 exchanges.append(
                     GWFGWF(
@@ -175,29 +211,16 @@ class ExchangeCreator(abc.ABC):
     def _local_cell_idx_to_id(cls, partition_info) -> Dict[int, pd.DataFrame]:
         local_cell_idx_to_id = {}
         for submodel_partition_info in partition_info:
+            model_id = submodel_partition_info.id
+
             local_cell_indices = cls._get_local_cell_indices(submodel_partition_info)
+            local_cell_id = list(np.ndindex(local_cell_indices.shape))
 
-            if is_unstructured(local_cell_indices):
-                model_id = submodel_partition_info.id
-                local_cell_idx_to_id[model_id] = pd.DataFrame(
-                    {
-                        "local_idx": local_cell_indices,
-                        "local_cell_id": local_cell_indices,
-                    }
-                )
-            else:
-                local_row, local_column = np.unravel_index(
-                    local_cell_indices, local_cell_indices.shape
-                )
-
-                model_id = submodel_partition_info.id
-                local_cell_idx_to_id[model_id] = pd.DataFrame(
-                    {
-                        "local_idx": local_cell_indices.values.flatten(),
-                        "local_cell_id": zip(
-                            local_row.flatten() + 1, local_column.flatten() + 1
-                        ),
-                    }
-                )
+            local_cell_idx_to_id[model_id] = pd.DataFrame(
+                {
+                    "local_idx": local_cell_indices.values.flatten(),
+                    "local_cell_id": local_cell_id,
+                }
+            )
 
         return local_cell_idx_to_id
