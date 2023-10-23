e30857c33ce37208475724ed22614c7a0917e52d
luitjan <luitjan.slooten@deltares.nl>
2023-10-23 16:03:31 +0200

    cleanup

--- imod/mf6/partitioned_simulation_postprocessing.py
+++ imod/mf6/partitioned_simulation_postprocessing.py
@@ -4,7 +4,7 @@ import imod
 from imod.mf6.model import GroundwaterFlowModel
 from imod.mf6.simulation import Modflow6Simulation
 from imod.typing.grid import GridDataArray, merge, is_unstructured, merge_to_dataset
-from typing import List
+from typing import List, Dict
 
 
 def merge_heads(simulation_dir: Path, simulation: Modflow6Simulation) -> GridDataArray:
@@ -56,7 +56,7 @@ def _get_single_file(model_directory: Path, extension: str) -> Path:
         )    
     return candidate_files[0]
 
-def merge_balances(simulation_dir: Path, simulation:Modflow6Simulation) -> GridDataArray:
+def merge_balances(simulation_dir: Path, simulation:Modflow6Simulation) -> Dict[str,GridDataArray]:
     """
     This function merges the head output of a split simulation into a single
     head file. Both structured and unstructured grids are supported.
@@ -65,19 +65,19 @@ def merge_balances(simulation_dir: Path, simulation:Modflow6Simulation) -> GridD
     model_names = list(
         simulation.get_models_of_type(GroundwaterFlowModel._model_id).keys()
     )
-    allKeys = set()
+    unique_balance_keys = set()
     cbc_per_partition = []
     for modelname in model_names:
         modelDirectory = simulation_dir / modelname
         cbc_path = _get_cbc_file_path(modelDirectory)
         grb_path  = _get_grb_file_path(modelDirectory)
         cbc = imod.mf6.open_cbc(cbc_path, grb_path)
-        allKeys.update(list(cbc.keys()))
+        unique_balance_keys.update(list(cbc.keys()))
         cbc_per_partition.append(cbc)
-    allKeys = list(allKeys)
+    unique_balance_keys = list(unique_balance_keys)
 
     merged_keys ={}
-    for key in allKeys:
+    for key in unique_balance_keys:
         balances_of_keys = []
         for balance in cbc_per_partition:
             if key in balance.keys():
