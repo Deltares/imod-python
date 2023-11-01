a3781fde1501110766c6fd81cdfdaf7631b02410
Hendrik Kok <hendrik.kok@deltares.nl>
2023-11-01 10:56:53 +0100

    black

--- imod/mf6/out/disv.py
+++ imod/mf6/out/disv.py
@@ -473,10 +473,10 @@ def open_cbc(
             # for non cell flow budget terms, use auxiliary variables as return value
             if header_list[0].text.startswith("data-"):
                 for return_variable in header_list[0].auxtxt:
-                        key_aux = header_list[0].txt2id1 + "-" + return_variable
-                        cbc_content[key_aux] = open_imeth6_budgets(
-                            cbc_path, grb_content, header_list, return_variable
-                        )
+                    key_aux = header_list[0].txt2id1 + "-" + return_variable
+                    cbc_content[key_aux] = open_imeth6_budgets(
+                        cbc_path, grb_content, header_list, return_variable
+                    )
             else:
                 cbc_content[key] = open_imeth6_budgets(
                     cbc_path, grb_content, header_list
