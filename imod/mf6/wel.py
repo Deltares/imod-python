7139c715fd03eb34fd5803dee28e01ab478a05a9
Joeri van Engelen <joerivanengelen@hotmail.com>
2023-11-02 17:05:44 +0000

    Rename utilities

--- imod/mf6/wel.py
+++ imod/mf6/wel.py
@@ -18,8 +18,8 @@ from imod.mf6.boundary_condition import (
 from imod.mf6.interfaces.ipointdatapackage import IPointDataPackage
 from imod.mf6.mf6_wel_adapter import Mf6Wel
 from imod.mf6.package import Package
-from imod.mf6.utilities.clip_utilities import clip_by_grid
-from imod.mf6.utilities.dataset_utilities import remove_inactive
+from imod.mf6.utilities.clip import clip_by_grid
+from imod.mf6.utilities.dataset import remove_inactive
 from imod.mf6.write_context import WriteContext
 from imod.prepare import assign_wells
 from imod.schemata import DTypeSchema
