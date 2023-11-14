5796ae1af87936491051b53ec4504dde4e904fea
Joeri van Engelen <joerivanengelen@hotmail.com>
2023-11-14 16:07:45 +0100

    Update example to also write and read mass budgets

--- examples/mf6/example_1d_transport.py
+++ examples/mf6/example_1d_transport.py
@@ -55,6 +55,7 @@ def create_transport_model(flowmodel, speciesname, dispersivity, retardation, de
 
     tpt_model = imod.mf6.GroundwaterTransportModel()
     tpt_model["ssm"] = imod.mf6.SourceSinkMixing.from_flow_model(flowmodel, speciesname)
+    tpt_model["ssm"].dataset["save_flows"] = True
     tpt_model["adv"] = imod.mf6.AdvectionUpstream()
     tpt_model["dsp"] = imod.mf6.Dispersion(
         diffusion_coefficient=0.0,
@@ -213,6 +214,7 @@ simulation.run()
 # Open the concentration results and store them in a single DataArray.
 
 concentration = simulation.open_concentration(species_ls=["a", "b", "c", "d"])
+mass_budgets = simulation.open_transport_budget(species_ls=["a", "b", "c", "d"])
 
 # %%
 # Visualize the last concentration profiles of the model run for the different
