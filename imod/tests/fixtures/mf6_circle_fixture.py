b1f34aedcb2aae10935b9c66ca9e4d6dd15d9943
luitjan <luitjan.slooten@deltares.nl>
2023-11-17 17:00:24 +0100

    formatting

--- imod/tests/fixtures/mf6_circle_fixture.py
+++ imod/tests/fixtures/mf6_circle_fixture.py
@@ -160,9 +160,6 @@ def circle_result_sto(tmpdir_factory):
     """
     Circle result with storage fluxes, which are saved as METH1 instead of METH6
     """
-    # Using a tmpdir_factory is the canonical way of sharing a tempory pytest
-    # directory between different testing modules.
-    modeldir = tmpdir_factory.mktemp("circle_sto")
     simulation = make_circle_model_save_sto()
     gwf_model = simulation["GWF_1"]
 
