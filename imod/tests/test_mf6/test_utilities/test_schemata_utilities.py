d1f7caf8e6a602e444b8c7934cfce06d4f4d266b
Joeri van Engelen <joerivanengelen@hotmail.com>
2023-11-20 16:27:47 +0100

    Add comments

--- imod/tests/test_mf6/test_utilities/test_schemata_utilities.py
+++ imod/tests/test_mf6/test_utilities/test_schemata_utilities.py
@@ -44,8 +44,10 @@ def test_filter_schemata_dict(schemata, arg, expected):
     filtered_dict = filter_schemata_dict(schemata, arg)
 
     # Assert
+    # Test if same variable names present in dicts
     assert filtered_dict.keys() == expected.keys()
 
+    # Test if scheme types in list equal.
     for key in filtered_dict.keys():
         schema_types = [type(s) for s in filtered_dict[key]]
         expected_types = [type(s) for s in expected[key]]
