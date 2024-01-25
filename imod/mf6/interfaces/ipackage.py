4bd7f4bb902194cd6a857cc413b9163b7185f4e2
luitjan <luitjan.slooten@deltares.nl>
2024-01-25 15:32:42 +0100

    formatting

--- imod/mf6/interfaces/ipackage.py
+++ imod/mf6/interfaces/ipackage.py
@@ -10,7 +10,6 @@ class IPackage(IPackageBase, metaclass=abc.ABCMeta):
     The base methods and attributes available in all packages
     """
 
-
     @property
     @abc.abstractmethod
     def auxiliary_data_fields(self) -> Dict[str, str]:
