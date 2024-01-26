f5f682a553e13fa98ede4be60e1b9b7355a50927
luitjan <luitjan.slooten@deltares.nl>
2024-01-26 14:57:09 +0100

    started adding test

--- imod/tests/test_mf6/test_multimodel/test_mf6_partitioning_structured_transport.py
+++ imod/tests/test_mf6/test_multimodel/test_mf6_partitioning_structured_transport.py
@@ -5,7 +5,7 @@ from imod.typing.grid import zeros_like
 def test_partition_structured(flow_transport_simulation):
     # Arrange.
     transport_model = flow_transport_simulation["tpt_a"]
-    submodel_labels = zeros_like(transport_model.domain)
-    submodel_labels[:, :, 30:] = 1
+    submodel_labels = zeros_like(transport_model.domain.isel(layer=0))
+    submodel_labels[:, 30:] = 1
 
-    partition_info = create_partition_info(submodel_labels)
+    split_simulation = flow_transport_simulation.split(submodel_labels)
