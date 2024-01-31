6267c0394b3c068e4f83d9377b377ee630096a2f
luitjansl <64598682+luitjansl@users.noreply.github.com>
2024-01-31 13:46:22 +0100

    Update imod/tests/test_mf6/test_mf6_simulation.py
    review comment
    
    Co-authored-by: Joeri van Engelen <joerivanengelen@hotmail.com>

--- imod/tests/test_mf6/test_mf6_simulation.py
+++ imod/tests/test_mf6/test_mf6_simulation.py
@@ -395,19 +395,10 @@ class TestModflow6Simulation:
             submodel_labels, create_partition_info_mock()
         )
 
-        assert (
-            exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_count == 2
-        )
-        call1 = (
-            exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_args_list[
-                0
-            ][0]
-        )
-        call2 = (
-            exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_args_list[
-                1
-            ][0]
-        )
+        assert exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_count == 2  # noqa: E501
+        call1 = exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_args_list[0][0]  # noqa: E501
+        call2 = exchange_creator_mock.return_value.create_gwfgwf_exchanges.call_args_list[1][0]  # noqa: E501
+
 
         assert call1[0] == "test_model1"
         xr.testing.assert_equal(call1[1], idomain.layer)
