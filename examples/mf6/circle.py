62ae175ad17064fe4297ed5c6fbdb97ca2c173e3
Deltares service account <149159792+deltares-service-account@users.noreply.github.com>
2024-02-19 15:08:56 +0100

    [TEAMCITY] Update project dependencies (#853)
    Teamcity automatically updated the dependencies defined the pixi.toml
    file. Please verify that all tests succeed before merging
    
    ---------
    
    Co-authored-by: SVC TeamCity Ansible <svc-teamcity-ansible@DIRECTORY.INTRA>
    Co-authored-by: Joeri van Engelen <joerivanengelen@hotmail.com>

--- examples/mf6/circle.py
+++ examples/mf6/circle.py
@@ -201,7 +201,8 @@ print(cbc.keys())
 #
 # We'll create a dataset for the horizontal flows for further analysis.
 
-ds = xu.UgridDataset(grids=grid)
+cbc_grid = cbc["flow-horizontal-face-x"].grid
+ds = xu.UgridDataset(grids=cbc_grid)
 ds["u"] = cbc["flow-horizontal-face-x"]
 ds["v"] = cbc["flow-horizontal-face-y"]
 
