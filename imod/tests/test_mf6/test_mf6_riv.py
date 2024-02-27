5df8c248193a2fe6937961572388349faa33cb0d
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-02-27 17:42:16 +0100

    Add test if layer defined by coord in inactive area

--- imod/tests/test_mf6/test_mf6_riv.py
+++ imod/tests/test_mf6/test_mf6_riv.py
@@ -128,7 +128,7 @@ def test_inconsistent_nan(riv_data, dis_data):
     assert len(errors) == 1
 
 @parametrize_with_cases("riv_data,dis_data", cases=RivDisCases)
-def test_layer_coord_allowed(riv_data, dis_data):
+def test_layer_as_coord_in_active_cells(riv_data, dis_data):
     # Test if no bugs like https://github.com/Deltares/imod-python/issues/830
     river = imod.mf6.River(**riv_data)
     river.dataset = river.dataset.sel(layer=2, drop=False)
@@ -139,6 +139,16 @@ def test_layer_coord_allowed(riv_data, dis_data):
 
     assert len(errors) == 0
 
+@parametrize_with_cases("riv_data,dis_data", cases=RivDisCases)
+def test_layer_as_coord_in_inactive_cells(riv_data, dis_data):
+    river = imod.mf6.River(**riv_data)
+    river.dataset = river.dataset.sel(layer=2, drop=False)
+
+    dis_data["idomain"][0, ...] = 0
+
+    errors = river._validate(river._write_schemata, **dis_data)
+
+    assert len(errors) == 1
 
 @parametrize_with_cases("riv_data", cases=RivCases)
 def test_check_layer(riv_data):
