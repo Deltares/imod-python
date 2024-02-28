6e4949e0a25d1cd3156c50daabae04501222d6b6
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-02-28 15:17:48 +0100

    Add more functions to public API

--- imod/util/__init__.py
+++ imod/util/__init__.py
@@ -5,3 +5,5 @@ The utilies imported here are public API
 """
 
 from imod.util.structured import where, replace 
+from imod.util.context import cd, ignore_warnings
+from imod.util.path import temporary_directory
