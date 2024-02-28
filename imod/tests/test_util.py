8bd68ec6422cb67967df67ded6ddd47c6d31e9dc
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-02-28 15:58:32 +0100

    Update functions in util.time to new locations

--- imod/tests/test_util.py
+++ imod/tests/test_util.py
@@ -366,7 +366,7 @@ def test_decompose_dateonly_year9999():
 
 def test_datetime_conversion__withinbounds():
     times = [datetime.datetime(y, 1, 1) for y in range(2000, 2010)]
-    converted, use_cftime = util._convert_datetimes(times, use_cftime=False)
+    converted, use_cftime = util.time._convert_datetimes(times, use_cftime=False)
     assert use_cftime is False
     assert all(t.dtype == "<M8[ns]" for t in converted)
     assert converted[0] == np.datetime64("2000-01-01", "ns")
@@ -376,7 +376,7 @@ def test_datetime_conversion__withinbounds():
 def test_datetime_conversion__outofbounds():
     times = [datetime.datetime(y, 1, 1) for y in range(1670, 1680)]
     with pytest.warns(UserWarning):
-        converted, use_cftime = util._convert_datetimes(times, use_cftime=False)
+        converted, use_cftime = util.time._convert_datetimes(times, use_cftime=False)
     assert use_cftime is True
     assert all(type(t) is cftime.DatetimeProlepticGregorian for t in converted)
     assert converted[0] == cftime.DatetimeProlepticGregorian(1670, 1, 1)
@@ -385,7 +385,7 @@ def test_datetime_conversion__outofbounds():
 
 def test_datetime_conversion__withinbounds_cftime():
     times = [datetime.datetime(y, 1, 1) for y in range(2000, 2010)]
-    converted, use_cftime = util._convert_datetimes(times, use_cftime=True)
+    converted, use_cftime = util.time._convert_datetimes(times, use_cftime=True)
     assert use_cftime is True
     assert all(type(t) is cftime.DatetimeProlepticGregorian for t in converted)
     assert converted[0] == cftime.DatetimeProlepticGregorian(2000, 1, 1)
