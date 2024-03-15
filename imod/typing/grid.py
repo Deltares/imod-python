ff2e267d3cd8a4a696075422c0c1313bc4e08a95
luitjansl <64598682+luitjansl@users.noreply.github.com>
2024-03-15 13:44:13 +0100

    Issue #889 mask simulation (#913)
    Fixes #889 
    
    # Description
    Adds a masking method to the simulation object. It masks all the models
    that hang under the simulation, provided the simulation has not been
    split, and all the models have the same domain.
    
    # Checklist
    
    - [X] Links to correct issue
    - [X] Update changelog, if changes affect users
    - [X] PR title starts with ``Issue #nr``, e.g. ``Issue #737``
    - [X] Unit tests were added
    - [ ] **If feature added**: Added/extended example
    
    ---------
    
    Co-authored-by: Joeri van Engelen <joerivanengelen@hotmail.com>

--- imod/typing/grid.py
+++ imod/typing/grid.py
@@ -280,3 +280,29 @@ def is_equal(array1: xr.DataArray, array2: xr.DataArray)->bool:
 @typedispatch
 def is_equal(array1: object, array2:object)->bool:
     return False
+
+
+@typedispatch
+def is_same_domain(grid1: xu.UgridDataArray, grid2: xu.UgridDataArray)->bool:
+    return  grid1.coords.equals(grid2.coords) and grid1.ugrid.grid.equals(grid2.ugrid.grid)
+
+@typedispatch
+def is_same_domain(grid1: xr.DataArray, grid2: xr.DataArray)->bool:
+    return grid1.coords.equals(grid2.coords)
+
+@typedispatch
+def is_same_domain(grid1: object, grid2:object)->bool:
+    return False
+
+@typedispatch
+def get_spatial_dimension_names( grid: xr.DataArray)-> list[str]:
+    return ["x", "y", "layer", "dx", "dy"]
+
+@typedispatch
+def get_spatial_dimension_names( grid: xu.UgridDataArray)-> list[str]:
+    facedim = grid.ugrid.grid.face_dimension
+    return [ facedim, "layer"]
+
+@typedispatch
+def get_spatial_dimension_names( grid: object)-> list[str]:
+    return []
