2780ad06eb54f40a15f15d1c799f6dbd00f4c4fa
luitjan <luitjan.slooten@deltares.nl>
2024-03-26 15:18:10 +0100

    fix unit tests

--- imod/tests/test_mf6/test_utilities/test_regrid_utils.py
+++ imod/tests/test_mf6/test_utilities/test_regrid_utils.py
@@ -8,6 +8,19 @@ from imod.mf6 import Dispersion
 from imod.mf6.utilities.regrid import RegridderType, RegridderWeightsCache
 
 
+def is_equal_regridder(instance_1, instance_2)->bool:
+    if type(instance_1) != type(instance_2):
+        return False
+    keys = ["__regrid_data","__regrid_indices","__regrid_indptr"]
+    for key in keys:
+        if hash(pickle.dumps(instance_1.weights[key])) != hash (pickle.dumps(instance_2.weights[key])):
+            return False
+    keys = ["__regrid_n","__regrid_m","__regrid_nnz"]
+    for key in keys:
+        if instance_1.weights[key].values[()] != instance_2.weights[key].values[()]:
+            return False    
+    return True
+
 def test_regridders_weight_cache_returns_similar_instance_when_enum_and_method_match(
     basic_unstructured_dis,
 ):
@@ -19,7 +32,8 @@ def test_regridders_weight_cache_returns_similar_instance_when_enum_and_method_m
     first_instance = collection.get_regridder(grid, new_grid, RegridderType.OVERLAP, "harmonic_mean")
     second_instance = collection.get_regridder(grid, new_grid, RegridderType.OVERLAP, "harmonic_mean")
 
-    assert hash (pickle.dumps(first_instance.weights["__regrid_data"])) == hash (pickle.dumps(second_instance.weights["__regrid_data"]))
+    assert  is_equal_regridder(first_instance, second_instance)
+
 
 
 def test_regridders_weight_cache_combining_different_instantiation_parmeters(
@@ -33,7 +47,7 @@ def test_regridders_weight_cache_combining_different_instantiation_parmeters(
     first_instance = collection.get_regridder(grid, new_grid, RegridderType.OVERLAP, "harmonic_mean")
     second_instance = collection.get_regridder(grid, new_grid, OverlapRegridder, "harmonic_mean")
 
-    assert hash (pickle.dumps(first_instance.weights["__regrid_data"])) == hash (pickle.dumps(second_instance.weights["__regrid_data"]))
+    assert  is_equal_regridder(first_instance, second_instance)
 
 def test_regridders_weight_cache_returns_different_instance_when_name_does_not_match(
     basic_unstructured_dis,
@@ -46,20 +60,8 @@ def test_regridders_weight_cache_returns_different_instance_when_name_does_not_m
     first_instance = collection.get_regridder(grid, new_grid, RegridderType.CENTROIDLOCATOR)
     second_instance = collection.get_regridder(grid, new_grid, RegridderType.BARYCENTRIC)
 
-    assert hash (pickle.dumps(first_instance)) != hash (pickle.dumps(second_instance))
+    assert not is_equal_regridder(first_instance, second_instance)
 
-def test_regridders_weight_cache_returns_different_instance_when_method_does_not_match(
-    basic_unstructured_dis,
-):
-    grid, _, _ = basic_unstructured_dis
-    new_grid = copy.deepcopy(grid)
-
-    collection = RegridderWeightsCache(grid, new_grid)
-
-    first_instance = collection.get_regridder(grid, new_grid, RegridderType.OVERLAP, "geometric_mean")
-    second_instance = collection.get_regridder(grid, new_grid, RegridderType.OVERLAP, "harmonic_mean")
-
-    assert hash (pickle.dumps(first_instance)) != hash (pickle.dumps(second_instance))
 
 def test_non_regridder_cannot_be_instantiated(
     basic_unstructured_dis,
