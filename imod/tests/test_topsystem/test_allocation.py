1ec16d9e7b9e8876f1b55fa4b22b861623e7c83a
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-04-08 18:23:12 +0200

    Add tests for allocation functions

--- imod/tests/test_topsystem/test_allocation.py
+++ imod/tests/test_topsystem/test_allocation.py
@@ -2,37 +2,89 @@ import numpy as np
 from pytest_cases import parametrize_with_cases
 
 from imod.prepare.topsystem import ALLOCATION_OPTION, allocate_river_cells
-from imod.typing.grid import zeros_like
+from imod.typing import GridDataArray
+from imod.typing.grid import is_unstructured, zeros_like
+
+
+def take_first_planar_cell(grid: GridDataArray):
+    if is_unstructured(grid):
+        return grid.values[:, 0]
+    else:
+        return grid.values[:, 0, 0]
 
 
 class RiverCases:
-    def case_structured(basic_dis):
+    def case_structured(self, basic_dis):
         ibound, top, bottom = basic_dis
         top = top.sel(layer=1)
         elevation = zeros_like(ibound.sel(layer=1))
-        stage = elevation - 2.5
+        stage = elevation - 7.5
         bottom_elevation = elevation - 10.0
         active = ibound == 1
         return active, top, bottom, stage, bottom_elevation
 
-    def case_unstructured(basic_unstructured_dis):
+    def case_unstructured(self, basic_unstructured_dis):
         ibound, top, bottom = basic_unstructured_dis
         elevation = zeros_like(ibound.sel(layer=1))
-        stage = elevation - 2.5
+        stage = elevation - 7.5
         bottom_elevation = elevation - 10.0
         active = ibound == 1
         return active, top, bottom, stage, bottom_elevation
 
 
 class AllocationOptionCases:
-    def case_stage_to_riv_bot():
+    def case_stage_to_riv_bot(self):
         option = ALLOCATION_OPTION.stage_to_riv_bot
+        expected = [False, True, False]
+
+        return option, expected, None
+
+    def case_first_active_to_riv_bot(self):
+        option = ALLOCATION_OPTION.first_active_to_riv_bot
         expected = [True, True, False]
 
-        return option, expected
+        return option, expected, None
+
+    def case_first_active_to_riv_bot__drn(self):
+        option = ALLOCATION_OPTION.first_active_to_riv_bot__drn
+        expected = [False, True, False]
+        expected__drn = [True, False, False]
+
+        return option, expected, expected__drn
+
+    def case_at_elevation(self):
+        option = ALLOCATION_OPTION.at_elevation
+        expected = [False, True, False]
+
+        return option, expected, None
+
+    def case_at_first_active(self):
+        option = ALLOCATION_OPTION.at_first_active
+        expected = [True, False, False]
+
+        return option, expected, None
 
 
-@parametrize_with_cases(argnames=["active,top,bottom,stage,bottom_elevation", "option,expected"], cases = [RiverCases, AllocationOptionCases])
-def test_riv_allocation(active, top, bottom, stage, bottom_elevation, option, expected):
-    actual = allocate_river_cells(option, active, top, bottom, stage, bottom_elevation)
-    np.testing.assert_equal(actual.values[:, 0, 0] == expected)
+@parametrize_with_cases(
+    argnames="active,top,bottom,stage,bottom_elevation",
+    cases=RiverCases,
+)
+@parametrize_with_cases(
+    argnames="option,expected_riv, expected_drn", cases=AllocationOptionCases
+)
+def test_riv_allocation(
+    active, top, bottom, stage, bottom_elevation, option, expected_riv, expected_drn
+):
+    actual_riv_da, actual_drn_da = allocate_river_cells(
+        option, active, top, bottom, stage, bottom_elevation
+    )
+
+    actual_riv = take_first_planar_cell(actual_riv_da)
+
+    if actual_drn_da is None:
+        actual_drn = actual_drn_da
+    else:
+        actual_drn = take_first_planar_cell(actual_drn_da)
+
+    np.testing.assert_equal(actual_riv, expected_riv)
+    np.testing.assert_equal(actual_drn, expected_drn)
