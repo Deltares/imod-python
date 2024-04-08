6cced53d77edee09a1615195227f05f9cea2a652
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-04-08 18:21:30 +0200

    Apply decorator to preserve gridtype

--- imod/prepare/layer.py
+++ imod/prepare/layer.py
@@ -3,9 +3,10 @@ This module contains all kinds of utilities to work with layers.
 """
 
 from imod.typing import GridDataArray
-from imod.typing.grid import zeros_like
+from imod.typing.grid import preserve_gridtype, zeros_like
 
 
+@preserve_gridtype
 def get_upper_active_layer_number(active: GridDataArray) -> GridDataArray:
     """
     Returns planar grid of integers with the layer number of the upper most
@@ -23,6 +24,7 @@ def get_upper_active_layer_number(active: GridDataArray) -> GridDataArray:
     return layer.where(active, nodata).min("layer")
 
 
+@preserve_gridtype
 def get_upper_active_grid_cells(active: GridDataArray) -> GridDataArray:
     """
     Returns grid of booleans designating location of the uppermost active grid
@@ -38,6 +40,7 @@ def get_upper_active_grid_cells(active: GridDataArray) -> GridDataArray:
     return layer == upper_active_layer
 
 
+@preserve_gridtype
 def get_lower_active_layer_number(active: GridDataArray) -> GridDataArray:
     """
     Returns two-dimensional grid of integers with the layer number of the lower
@@ -55,6 +58,7 @@ def get_lower_active_layer_number(active: GridDataArray) -> GridDataArray:
     return layer.where(active, nodata).max("layer")
 
 
+@preserve_gridtype
 def get_lower_active_grid_cells(active: GridDataArray) -> GridDataArray:
     """
     Returns grid of booleans designating location of the lowermost active grid
