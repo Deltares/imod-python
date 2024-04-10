39108cabc23347b6dd3ab6954e498c34a9371030
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-04-10 16:30:04 +0200

    Extend docstring create_layered_top and import in prepare module

--- imod/prepare/layer.py
+++ imod/prepare/layer.py
@@ -76,7 +76,21 @@ def get_lower_active_grid_cells(active: GridDataArray) -> GridDataArray:
 
 def create_layered_top(bottom: GridDataArray, top: GridDataArray) -> GridDataArray:
     """
-    Create a top array with layers from a single top array and a full bottom array
+    Create a top array with a layer dimension, from a top array with no layer
+    dimension and a bottom array with a layer dimension. The (output) top of
+    layer n is assigned the bottom of layer n-1.
+
+    Parameters
+    ----------
+    bottom: {DataArray, UgridDataArray}
+        Bottoms with layer dimension
+    top: {DataArray, UgridDataArray}
+        Top, without layer dimension
+    
+    Returns
+    -------
+    new_top: {DataArray, UgridDataArray}
+        Top with layer dimension.
     """
     new_top = zeros_like(bottom)
     new_top[0] = top
