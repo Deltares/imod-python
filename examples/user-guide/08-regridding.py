17a39ab769ead4e9fc122dbb7602d78b5e3275ef
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-05-02 16:06:21 +0200

    Merge hondsrug regridding example into user guide

--- examples/user-guide/08-regridding.py
+++ examples/user-guide/08-regridding.py
@@ -36,9 +36,9 @@ the output array may have no-data values as well, and where these end up depends
 on the chosen regridding method. Also note that regridding is only possible in
 the xy-plane, and not across the layer dimension. The output array will have the
 same number of layers as the input array.
-
 """
 
+# sphinx_gallery_thumbnail_number = -1
 
 # %%
 # Obtaining the final (i)domain
@@ -102,17 +102,30 @@ same number of layers as the input array.
 #   the one you just regridded with non-default methods if it was not popped) 
 # 
 # In code, consider an example where we want to regrid the recharge package using non default methods
-# then we would do the following. First we'll load some example simulation:
-from examples.mf6.example_models import create_twri_simulation
+# then we would do the following. First we'll load some example simulation.
+# There is a separate example contained in :doc:`/examples/mf6/hondsrug`
+# that you should look at if you are interested in the model building
+import imod
 
-original_simulation = create_twri_simulation()
+tmpdir = imod.util.temporary_directory()
 
-original_simulation
+original_simulation = imod.data.hondsrug_simulation(tmpdir / "hondsrug_saved")
 
 # %%
-# Let's inspect it's discretization
+# The model was written before the xt3d_option and rhs_option arguments were
+# added to iMOD Python. Set missing options to False.
+original_simulation["GWF"]["npf"].set_xt3d_option(is_xt3d_used=False, is_rhs=False)
 
-original_simulation["GWF_1"]["dis"]
+# %% 
+# To reduce computational overhead for this example, we are going to clip off
+# most of the timesteps.
+
+original_simulation = original_simulation.clip_box(time_max="2010-01-01")
+
+# %%
+# Let's take a look at the original discretization:
+
+original_simulation["GWF"]["dis"]
 
 # %%
 # We want to regrid this to the following target grid:
@@ -120,16 +133,12 @@ import xarray as xr
 
 import imod
 
-nrow = 21
-ncol = 12
-
-dx = 6251
-dy = -3572
-xmin = 0.0
-xmax = dx * ncol
-ymin = 0.0
-ymax = abs(dy) * nrow
-
+dx = 100
+dy = -100
+xmin = 237500.0
+xmax = 250000.0
+ymin = 559000.0
+ymax = 564000.0
 target_grid = imod.util.empty_2d(dx=dx, dy=dy, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
 
 target_grid
@@ -144,7 +153,7 @@ target_grid
 #%%
 # Next, we'll remove the recharge package and obtain it as a variable:
 
-original_rch_package = original_simulation["GWF_1"].pop("rch")
+original_rch_package = original_simulation["GWF"].pop("rch")
 
 original_rch_package
 
@@ -154,13 +163,28 @@ regridded_simulation = original_simulation.regrid_like("regridded", target_grid)
 
 # %% 
 # Let's look at the discretization again:
-regridded_simulation["GWF_1"]["dis"]
+regridded_simulation["GWF"]["dis"]
 
 # %%
 # All packages have been regridded, for example the NPF package:
 
-regridded_simulation["GWF_1"]["npf"]
+regridded_simulation["GWF"]["npf"]
 
+# %%
+# Let's make a comparison plot of the hydraulic conductivities:
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axes = plt.subplots(nrows=2, sharex=True)
+plot_kwargs = dict(colors="viridis", levels=np.linspace(0.0, 100.0, 21), fig=fig)
+
+imod.visualize.spatial.plot_map(original_simulation["GWF"]["npf"]["k"].sel(layer=3), ax=axes[0], **plot_kwargs)
+imod.visualize.spatial.plot_map(regridded_simulation["GWF"]["npf"]["k"].sel(layer=3), ax=axes[1], **plot_kwargs)
+
+axes[0].set_ylabel("original")
+axes[1].set_ylabel("regridded")
+
+plt.show()
 
 # %%
 # Set up the input needed for custom regridding including the method and the old grid.
@@ -196,14 +220,151 @@ regridded_recharge
 
 # %%
 # Next, add the recharge package to the regridded simulation
-regridded_simulation["GWF_1"]["rch"] = regridded_recharge
+regridded_simulation["GWF"]["rch"] = regridded_recharge
 
+# We can also reattach the original again
+original_simulation["GWF"]["rch"] = original_rch_package
+
+# %%
+# Comparison with histograms
+# ==========================
+#
+# In the next segment we will compare the input of the models on different grids.
+# We advice to always check how your input is regridded. In this example we upscaled grid,
+# many input parameters are regridded with a ``mean`` method. This means that their input
+# range is reduced, which can be seen in tailings in the histograms becoming shorter
+
+def plot_histograms_side_by_side(
+    array_original, array_regridded, title
+):
+    """This function creates a plot of normalized histograms of the 2 input
+    DataArray. It plots a title above each histogram."""
+    _, (ax0, ax1) = plt.subplots(1, 2, sharex=True, sharey=True, tight_layout=True)
+    array_original.plot.hist(ax=ax0, bins=25, density=True)
+    array_regridded.plot.hist(ax=ax1, bins=25, density=True)
+    ax0.title.set_text(f"{title} (original)")
+    ax1.title.set_text(f"{title} (regridded)")
+    plt.show()
+
+# %%
+# Compare constant head arrays.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["chd"]["head"],
+    regridded_simulation["GWF"]["chd"]["head"],
+    "chd head",
+)
+
+# %%
+# Compare horizontal hydraulic conductivities.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["npf"]["k"],
+    regridded_simulation["GWF"]["npf"]["k"],
+    "npf k",
+)
+# %%
+# Compare vertical hydraulic conductivities.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["npf"]["k33"],
+    regridded_simulation["GWF"]["npf"]["k33"],
+    "npf k33",
+)
+# %%
+# Compare starting heads.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["ic"]["start"],
+    regridded_simulation["GWF"]["ic"]["start"],
+    "ic start",
+)
+
+# %%
+# Compare river stages.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["riv"]["stage"],
+    regridded_simulation["GWF"]["riv"]["stage"],
+    "riv stage",
+)
+
+# %%
+# Compare river bottom elevations.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["riv"]["bottom_elevation"],
+    regridded_simulation["GWF"]["riv"]["bottom_elevation"],
+    "riv bottom elevation",
+)
+
+# %%
+# Compare riverbed conductance.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["riv"]["conductance"],
+    regridded_simulation["GWF"]["riv"]["conductance"],
+    "riv conductance",
+)
+
+# %%
+# Compare recharge rates.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["rch"]["rate"],
+    regridded_simulation["GWF"]["rch"]["rate"],
+    "rch rate",
+)
+
+# %%
+# Compare drainage elevations.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["drn-pipe"]["elevation"],
+    regridded_simulation["GWF"]["drn-pipe"]["elevation"],
+    "drn-pipe elevation",
+)
+
+# %%
+# Compare drain conductances.
+plot_histograms_side_by_side(
+    original_simulation["GWF"]["drn-pipe"]["conductance"],
+    regridded_simulation["GWF"]["drn-pipe"]["conductance"],
+    "drn-pipe conductance",
+)
+
+# %%
+# Let's compare outputs now.
+
+# Write simulation
+original_simulation.write(tmpdir / "original")
+# Run and open heads
+original_simulation.run()
+hds_original = original_simulation.open_head()
+
+hds_original
+
+# %%
+regridded_simulation.write(tmpdir / "regridded", validate=False)
+regridded_simulation.run()
+hds_regridded = regridded_simulation.open_head()
+
+hds_regridded
+
+# %%
+# Let's make a comparison plot of the regridded heads.
+fig, axes = plt.subplots(nrows=2, sharex=True)
+plot_kwargs = dict(colors="viridis", levels=np.linspace(0.0, 11.0, 12), fig=fig)
+
+imod.visualize.spatial.plot_map(hds_original.isel(layer=6, time=-1), ax=axes[0], **plot_kwargs)
+imod.visualize.spatial.plot_map(hds_regridded.isel(layer=6, time=-1), ax=axes[1], **plot_kwargs)
+
+axes[0].set_ylabel("original")
+axes[1].set_ylabel("regridded")
+
+plt.show()
 
 # %%
 # A note on regridding conductivity
 # ================================= 
-# In the npf package, it is possible to use 1 array (K), 2(K and K22) or 3 (K,
-# K22, K33) for definining the conductivity tensor. If 1 array is given the tensor
+# In the npf package, it is possible to use for definining the conductivity tensor:
+#
+# - 1 array (K) 
+# - 2 arrays (K and K22) 
+# - 3 arrays (K, K22, K33) 
+# 
+# If 1 array is given the tensor
 # is called isotropic. Defining only K gives the same behavior as specifying K,
 # K22 and K33 with the same value. When regridding, K33 has a default method
 # different from that of K and K22, but it can only be applied if K33 exists in
@@ -224,6 +385,7 @@ regridded_simulation["GWF_1"]["rch"] = regridded_recharge
 # methods. An example of this is upscaling recharge (so the target grid has
 # coarser cells than the source grid). Its default method is averaging, with the
 # following rules:
+#
 # - if a cell in the source grid is inactive in the source recharge package
 #   (meaning no recharge), it will not count when averaging. So if a target cell
 #   has partial overlap with one source recharge cell, and the rest of the target
@@ -254,7 +416,7 @@ regridded_simulation["GWF_1"]["rch"] = regridded_recharge
 # regridded simulation. 
 # 
 # 
-# This  code snippet prints all default methods:
+# This code snippet prints all default methods:
 #
 import pandas as pd
 
@@ -290,3 +452,5 @@ for pkg in ALL_PACKAGE_INSTANCES:
 regrid_method_table = regrid_method_table.set_index(["package name", "array name"])
 
 print(regrid_method_table.to_string())
+
+# %%
