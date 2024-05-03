3df0f8f951bde660856f34954ff98a9f01a44c79
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-05-03 10:49:57 +0200

    Remove unused init arguments RegridderWeightsCache

--- imod/mf6/utilities/regrid.py
+++ imod/mf6/utilities/regrid.py
@@ -36,16 +36,11 @@ class RegridderWeightsCache:
 
     def __init__(
         self,
-        source_grid: Union[xr.DataArray, xu.UgridDataArray],
-        target_grid: Union[xr.DataArray, xu.UgridDataArray],
         max_cache_size: int = 6,
     ) -> None:
         self.regridder_instances: dict[
             tuple[type[BaseRegridder], Optional[str]], BaseRegridder
         ] = {}
-        self._source_grid = source_grid
-        self._target_grid = target_grid
-
         self.weights_cache = {}
         self.max_cache_size = max_cache_size
 
@@ -332,7 +327,7 @@ def _regrid_like(
         )
     new_model = model.__class__()
     if regrid_context is None:
-        regrid_context = RegridderWeightsCache(model.domain, target_grid)
+        regrid_context = RegridderWeightsCache()
     for pkg_name, pkg in model.items():
         if isinstance(pkg, (IRegridPackage, ILineDataPackage, IPointDataPackage)):
             new_model[pkg_name] = pkg.regrid_like(target_grid, regrid_context)
@@ -390,7 +385,7 @@ def _regrid_like(
         )
     flow_models = simulation.get_models_of_type("gwf6")
     old_grid = list(flow_models.values())[0].domain
-    regrid_context = RegridderWeightsCache(old_grid, target_grid)
+    regrid_context = RegridderWeightsCache()
 
     models = simulation.get_models()
     for model_name, model in models.items():
