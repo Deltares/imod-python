4094bd27bfcf7f537430f8a978d64878ce5e345a
luitjan <luitjan.slooten@deltares.nl>
2024-05-14 17:00:25 +0200

    cleanup

--- imod/typing/grid.py
+++ imod/typing/grid.py
@@ -403,10 +403,10 @@ def preserve_gridtype(func):
 def is_planar_grid(
     grid: xr.DataArray | xr.Dataset | xu.UgridDataArray | xu.UgridDataset,
 ) -> bool:
+    # Returns True if the grid is planar. It has then a layer coordinate with
+    # length 1 and value 0, or an empty layer coordinate axis, or no layer coordinate at all    
     if not is_spatial_2D(grid):
          return False
-    # Returns True if the grid is planar. It has then a layer coordinate with
-    # length 1 and value 0, or an empty layer coordinate axis, or no layer coordinate at all
     if "layer" not in grid.coords:
         return True
     if grid["layer"].shape == ():
