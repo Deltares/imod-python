f21752a6deaf23c3efd9f820ad188e1f879814e6
luitjan <luitjan.slooten@deltares.nl>
2024-07-08 11:41:53 +0200

    removed gram test

--- imod/tests/test_mf6/test_mf6_simulation.py
+++ imod/tests/test_mf6/test_mf6_simulation.py
@@ -503,36 +503,3 @@ def test_import_from_imod5(imod5_dataset, tmp_path):
     # write and validate the simulation.
     simulation.write(tmp_path, binary=False, validate=True)
 
-
-def test_import_from_GRAM(tmp_path):
-    default_simulation_allocation_options = SimulationAllocationOptions
-    default_simulation_distributing_options = SimulationDistributingOptions
-
-    datelist = []
-    date = datetime(1989, 1, 1)
-    while date < datetime(2013, 1, 1):
-        datelist.append(date)
-        date = date + timedelta(days=7)
-    path = "D:\\tmp\\van_hendrik\\GRAM3.1_prj"
-    imod5_dataset = imod.data.imod5_projectfile_data(path)
-
-    simulation = Modflow6Simulation.from_imod5_data(
-        imod5_dataset[0],
-        datelist,
-        default_simulation_allocation_options,
-        default_simulation_distributing_options,
-    )
-
-    simulation["imported_model"]["oc"] = OutputControl(
-        save_head="last", save_budget="last"
-    )
-
-    simulation.create_time_discretization(["01-01-2003", "02-01-2003"])
-
-    # Remove HFB packages outside domain
-    # TODO: Build in support for hfb packages outside domain
-    for hfb_outside in ["hfb-24", "hfb-26"]:
-        simulation["imported_model"].pop(hfb_outside)
-
-    # write and validate the simulation.
-    simulation.write(tmp_path, binary=False, validate=True)
