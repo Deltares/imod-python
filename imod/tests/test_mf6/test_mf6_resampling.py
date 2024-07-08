5ab7a29c9fc888800a7bdbbad72f53499c1f7793
luitjan <luitjan.slooten@deltares.nl>
2024-07-08 15:15:29 +0200

    made new test file for resampling tests

--- imod/tests/test_mf6/test_mf6_resampling.py
+++ imod/tests/test_mf6/test_mf6_resampling.py
@@ -6,6 +6,8 @@ from imod.util.expand_repetitions import resample_timeseries
 
 
 def test_timeseries_resampling():
+    # In this test, we resample a timeseries for a coarser output discretization.
+    # The output times are a subset of the input times.
     # fmt: off
     data = [
         {  "time": datetime(1989, 1, 1), "rate": 100, "x": 0,"y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10, },  # output time 1
@@ -41,6 +43,8 @@ def test_timeseries_resampling():
 
 
 def test_timeseries_resampling_2():
+    # In this test, we resample a timeseries for a coarser output discretization.
+    # The output times are a not a subset of the input times, and they begin earlier.
     # fmt: off
     data = [
         { "time": datetime(1989, 1, 1, 14, 0, 0), "rate": 100, "x": 0, "y": 0, "id": "ID",  "filt_top": 20,  "filt_bot": 10, },  # after output time 1
@@ -78,6 +82,9 @@ def test_timeseries_resampling_2():
 
 
 def test_timeseries_resampling_3():
+    # In this test, we resample a timeseries for a coarser output discretization.
+    # The output times are a subset of the input times.
+
     # fmt: off
     data = [
         { "time": datetime(1989, 1, 1), "rate": 100, "x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10,},  # output time 1
@@ -103,6 +110,47 @@ def test_timeseries_resampling_3():
         {"time": datetime(1989, 1, 1), "rate": 175,"x": 0,"y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10},
         {"time": datetime(1989, 1, 5), "rate": 400,"x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10, },
         {"time": datetime(1989, 1, 6), "rate": 400,  "x": 0, "y": 0, "id": "ID","filt_top": 20, "filt_bot": 10, },
+    ]
+    # fmt: on
+    # setup panda table
+    expected_timeseries = pd.DataFrame(
+        expected_data, columns=["time", "rate", "x", "y", "id", "filt_top", "filt_bot"]
+    )
+
+    pd.testing.assert_frame_equal(
+        new_timeseries, expected_timeseries, check_dtype=False
+    )
+
+
+def test_timeseries_resampling_4():
+    # In this test, we resample a timeseries for a coarser output discretization.
+    # The output times are a not a subset of the input times, and they end later.
+    #
+    # fmt: off
+    data = [
+        { "time": datetime(1989, 1, 1, 11,0,0), "rate": 100, "x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10,},  # output time 1
+        { "time": datetime(1989, 1, 2, 11,0,0), "rate": 100, "x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10,  },
+        { "time": datetime(1989, 1, 3, 11,0,0), "rate": 200, "x": 0, "y": 0, "id": "ID", "filt_top": 20,"filt_bot": 10,},
+        {  "time": datetime(1989, 1, 4, 11,0,0),  "rate": 300, "x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10, },  # output time 2
+        {  "time": datetime(1989, 1, 5, 11,0,0), "rate": 300,  "x": 0,"y": 0, "id": "ID","filt_top": 20, "filt_bot": 10,},
+    ]  # 10 years later than output time 3
+    # fmt: on
+    # initialize data of lists.
+
+    # setup panda table
+    timeseries = pd.DataFrame(
+        data, columns=["time", "rate", "x", "y", "id", "filt_top", "filt_bot"]
+    )
+
+    new_dates = [datetime(1989, 1, 1), datetime(1989, 1, 3), datetime(1999, 1, 4)]
+
+    new_timeseries = resample_timeseries(timeseries, new_dates)
+
+    # fmt: off
+    expected_data = [
+        {"time": datetime(1989, 1, 1), "rate": 77.083333,"x": 0,"y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10},
+        {"time": datetime(1989, 1, 3), "rate":299.947532,"x": 0, "y": 0, "id": "ID", "filt_top": 20, "filt_bot": 10, },
+        {"time": datetime(1999, 1, 4), "rate": 300,  "x": 0, "y": 0, "id": "ID","filt_top": 20, "filt_bot": 10, },
     ]
     # fmt: on
     # setup panda table
