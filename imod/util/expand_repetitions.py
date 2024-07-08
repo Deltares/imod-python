9a046d67177293c868cbaa68064ada060da4a208
luitjan <luitjan.slooten@deltares.nl>
2024-07-08 14:45:41 +0200

    added docstrings

--- imod/util/expand_repetitions.py
+++ imod/util/expand_repetitions.py
@@ -9,6 +9,30 @@ import pandas as pd
 def expand_repetitions(
     repeat_stress: List[datetime], time_min: datetime, time_max: datetime
 ) -> Dict[datetime, datetime]:
+    """
+    Given a list of repeat stresses,  and the start and end time of the simulation,
+    this function returns a dictionary indicating what repeat stress should be used
+    at what time in the simulation.
+
+    Parameters
+    ----------
+    repeat_stress:  list of datetime, optional
+        This dict list contains contains, per topic, the period alias (a string) to
+        its datetime.
+    time_min: datetime
+        starting time  of the simulation
+    time_max: datetime
+        ending time  of the simulation
+
+
+    returns:
+    --------
+    A dictionary that can be used to repeat data for e.g.repeating stress periods such as
+    seasonality without duplicating the values. The ``repeat_items``
+    dimension should have size 2: the first value is the "key", the second
+    value is the "value". For the "key" datetime, the data of the "value"
+    datetime will be used.
+    """
     expanded = {}
     for year, date in itertools.product(
         range(time_min.year, time_max.year + 1),
@@ -20,7 +44,24 @@ def expand_repetitions(
     return expanded
 
 
-def resample_timeseries(well_rate, times):
+def resample_timeseries(well_rate: pd.DataFrame, times: list[datetime]) -> pd.DataFrame:
+    """
+    On input, well_rate is a dataframe containing a timeseries for rate for one well
+    while "times" is a list of datetimes.
+    This function creates a new dataframe containing a timeseries defined on the
+    times in "times".
+
+    Parameters
+    ----------
+    well_rate:  pd.DataFrame
+        input timeseries for well
+    times: datetime
+        list of times on which the output datarame should have entries.
+
+    returns:
+    --------
+    a new dataframe containing a timeseries defined on the times in "times".
+    """
     output_frame = pd.DataFrame(times)
     output_frame = output_frame.rename(columns={0: "time"})
     intermediate_df = pd.merge(
@@ -70,7 +111,14 @@ def resample_timeseries(well_rate, times):
     return output_frame.drop("time_to_next", axis=1)
 
 
-def integrate_timestep_rate(well_rate, time_0, time_1):
+def integrate_timestep_rate(
+    well_rate: pd.DataFrame, time_0: datetime, time_1: datetime
+) -> float:
+    """
+    Given a dataframe "well_rate", computes the pumping rate between time_0 and time_1
+    that best matches the pumping rate in "well_rate" if the pumping rate were constant
+    between time_0 and time_1.
+    """
     delta_time = time_1 - time_0
     timestep_data = well_rate.loc[
         (well_rate["time"] >= time_0) & (well_rate["time"] < time_1)
