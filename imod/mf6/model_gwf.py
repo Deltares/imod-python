506020d34afaa8ec0a861c892e735dc333954ebd
luitjansl <64598682+luitjansl@users.noreply.github.com>
2024-07-26 15:03:55 +0200

    Issue #1045 ghb allocation (#1119)
    Fixes #1045
    
    # Description
    Enables using allocation options when importing a ghb from imod5 and it
    is planar (layer=0)
    
    # Checklist
    
    - [X] Links to correct issue
    - [ ] Update changelog, if changes affect users
    - [X] PR title starts with ``Issue #nr``, e.g. ``Issue #737``
    - [X] Unit tests were added
    - [ ] **If feature added**: Added/extended example
    
    ---------
    
    Co-authored-by: Joeri van Engelen <joerivanengelen@hotmail.com>

--- imod/mf6/model_gwf.py
+++ imod/mf6/model_gwf.py
@@ -12,6 +12,7 @@ from imod.mf6 import ConstantHead
 from imod.mf6.clipped_boundary_condition_creator import create_clipped_boundary
 from imod.mf6.dis import StructuredDiscretization
 from imod.mf6.drn import Drainage
+from imod.mf6.ghb import GeneralHeadBoundary
 from imod.mf6.hfb import SingleLayerHorizontalFlowBarrierResistance
 from imod.mf6.ic import InitialConditions
 from imod.mf6.model import Modflow6Model
@@ -21,6 +22,7 @@ from imod.mf6.regrid.regrid_schemes import (
     ConstantHeadRegridMethod,
     DiscretizationRegridMethod,
     DrainageRegridMethod,
+    GeneralHeadBoundaryRegridMethod,
     InitialConditionsRegridMethod,
     NodePropertyFlowRegridMethod,
     RechargeRegridMethod,
@@ -279,10 +281,31 @@ class GroundwaterFlowModel(Modflow6Model):
         # now import the non-singleton packages'
 
         # import wells
+        # import wells
         imod5_keys = list(imod5_data.keys())
         wel_keys = [key for key in imod5_keys if key[0:3] == "wel"]
         for wel_key in wel_keys:
             result[wel_key] = Well.from_imod5_data(wel_key, imod5_data, times)
+
+        imod5_keys = list(imod5_data.keys())
+        ghb_keys = [key for key in imod5_keys if key[0:3] == "ghb"]
+        for ghb_key in ghb_keys:
+            ghb_pkg = GeneralHeadBoundary.from_imod5_data(
+                ghb_key,
+                imod5_data,
+                period_data,
+                dis_pkg,
+                npf_pkg,
+                times[0],
+                times[-1],
+                allocation_options.ghb,
+                distributing_options.ghb,
+                regridder_types=cast(
+                    GeneralHeadBoundaryRegridMethod, regridder_types.get(ghb_key)
+                ),
+                regrid_cache=regrid_cache,
+            )
+            result[ghb_key] = ghb_pkg
 
         # import drainage
 
