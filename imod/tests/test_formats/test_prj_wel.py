135c4166c1e21d7ddaaab584927b360cbb03304e
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-07-30 16:39:00 +0200

    Use test ipf cases in fixtures

--- imod/tests/test_formats/test_prj_wel.py
+++ imod/tests/test_formats/test_prj_wel.py
@@ -1,4 +1,4 @@
-from shutil import copytree
+from shutil import copyfile
 from textwrap import dedent
 
 import numpy as np
@@ -44,9 +44,9 @@ class WellPrjCases:
             1,2, 001, 1.0, 0.0, -999.9900 ,"ipf/associated.ipf"
             """
         )
-    
+
     def case_associated__all(self):
-       return dedent(
+        return dedent(
             """
             0003,(WEL),1
             2000-01-01
@@ -59,11 +59,11 @@ class WellPrjCases:
             001,001
             1,2, 001, 1.0, 0.0, -999.9900 ,"ipf/associated.ipf"
             """
-       )
+        )
 
     def case_associated__all_fails(self):
-       """FAIL: Varying factor over time"""
-       return dedent(
+        """FAIL: Varying factor over time"""
+        return dedent(
             """
             0003,(WEL),1
             2000-01-01
@@ -76,8 +76,7 @@ class WellPrjCases:
             001,001
             1,2, 001, 0.2, 0.0, -999.9900 ,"ipf/associated.ipf"
             """
-       )
-
+        )
 
     def case_associated__fail(self):
         """FAIL: Assign same file to multiple layers"""
@@ -138,19 +137,27 @@ class WellPrjCases:
 
 
 @parametrize_with_cases("wel_case", cases=WellPrjCases)
-def test_import_wells(wel_case, tmp_path, current_cases):
+def test_import_wells(wel_case, well_mixed_ipfs, tmp_path, current_cases):
     # Arrange
     case_name = current_cases["wel_case"].id
     wel_file = tmp_path / f"{case_name}.prj"
-    
+
     with open(wel_file, "w") as f:
         f.write(wel_case)
-    
-    src = r"c:\Users\engelen\projects_wdir\imod-python\imod5_wel\ipf"
-    copytree(src, tmp_path/"ipf")
 
-    times = [np.datetime64("2000-01-01"), np.datetime64("2000-01-02"), np.datetime64("2000-01-03")]
+    ipf_dir = tmp_path / "ipf"
+    ipf_dir.mkdir(exist_ok=True)
+
+    # copy files to test folder
+    for p in well_mixed_ipfs:
+        copyfile(p, ipf_dir / p.name)
+
+    times = [
+        np.datetime64("2000-01-01"),
+        np.datetime64("2000-01-02"),
+        np.datetime64("2000-01-03"),
+    ]
 
     # Act
     data, _ = open_projectfile_data(wel_file)
-    well = LayeredWell.from_imod5_data("wel-1", data, times)
+    #well = LayeredWell.from_imod5_data("wel-1", data, times)
