345671fd9614c466b33db832cfdeee7854cb55b6
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-08-02 16:06:07 +0200

    Add extra test cases for from_imod5_data test

--- imod/tests/test_formats/test_prj_wel.py
+++ imod/tests/test_formats/test_prj_wel.py
@@ -3,6 +3,7 @@ from shutil import copyfile
 from textwrap import dedent
 
 import pytest
+import xarray as xr
 from pytest_cases import parametrize_with_cases
 
 from imod.formats.prj import open_projectfile_data
@@ -177,6 +178,72 @@ class WellPrjCases:
         )
         expected = "FAIL"
         return prj_string, expected
+
+
+class WellIMOD5Cases:
+    def case_simple__first(self):
+        dataframe = {"wel-simple1": {"time": [datetime(2000, 1, 1)]}}
+        expected = {"wel-simple1": [datetime(2000, 1, 1)]}
+        return dataframe, expected
+
+    def case_simple__all(self):
+        dataframe = {
+            "wel-simple1": {"time": [datetime(2000, 1, 1), datetime(2000, 1, 2)]},
+            "wel-simple2": {"time": [datetime(2000, 1, 2)]},
+            "wel-simple3": {"time": [datetime(2000, 1, 3)]},
+        }
+        expected = { # Add extra timestep with 0.0 rate assigned
+            "wel-simple1": [datetime(2000, 1, 1), datetime(2000, 1, 2), datetime(2000, 1, 3)],
+            "wel-simple2": [datetime(2000, 1, 2), datetime(2000, 1, 3)],
+            "wel-simple3": [datetime(2000, 1, 3)],
+        }
+        return dataframe, expected
+
+    def case_associated__first(self):
+        dataframe = {"wel-associated": {"time": [datetime(2000, 1, 1)]}}
+        expected = {"wel-associated": [datetime(2000, 1, 1)]}
+        return dataframe, expected
+
+    def case_associated__all(self):
+        dataframe = {
+            "wel-associated": {
+                "time": [
+                    datetime(2000, 1, 1),
+                    datetime(2000, 1, 2),
+                    datetime(2000, 1, 3),
+                ]
+            },
+        }
+        expected = {"wel-associated": [datetime(2000, 1, 1)]}
+        return dataframe, expected
+
+    def case_mixed__first(self):
+        dataframe = {
+            "wel-simple1": {"time": [datetime(2000, 1, 1)]},
+            "wel-associated": {"time": [datetime(2000, 1, 1)]},
+        }
+        expected = {
+            "wel-simple1": [datetime(2000, 1, 1), datetime(2000, 1, 2)],
+            "wel-associated": [datetime(2000, 1, 1)]
+        }
+        return dataframe, expected
+
+    def case_mixed__all(self):
+        dataframe = {
+            "wel-associated": {
+                "time": [
+                    datetime(2000, 1, 1),
+                    datetime(2000, 1, 2),
+                    datetime(2000, 1, 3),
+                ]
+            },
+            "wel-simple1": {"time": [datetime(2000, 1, 2)]},
+        }
+        expected = {
+            "wel-simple1": [datetime(2000, 1, 2), datetime(2000, 1, 3)],
+            "wel-associated": [datetime(2000, 1, 1)]
+        }
+        return dataframe, expected
 
 
 @parametrize_with_cases("wel_case, expected", cases=WellPrjCases)
