ceb1c8ccd0beb30f70fc10ebc1211db79fa503bf
luitjan <luitjan.slooten@deltares.nl>
2024-08-06 16:48:01 +0200

    added test for tutorial

--- imod/tests/test_mf6/test_mf6_LHM.py
+++ imod/tests/test_mf6/test_mf6_LHM.py
@@ -453,3 +453,49 @@ def test_mf6_Mipwa(tmp_path):
         for k, package in simulation["imported_model"].items():
             package.dataset.load()
         simulation.write(tmp_path, binary=False, validate=True)
+
+
+def test_mf6_tut(tmp_path):
+    prj_dir = Path("D:\\dev\\imod_python-gh\\vanFrans\\e400_iMOD5\\f02_tutorial_tests\\c05_TUT_iMODBatch")
+    data = open_projectfile_data(
+        prj_dir / "c05_TUT_iMODBatch_T2__root__.prj"
+    )
+
+    logfile_path = tmp_path / "logfile.txt"
+    with open(logfile_path, "w") as sys.stdout:
+        imod.logging.configure(
+            LoggerType.PYTHON,
+            log_level=LogLevel.DEBUG,
+            add_default_file_handler=False,
+            add_default_stream_handler=True,
+        )    
+
+        imod5_data = data[0]
+        period_data = data[1]
+        default_simulation_allocation_options = SimulationAllocationOptions
+        default_simulation_distributing_options = SimulationDistributingOptions
+
+        regridding_option = {}
+        regridding_option["npf"] = NodePropertyFlowRegridMethod()
+        regridding_option["dis"] = DiscretizationRegridMethod()
+        regridding_option["sto"] = StorageCoefficientRegridMethod()
+        times = pd.date_range(start="1/1/1989", end="12/1/1991", freq="ME")
+
+        simulation = Modflow6Simulation.from_imod5_data(
+            imod5_data,
+            period_data,
+            default_simulation_allocation_options,
+            default_simulation_distributing_options,
+            times,
+            regridding_option,
+        )
+        simulation["imported_model"]["oc"] = OutputControl(
+            save_head="last", save_budget="last"
+        )
+
+        mask_arrays(simulation["imported_model"]["drn-12"])
+
+
+        for k, package in simulation["imported_model"].items():
+            package.dataset.load()
+        simulation.write(tmp_path, binary=False, validate=True)
