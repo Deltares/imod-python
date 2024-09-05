04dfa790100c65bc01bcad059484c8c33fc96d8d
Sunny Titus <sunny.titus@deltares.nl>
2024-09-05 16:49:51 +0200

    Run lint build step inside of docker container

--- .teamcity/Templates/LintTemplate.kt
+++ .teamcity/Templates/LintTemplate.kt
@@ -2,6 +2,8 @@ package Templates
 
 import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.Template
+import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object LintTemplate : Template({
@@ -24,11 +26,17 @@ object LintTemplate : Template({
                     pixi run --environment default --frozen lint 
                 """.trimIndent()
             formatStderrAsError = true
+            dockerImage = "containers.deltares.nl/hydrology_product_line_imod/windows-pixi:v0.26.1"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
+            dockerPull = true
         }
     }
 
-    requirements {
-        equals("env.OS", "Windows_NT")
-        doesNotExist("container.engine")
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_134"
+            }
+        }
     }
 })
