5c539df07cf91154fb91bea6a227dce24c930605
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-09-09 17:20:16 +0200

    Capture stderr message

--- imod/tests/test_examples.py
+++ imod/tests/test_examples.py
@@ -19,4 +19,9 @@ def get_examples():
 @pytest.mark.example
 @pytest.mark.parametrize("example", get_examples())
 def test_example(example):
-    subprocess.run([sys.executable, example], check=True, capture_output=True)
+    result = subprocess.run([sys.executable, example], capture_output=True)
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Example failed to run with returncode "
+            f"{result.returncode}, and error message:\n\n{result.stderr.decode()} "
+        )
