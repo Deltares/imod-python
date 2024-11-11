c3f74f1322e3bcac20dfbea4b6c2e12a1906aa22
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-11-11 10:44:21 +0100

    Add some titles to plots in cleanup example

--- examples/user-guide/10-cleanup.py
+++ examples/user-guide/10-cleanup.py
@@ -8,8 +8,12 @@ up data. We included some convenience methods to help clean up inconsistent
 datasets.
 """
 
+# sphinx_gallery_thumbnail_number = -1
+
+
 # %%
 # We'll start with the usual imports
+import matplotlib.pyplot as plt
 import numpy as np
 
 import imod
@@ -169,9 +173,11 @@ cleaned_ds = gwf_model["new_riv"].dataset
 
 diff_stage = dirty_ds["stage"] - cleaned_ds["stage"]
 
-imod.visualize.plot_map(
+fig, ax = imod.visualize.plot_map(
     diff_stage.max(dim="layer"), "viridis", np.linspace(0, max_diff, 9)
 )
+ax.set_title("stage lowered by cleanup (m)")
+plt.show()
 
 # %%
 #
@@ -182,9 +188,12 @@ imod.visualize.plot_map(
 
 diff_riv_bot = dirty_ds["bottom_elevation"] - cleaned_ds["bottom_elevation"]
 
-imod.visualize.plot_map(
+fig, ax = imod.visualize.plot_map(
     diff_riv_bot.max(dim="layer"), "viridis", np.linspace(0, max_diff, 9)
 )
+ax.set_title("river bottom lowered by cleanup (m)")
+plt.show()
+
 # %%
 #
 # You can see the bottom elevation was lowered by the cleanup method.
