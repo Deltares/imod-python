af270c2d7e83f1be2c68eaf13d443dee9707d5e5
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-11-14 17:40:58 +0100

    Type annotate

--- imod/msw/ponding.py
+++ imod/msw/ponding.py
@@ -8,7 +8,7 @@ from imod.msw.fixed_format import VariableMetaData
 from imod.msw.pkgbase import DataDictType, MetaSwapPackage
 from imod.msw.regrid.regrid_schemes import PondingRegridMethod
 from imod.msw.utilities.common import concat_imod5
-from imod.typing import IntArray
+from imod.typing import GridDataDict, IntArray
 
 
 class Ponding(MetaSwapPackage, IRegridPackage):
@@ -73,7 +73,7 @@ class Ponding(MetaSwapPackage, IRegridPackage):
         return self.write_dataframe_fixed_width(file, dataframe)
 
     @classmethod
-    def from_imod5_data(cls, imod5_data):
+    def from_imod5_data(cls, imod5_data: dict[str, GridDataDict]) -> "Ponding":
         """
         Concatenate ponding depths along subunits
         """
--- imod/msw/utilities/common.py
+++ imod/msw/utilities/common.py
@@ -2,5 +2,5 @@ from imod.typing import GridDataArray
 from imod.typing.grid import concat
 
 
-def concat_imod5(arg1: GridDataArray, arg2: GridDataArray):
+def concat_imod5(arg1: GridDataArray, arg2: GridDataArray) -> GridDataArray:
     return concat([arg1, arg2], dim="subunit").assign_coords(subunit=[0, 1])
