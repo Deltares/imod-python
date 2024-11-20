0dd06fb32288ae8d5a303730405e99a5252ba6e8
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-11-20 11:23:33 +0100

    Issue #1299 public api fixes from imod5 (#1300)
    Fixes #1299
    
    * The SimulationAllocationOptions and SimulationDistributingOptions are
    stowed away very far, and not part of the public API
    * Yet, these objects used to store default settings are required to call
    the public api method Modflow6Simulation.from_imod5_data...
    * A description of "period_times" is missing in the docstring
    * Example code snippets are missing from the docstring
    
    # Description
    This PR fixes the following things:
    * Makes ``SimulationAllocationOptions`` and
    ``SimulationDistributingOptions`` part of the public API
    * Makes ``allocation_options`` and ``distributing_options`` optional
    arguments for ``Modflow6Simulation.from_imod5_data`` and
    ``GroundwaterFlowModel.from_imod5_data``
    * To do this, I had to change the order of arguments for
    Modflow6Simulation.from_imod5_data, as Python doesn't allow non-optional
    args after an optional arg. This can break existing scripts, however
    this has just been released, so not many scripts use it. As long as the
    tutorial material is updated within time.
    * Extends docstrings of ``GroundwaterFlowModel.from_imod5_data``,
    ``Modflow6Simulation.from_imod5_data``, ``SimulationAllocationOptions``,
    and ``SimulationDistributingOptions``

--- imod/prepare/topsystem/default_allocation_methods.py
+++ imod/prepare/topsystem/default_allocation_methods.py
@@ -7,14 +7,33 @@ from imod.prepare.topsystem.conductance import DISTRIBUTING_OPTION
 @dataclass()
 class SimulationAllocationOptions:
     """
-    Object containing allocation otpions, specified per packages type on
-    importing fron imod5. Can be used to set defaults when importing a
+    Object containing default allocation options, specified per packages type on
+    importing from imod5. Can be used to set defaults when importing a
     simulation or a GroundwaterFlowModel from imod5.
 
     Parameters
     ----------
-    drn: allocation option to be used for drainage packages
-    riv: allocation option to be used for river packages
+    drn: ALLOCATION_OPTION
+        allocation option to be used for drainage packages, defaults to
+        ``first_active_to_elevation``.
+    riv: ALLOCATION_OPTION
+        allocation option to be used for river packages, defaults to
+        ``stage_to_riv_bot``.
+    ghb: ALLOCATION_OPTION
+        allocation option to be used for general head boundary packages,
+        defaults to ``at_elevation``.
+
+    Examples
+    --------
+
+    Initiate allocation default options
+
+    >>> alloc_options = SimulationAllocationOptions()
+
+    You can set different options as follows:
+
+    >>> from imod.prepare.topsystem import ALLOCATION_OPTION
+    >>> alloc_options.riv = ALLOCATION_OPTION.at_elevation
 
     """
 
@@ -26,14 +45,33 @@ class SimulationAllocationOptions:
 @dataclass()
 class SimulationDistributingOptions:
     """
-    Object containing conductivity distribution methods, specified per packages
+    Object containing conductance distribution methods, specified per packages
     type. Can be used to set defaults when importing a simulation or a
     GroundwaterFlowModel from imod5.
 
     Parameters
     ----------
-    drn: distribution option to be used for drainage packages
-    riv: distribution option to be used for river packages
+    drn: DISTRIBUTING_OPTION
+        distribution option to be used for drainage packages, defaults to
+        ``by_corrected_transmissivity``.
+    riv: DISTRIBUTING_OPTION
+        distribution option to be used for river packages, defaults to
+        ``by_corrected_transmissivity``.
+    ghb: DISTRIBUTING_OPTION
+        distribution option to be used for general head boundary packages,
+        defaults to ``by_layer_transmissivity``.
+
+    Examples
+    --------
+
+    Initiate default distributing options
+
+    >>> dist_options = SimulationDistributingOptions()
+
+    You can set different options as follows:
+
+    >>> from imod.prepare.topsystem import DISTRIBUTING_OPTION
+    >>> dist_options.riv = DISTRIBUTING_OPTION.by_layer_transmissivity
 
     """
 
