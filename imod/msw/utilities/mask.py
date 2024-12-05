e5acfb3314c057fd41ae7652fd104a1167d31734
JoerivanEngelen <joerivanengelen@hotmail.com>
2024-12-05 16:30:27 +0100

    Add masking utilities

---
+++ imod/msw/utilities/mask.py
@@ -0,0 +1,41 @@
+import numbers
+from dataclasses import dataclass
+
+from imod.msw.pkgbase import MetaSwapPackage
+from imod.typing import GridDataArray, GridDataDict
+
+
+@dataclass
+class MetaSwapActive:
+    all: GridDataArray
+    per_subunit: GridDataArray
+
+
+def mask_and_broadcast_grid_data(
+        package: MetaSwapPackage, 
+        grid_data: GridDataDict, 
+        msw_active: MetaSwapActive
+    ):
+    """
+    Mask and broadcast grid data, carefully mask per subunit if variable needs
+    to contain subunits.
+    """
+
+    return {
+        key: (
+            _mask_spatial_var(grid, msw_active.per_subunit)
+            if key in package._with_subunit
+            else _mask_spatial_var(grid, msw_active.all)
+        )
+        for key, grid in grid_data.items()
+    }
+
+def _mask_spatial_var(self, da: GridDataArray, active: GridDataArray) -> GridDataArray:
+    if issubclass(da.dtype.type, numbers.Integral):
+        return da.where(active, other=0)
+    elif issubclass(da.dtype.type, numbers.Real):
+        return da.where(active)
+    else:
+        raise TypeError(
+            f"Expected dtype float or integer. Received instead: {da.dtype}"
+        )
