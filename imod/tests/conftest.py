422faffb846332c60625abc8d911037f73b1de0a
JoerivanEngelen <joerivanengelen@hotmail.com>
2024-12-06 12:07:45 +0100

    Import added fixtures

--- imod/tests/conftest.py
+++ imod/tests/conftest.py
@@ -92,4 +92,6 @@ from .fixtures.mf6_welltest_fixture import (
     well_test_data_transient,
 )
 from .fixtures.msw_fixture import fixed_format_parser, simple_2d_grid_with_subunits
+from .fixtures.msw_meteo_fixture import meteo_grids
 from .fixtures.msw_model_fixture import coupled_mf6_model, coupled_mf6wel, msw_model
+from .fixtures.msw_imod5_cap_fixture import imod5_cap_data
