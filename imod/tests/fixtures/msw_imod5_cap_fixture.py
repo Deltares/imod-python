5c41cc79515f1faac573d8df48d1b1b5cda6c31a
JoerivanEngelen <joerivanengelen@hotmail.com>
2024-12-06 17:37:07 +0100

    Fixes to cap data fixture

--- imod/tests/fixtures/msw_imod5_cap_fixture.py
+++ imod/tests/fixtures/msw_imod5_cap_fixture.py
@@ -23,202 +23,212 @@ def imod5_cap_data() -> GridDataDict:
 
     # fmt: off
     d["boundary"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1, 1, 1],
                 [0, 0, 0],
                 [1, 1, 0],
-            ],
+            ]],
         dtype=int),
         **da_kwargs
     )
     d["landuse"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1, 2, 3],
                 [0, 0, 0],
                 [3, 2, 1],
-            ],
+            ]],
         dtype=int),
         **da_kwargs
     )
     d["rootzone_thickness"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [0.1, 0.1, 0.1],
                 [nan, nan, nan],
                 [0.2, 0.2, 0.2],
-            ]
+            ]]
         ),
         **da_kwargs
     )
+    d["soil_physical_unit"] = xr.DataArray(
+        np.array([
+            [
+                [1, 1, 1],
+                [0, 0, 0],
+                [2, 1, 1],
+            ]],
+        dtype=int),
+        **da_kwargs
+    )
     d["surface_elevation"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1.1, 1.2, 1.3],
                 [nan, nan, nan],
                 [1.4, 1.5, 1.6],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["artificial_recharge"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [0.2, 0.2, 0.2],
                 [nan, nan, nan],
                 [0.3, 0.3, 0.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["artificial_recharge_layer"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1, 2, 3],
                 [0, 0, 0],
                 [3, 2, 1],
-            ],
+            ]],
         dtype=int),
         **da_kwargs
     )
     d["artificial_recharge_capacity"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [0.4, 0.4, 0.4],
                 [nan, nan, nan],
                 [0.6, 0.6, 0.6],
-            ]
+            ]]
         ),
         **da_kwargs
     )
-    d["wet_area"] = xr.DataArray(
-        np.array(
+    d["wetted_area"] = xr.DataArray(
+        np.array([
             [
                 [0.1, 0.1, 0.1],
                 [nan, nan, nan],
                 [0.2, 0.2, 0.2],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["urban_area"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [0.2, 0.2, 0.2],
                 [nan, nan, nan],
                 [0.3, 0.3, 0.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["urban_ponding_depth"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [2.2, 2.2, 2.2],
                 [nan, nan, nan],
                 [2.3, 2.3, 2.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["rural_ponding_depth"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1.2, 1.2, 1.2],
                 [nan, nan, nan],
                 [1.3, 1.3, 1.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["urban_runoff_resistance"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [3.2, 3.2, 3.2],
                 [nan, nan, nan],
                 [3.3, 3.3, 3.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["rural_runoff_resistance"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [3.6, 3.6, 3.6],
                 [nan, nan, nan],
                 [3.7, 3.7, 3.7],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["urban_runon_resistance"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [5.2, 5.2, 5.2],
                 [nan, nan, nan],
                 [5.3, 5.3, 5.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["rural_runon_resistance"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [5.6, 5.6, 5.6],
                 [nan, nan, nan],
                 [5.7, 5.7, 5.7],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["urban_infiltration_capacity"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [10.2, 10.2, 10.2],
                 [nan, nan, nan],
                 [10.3, 10.3, 10.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["rural_infiltration_capacity"] = xr.DataArray(
-        np.array(
+        np.array([
             [
                 [20.2, 20.2, 20.2],
                 [nan, nan, nan],
                 [20.3, 20.3, 20.3],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["perched_water_table_level"]= xr.DataArray(
-        np.array(
+        np.array([
             [
                 [2.0, 2.0, 2.0],
                 [nan, nan, nan],
                 [2.0, 2.0, 2.0],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["soil_moisture_fraction"]= xr.DataArray(
-        np.array(
+        np.array([
             [
                 [1.5, 1.5, 1.5],
                 [nan, nan, nan],
                 [1.5, 1.5, 1.5],
-            ]
+            ]]
         ),
         **da_kwargs
     )
     d["conductivitiy_factor"]= xr.DataArray(
-        np.array(
+        np.array([
             [
                 [2.5, 2.5, 2.5],
                 [nan, nan, nan],
                 [2.5, 2.5, 2.5],
-            ]
+            ]]
         ),
         **da_kwargs
     )
