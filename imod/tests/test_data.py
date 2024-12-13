fb5c786f65765a523f215373c4856f82e7c41a74
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-12-13 15:23:59 +0100

    Issue #1329 dsd tutorial 3 material (#1334)
    Fixes #1329
    
    # Description
    Adds data for tutorial 03. This tutorial will be part of the iMOD
    Documentation.
    Data will be unzipped in a directory chosen by the user.
    
    So it should be used as follows in the tutorial:
    
    ```python
    import imod
    
    directory = r"\path\to\dir"
    
    # download data
    prj_path = imod.data.tutorial_03(directory)
    
    # open_data
    imod5_data, period_data = imod.prj.open_projectfile_data(prj_path)
    ```
    
    # Checklist
    
    - [x] Links to correct issue
    - [x] Update changelog, if changes affect users
    - [x] PR title starts with ``Issue #nr``, e.g. ``Issue #737``
    - [x] Unit tests were added
    - [ ] **If feature added**: Added/extended example

--- imod/tests/test_data.py
+++ imod/tests/test_data.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import xarray as xr
 
@@ -26,3 +27,46 @@ def test_hondsrug_data():
         imod.data.hondsrug_drainage,
     ]:
         assert isinstance(f(), xr.Dataset)
+
+
+def test_tutorial_03_data__unzipped(tmp_path):
+    """
+    Test if tutorial 03 material can be unzipped.
+    """
+    # Act
+    prj_path = imod.data.tutorial_03(tmp_path)
+
+    # Assert
+    assert isinstance(prj_path, Path)
+    assert prj_path.suffix == ".prj"
+    n_files = sum(1 for x in prj_path.parent.rglob("*") if x.is_file())
+    assert n_files == 107
+
+
+def test_tutorial_03_data__open_data(tmp_path):
+    """
+    Test if tutorial 03 material can be opened.
+    """
+    # Act
+    prj_path = imod.data.tutorial_03(tmp_path)
+    imod5_data, _ = imod.formats.prj.open_projectfile_data(prj_path)
+
+    # Assert
+    expected_keys = {
+        "bnd",
+        "top",
+        "bot",
+        "khv",
+        "kva",
+        "sto",
+        "shd",
+        "rch",
+        "riv",
+        "drn-1",
+        "drn-2",
+        "drn-3",
+        "drn-4",
+        "pcg",
+    }
+    missing_keys = expected_keys - set(imod5_data.keys())
+    assert len(missing_keys) == 0
