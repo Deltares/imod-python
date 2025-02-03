6d55b7166d94b500dd9ea74ea0ce7931b13597ee
Sunny Titus <sunny.titus@deltares.nl>
2025-02-03 14:36:19 +0100

    Fix failing unittests

--- imod/mf6/utilities/mask.py
+++ imod/mf6/utilities/mask.py
@@ -48,7 +48,9 @@ def _mask_all_packages(
     mask: GridDataArray,
 ):
     spatial_dimension_names = get_spatial_dimension_names(mask)
-    if any(coord not in spatial_dimension_names for coord in mask.coords):
+    additional_dimension_names = ['dx', 'dy']
+    dimension_names = spatial_dimension_names + additional_dimension_names
+    if any(coord not in dimension_names for coord in mask.coords):
         raise ValueError("unexpected coordinate dimension in masking domain")
 
     for pkgname, pkg in model.items():
