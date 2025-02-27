17379c06a78f0826e28d91baef9fbcb810f585b5
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-02-27 17:27:03 +0100

    Move useful dataset utilities to common utilities

--- imod/mf6/oc.py
+++ imod/mf6/oc.py
@@ -6,9 +6,9 @@ from typing import Any, Dict, Union
 import numpy as np
 
 from imod.common.interfaces.iregridpackage import IRegridPackage
+from imod.common.utilities.value_filters import is_dataarray_none
 from imod.logging import init_log_decorator
 from imod.mf6.package import Package
-from imod.mf6.utilities.dataset import is_dataarray_none
 from imod.mf6.write_context import WriteContext
 from imod.schemata import DTypeSchema
 
