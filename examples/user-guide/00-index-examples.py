b5fc9dfc0a619d45925e42c27b0567a3b9a708f6
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-04-23 14:31:54 +0200

    Make example scripts more readable as well

--- examples/user-guide/00-index-examples.py
+++ examples/user-guide/00-index-examples.py
@@ -6,7 +6,7 @@ import xugrid as xu
 import imod
 
 elevation_uda = xu.data.elevation_nl()
-# Drop unnecessary coords. These are also stored in elevation_uda.ugrid.grid
+# Drop unnecessary coords. These coords are also stored in elevation_uda.ugrid.grid
 elevation_uda = elevation_uda.drop_vars(["mesh2d_face_x", "mesh2d_face_y"])
 
 part = elevation_uda.ugrid.sel(
@@ -18,9 +18,11 @@ part = elevation_uda.ugrid.sel(
 resolution = 500.0
 xmin, ymin, xmax, ymax = part.ugrid.grid.bounds
 structured_grid = imod.util.empty_2d(resolution, xmin, xmax, resolution, ymin, ymax)
+
 # Interpolate
 regridder = xu.BarycentricInterpolator(part, structured_grid)
 interpolated_elevation = regridder.regrid(part)
+
 # Plot
 interpolated_elevation.plot.imshow()
 
@@ -40,14 +42,17 @@ interpolated_elevation.rio.to_raster(tmp_dir / "elevation.tif")
 # Open Geotiff with elevation data as xarray DataArray
 elevation = imod.rasterio.open(tmp_dir / "elevation.tif")
 elevation.load()
+
 # Create idomain grid
 layer_template = xr.DataArray([1, 1, 1], dims=("layer",), coords={"layer": [1, 2, 3]})
 idomain = layer_template * xr.ones_like(elevation).astype(int)
+
 # Compute bottom elevations of model layers
 layer_thickness = xr.DataArray(
     [10.0, 20.0, 10.0], dims=("layer",), coords={"layer": [1, 2, 3]}
 )
 bottom = elevation - layer_thickness.cumsum(dim="layer")
+
 # Create MODFLOW 6 DIS package
 dis_pkg = imod.mf6.StructuredDiscretization(
     idomain=idomain, top=elevation, bottom=bottom.transpose("layer", "y", "x")
@@ -58,25 +63,32 @@ dis_pkg = imod.mf6.StructuredDiscretization(
 # layer, row and column:
 import pandas as pd
 
-screen_top = [0.0, 0.0]
-screen_bottom = [-4.0, -10.0]
+# Specify well locations
 x = [150_200.0, 160_800.0]
 y = [450_300.0, 460_200.0]
-# Create transient well package, rates every 2 days
+
+# Specify well screen depths
+screen_top = [0.0, 0.0]
+screen_bottom = [-4.0, -10.0]
+
+# Specify flow rate, which changes over time.
 weltimes = pd.date_range("2000-01-01", "2000-01-03", freq="2D")
-rate = xr.DataArray(
-    [[0.5, 1.0], [2.5, 3.0]], coords={"time": weltimes}, dims=("time", "index")
-)
-wel_pkg = imod.mf6.Well(
-    x=x, y=y, screen_top=screen_top, screen_bottom=screen_bottom, rate=rate
-)
+well_rates_period1 = [0.5, 1.0]
+well_rates_period2 =  [2.5, 3.0]
+rate = xr.DataArray([well_rates_period1, well_rates_period2], coords={"time": weltimes}, dims=("time","index"))
+
+# Now construct the Well package
+wel_pkg = imod.mf6.Well(x=x, y=y, rate=rate, screen_top=screen_top, screen_bottom=screen_bottom)
+
 # iMOD Python will take care of the rest and assign the wells to the correct model
 # layers upon writing the model. It will furthermore distribute well rates based
 # on transmissivities. To verify how wells will be assigned to model layers before
 # writing the entire simulation, you can use the following command:
 wel_mf6_pkg = wel_pkg.to_mf6_pkg(idomain, elevation, bottom, k=1.0)
+
 # Wells have been distributed across two model layers based on screen depths.
 print(wel_mf6_pkg["cellid"])
+
 # Well rates have been distributed based on screen overlap
 print(wel_mf6_pkg["rate"])
 
@@ -90,7 +102,11 @@ simulation = imod.mf6.Modflow6Simulation("example")
 simulation["gwf"] = imod.mf6.GroundwaterFlowModel()
 simulation["gwf"]["dis"] = dis_pkg
 simulation["gwf"]["wel"] = wel_pkg
+
+# Create a time discretization based on the times assigned to the packages.
+# Specify the end time of the simulation as one of the additional_times
 simulation.create_time_discretization(additional_times=["2000-01-07"])
+
 # Note that timesteps in well package are also inserted in the time
 # discretization
 print(simulation["time_discretization"].dataset)
@@ -103,17 +119,30 @@ print(simulation["time_discretization"].dataset)
 # average.
 
 new_unstructured_grid = part
-
 sim_regridded = simulation.regrid_like("regridded", new_unstructured_grid)
+
 # Notice that discretization has converted to VerticesDiscretization (DISV)
 print(sim_regridded["gwf"]["dis"])
 
 # %%
 # To reduce the size of your model, you can clip it to a bounding box. This is
 # useful for example when you want to create a smaller model for testing purposes.
-
 sim_clipped = simulation.clip_box(
     x_min=125_000, x_max=175_000, y_min=425_000, y_max=475_000
 )
+# %% 
+# You can even provide states for the model, which will be set on the model
+# boundaries of the clipped model. Create a grid of zeros, which will be used to
+# set as heads at the boundary
+head_for_boundary = xr.zeros_like(idomain, dtype=float)
+states_for_boundary = {"gwf": head_for_boundary}
+
+sim_clipped = simulation.clip_box(
+    x_min=125_000, x_max=175_000, y_min=425_000, y_max=475_000, states_for_boundary=states_for_boundary
+)
+
+# Notice that a Constant Head (CHD) package has been created for the clipped
+# model.
+print(sim_clipped["gwf"])
 
 # %%
