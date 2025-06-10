1f0e0498d6725f14844e19e3ddbc2ae1998366ad
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-06-10 17:48:33 +0200

    Update tests

--- imod/tests/test_mf6/test_ex01_twri.py
+++ imod/tests/test_mf6/test_ex01_twri.py
@@ -12,6 +12,7 @@ from imod.mf6.validation_context import ValidationContext
 from imod.mf6.write_context import WriteContext
 from imod.schemata import ValidationError
 from imod.typing.grid import ones_like
+from imod.common.utilities.version import get_version
 
 
 def test_dis_render(twri_model, tmp_path):
@@ -283,8 +284,11 @@ def test_wel_render(twri_model, tmp_path):
     with open(wel_path, "r") as wel_file:
         actual = wel_file.read()
 
+    version = get_version()
     expected = textwrap.dedent(
-        """\
+        f"""\
+            # File written with iMOD Python version: {version}
+
             begin options
               save_flows
             end options
