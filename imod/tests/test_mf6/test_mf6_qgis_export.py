4ac039e50e6f1f6fb2582c311e892b7ba2d873e0
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-06-20 11:34:35 +0200

    Format

--- imod/tests/test_mf6/test_mf6_qgis_export.py
+++ imod/tests/test_mf6/test_mf6_qgis_export.py
@@ -13,7 +13,7 @@ import imod
 
 @pytest.mark.qgis_export
 def test_mf6_qgis_export__structured(tmp_path):
-    mf6_sim = imod.data.hondsrug_simulation(tmp_path /"unzipped")
+    mf6_sim = imod.data.hondsrug_simulation(tmp_path / "unzipped")
 
     path_dumped = Path(tmp_path) / "hondsrug_MDAL"
     mf6_sim.dump(path_dumped, mdal_compliant=True, crs="EPSG:28992")
@@ -22,7 +22,7 @@ def test_mf6_qgis_export__structured(tmp_path):
 
 @pytest.mark.qgis_export
 def test_mf6_qgis_export__unstructured(tmp_path):
-    mf6_sim = imod.data.hondsrug_simulation(tmp_path /"unzipped")
+    mf6_sim = imod.data.hondsrug_simulation(tmp_path / "unzipped")
     idomain = mf6_sim["GWF"]["dis"]["idomain"]
     grid = xu.UgridDataArray.from_structured2d(idomain)
 
