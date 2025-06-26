9bccd46facc783e217acc11389e1d109ccc964e6
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-06-26 15:45:53 +0200

    Add code to regrid a structured iMOD5 model to an unstructured grid

--- examples/user-guide/12-imod5-model-to-modflow6.py
+++ examples/user-guide/12-imod5-model-to-modflow6.py
@@ -115,11 +115,229 @@ mf6_sim["ims"] = imod.mf6.SolutionPresetSimple(["imported_model"])
 mf6_sim["ims"]
 
 # %%
+#
+# By default, the iMOD5 model rasters will not be directly loaded into memory,
+# but instead will be lazily loaded. Read more about this in the
+# :doc:`/06-lazy-evaluation` documentation.
+# 
+# By default the data is chunked per raster file, which is a chunk per layer,
+# per timestep. Usually this is not optimal, as this creates many small chunks.
+# The discretization (DIS) package and node property flow (NPF) package are used
+# frequently during the validation process, so it saves us a lot of waiting time
+# if we load these into memory.
+# 
+gwf_model = mf6_sim["imported_model"]
+gwf_model["dis"].dataset.load()
+gwf_model["npf"].dataset.load()
+
+# %%
 # 
 # This has a inner_dvclose of 0.001 instead of 0.01, which is a lot stricter: a
 # numerical error of 1 mm is only allowed by the solver, instead of 1 cm.
 #
+# Let's try to write this simulation. **spoiler alert**: this will fail, because
+# we still have to configure some packages.
+
+mf6_dir = tmpdir / "mf6_structured"
+
+# Ignore this "with" statement, it is to catch the error and render the
+# documentation without error.
+with imod.util.print_if_error(ValueError):
+    mf6_sim.write(mf6_dir)  # Attention: this will fail!
+
+# %%
+# 
+# We are still missing output control, as the projectfile does not contain this
+# information. For this example, we'll only save the last head of each stress period.
+
+
+gwf_model["oc"] = imod.mf6.OutputControl(
+    save_head="last",
+)
+
+from imod.schemata import ValidationError
+
+with imod.util.print_if_error(ValidationError):
+    mf6_sim.write(mf6_dir)  # Attention: this will fail!
+
+# %% 
+# 
+# Argh! The simulation still fails to write. In general, iMOD Python is a lot
+# stricter with writing model data than iMOD5. iMOD Python forces users to
+# conciously clean up their models, whereas iMOD5 cleans data under the hood.
+# The error message states that the nodata is not aligned with idomain. This
+# means there are k values and ic values specified at inactive locations, or
+# vice versa. Let's try if masking the data works. This will remove all inactive
+# locations (idomain==0) from the data.
+
+idomain = gwf_model["dis"]["idomain"]
+mf6_sim.mask_all_models(idomain)
+
+mf6_sim.write(mf6_dir)
+
+# %%
+#
+# Let's run the simulation and open the head data.
+
+mf6_sim.run()
+head_structured = mf6_sim.open_head()
+
+# %%
 # Now that we have a MODFLOW 6 simulation, we can regrid it to an unstructured
+# grid. Let's first load a triangular grid.
+
+# TODO: Add this to the artifacts
+path_grid = r"c:\Users\engelen\projects_wdir\imod-python\examples\hondsrug_unstructured\triangular_grid.nc"
+
+import xugrid as xu
+triangular_grid = xu.open_dataarray(path_grid).ugrid.grid
+
+triangular_grid.plot()
+
+# %%
+#
+# That looks more exciting than the rectangular grid we had before. You
+# can see there is refinement around some of the streams.
+#
+# However, this grid is triangular, which has the disadvantage that the connections
+# between cell centers are not orthogonal to the cell edges, which can lead to
+# mass balance errors. xugrid has a method to convert this triangular grid
+# to a Voronoi grid, which has orthogonal connections between cell centers and
+# edges.
+
+voronoi_grid = triangular_grid.tesselate_centroidal_voronoi()
+voronoi_grid.plot()
+
+# %%
+#
+# Now that we have a Voronoi grid, we can regrid the MODFLOW 6 simulation to this
 # grid.
+
+# Workaround for bug where iMOD Python doesn't recognice Ugrid2D, only UgridDataArray
+import xarray as xr
+import numpy as np
+
+data = xr.DataArray(np.ones(voronoi_grid .sizes["mesh2d_nFaces"]), dims=["mesh2d_nFaces"])
+voronoi_uda = xu.UgridDataArray(data, voronoi_grid )
+
+mf6_unstructured = mf6_sim.regrid_like("unstructured_example", voronoi_uda, validate=False)
+mf6_unstructured
+
+# %%
+#
+# Let's take a gander at how the river data is regridded.
+
+mf6_unstructured["imported_model"]["riv-1riv"]["stage"].isel(layer=0).ugrid.plot()
+
+# %% 
+# Let's try to write this to a temporary directory. **Spoiler alert**: this will
+# fail.
+mf6_dir = tmpdir / "mf6_unstructured"
+
+# Ignore this "with" statement, it is to catch the error and render the
+# documentation without error.
+with imod.util.print_if_error(ValidationError):
+    mf6_unstructured.write(mf6_dir) # Attention: this will fail!
+
+# %%
+#
+# The error message states that the iMOD5 model has a river package that has its
+# river bottom elevation below the model bottom. The averaging when regridding
+# can cause this: The model bottom has a continuous surface, whereas the rivers
+# usually are located in a local valley. Upscaling both with a mean causes the
+# river bottom elevation to have the tendency to be lower than the model bottom.
+# We therefore need to reallocate the river data to the new model layer
+# schematization. There currently is no direct method to do this, but we can
+# reallocate the river dataset with the following function.
+
+def reallocate_river(river_ds, dis, npf, allocation_option, distributing_option):
+    """
+    Reallocates river data in place. Aggregate river data to planar data first,
+    by taking the mean across layers for the stage and bottom elevation, and the
+    sum for the conductance. Consequently allocate and distribute the planar
+    data to the provided model layer schematization.
+
+    Parameters
+    ----------
+    river_ds : xarray.Dataset
+        The river dataset to reallocate.
+    dis : StructuredDiscretization | VerticesDiscretization
+        The discretization of the model to which the river data should be
+        reallocated.
+    npf : NodePropertyFlow
+        The node property flow package of the model to which the river
+        conductance should be distributed (if applicable).
+    allocation_option : ALLOCATION_OPTION
+        The allocation option to use for the reallocation.
+    distributing_option : DISTRIBUTING_OPTION
+        The distributing option to use for the reallocation.
+    """
+    aggr_dict = {
+        "stage": np.nanmean, "conductance": np.nansum, "bottom_elevation": np.nanmean
+    }
+    planar_data = {key: river_ds[key].reduce(func, dim="layer") for key, func in aggr_dict.items()}
+    riv_dict, _ = imod.mf6.River.allocate_and_distribute_planar_data(
+        planar_data, dis, npf, allocation_option, distributing_option
+    )
+    # .update for some reason requires the dimensions to be specified.    
+    riv_dict = {key: (da.dims, da) for key, da in riv_dict.items()}
+    river_ds.update(riv_dict)
+
+from imod.prepare import ALLOCATION_OPTION, DISTRIBUTING_OPTION
+
+gwf_unstructured = mf6_unstructured["imported_model"]
+dis = gwf_unstructured["dis"]
+npf = gwf_unstructured["npf"]
+
+args =  (dis, npf, ALLOCATION_OPTION.stage_to_riv_bot, DISTRIBUTING_OPTION.by_corrected_transmissivity)
+reallocate_river(gwf_unstructured["riv-1riv"].dataset, *args)
+reallocate_river(gwf_unstructured["riv-2riv"].dataset, *args)
+
+gwf_unstructured["riv-1riv"].cleanup(dis)
+gwf_unstructured["riv-2riv"].cleanup(dis)
+
+# %%
+#
+# Finally, we need to set the HFB validation settings to less strict. Otherwise,
+# we'll get errors about hfb's being connected to inactive cells. Normally, you
+# would set this when creating a new Modflow6Simulation, but since we created
+# one from an iMOD5 model, these settings cannot be set upon creation. We can
+# however set the validation settings directly by changing this attribute:
+
+from imod.mf6 import ValidationSettings
+
+mf6_unstructured._validation_context=ValidationSettings(strict_hfb_validation=False)
+
+# %%
+#
+# We'll now be able to finally write the unstructured model.
+
+mf6_unstructured.write(mf6_dir)
+
+# %%
+#
+# Let's run the unstructured model and open the head data.
+
+mf6_unstructured.run()
+head_unstructured = mf6_unstructured.open_head()
+
+# %%
+#
+# Let's upscale the structured head data to the unstructured grid. This is done
+# using the `OverlapRegridder from the xugrid package
+# <https://deltares.github.io/xugrid/examples/regridder_overview.html#overlapregridder>_,
+
+import xugrid as xu
+
+regridder = xu.OverlapRegridder(head_structured, head_unstructured.ugrid.grid)
+head_structured_upscaled = regridder.regrid(head_structured)
+
+# %%
+#
+# Compute the difference between the upscaled structured head and the
+# unstructured head. This should be close to zero, as the upscaling is done
+
+diff = head_structured_upscaled - head_unstructured
+diff.isel(time=-1).mean(dim="layer").ugrid.plot()
 
 # %%
