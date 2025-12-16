582e4e312e9bebb7db9db8e074acb67865e6aa95
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-12-16 16:51:08 +0100

    Add test to clip and add state at boundary from file

--- imod/tests/test_mf6/test_circle.py
+++ imod/tests/test_mf6/test_circle.py
@@ -10,6 +10,7 @@ import imod
 from imod.logging import LoggerType, LogLevel
 from imod.mf6.validation_settings import ValidationSettings
 from imod.mf6.write_context import WriteContext
+from imod.prepare.partition import create_partition_labels
 
 
 def test_simulation_write_and_run(circle_model, tmp_path):
@@ -233,6 +234,86 @@ def test_simulation_clip_and_state_at_boundary(circle_model_transport, tmp_path)
         == 20
     )
     assert half_simulation["GWF_1"]["chd_clipped"]["head"].notnull().sum() == 20
+    # Test if model runs
+    half_simulation.write(tmp_path / "half")
+    half_simulation.run()
+    # Test if the clipped model output has the correct dimension
+    head_half = half_simulation.open_head().compute().reindex_like(idomain_half)
+    concentration_half = (
+        half_simulation.open_concentration().compute().reindex_like(idomain_half)
+    )
+    assert head_half.shape == (52, 2, 108)
+    assert concentration_half.shape == (52, 2, 108)
+
+
+def test_simulation_clip_and_state_at_boundary__from_file(
+    circle_model_transport, tmp_path
+):
+    # Arrange
+    simulation = circle_model_transport
+    idomain = simulation["GWF_1"]["disv"]["idomain"]
+
+    partition_labels = create_partition_labels(idomain, npartitions=2)
+
+    simulation_split = simulation.split(partition_labels)
+    simulation_split.write(tmp_path / "full")
+    simulation_split.run()
+    head = simulation_split.open_head().reindex_like(idomain)
+    concentration = simulation_split.open_concentration().reindex_like(idomain)
+
+    simulation.dump(tmp_path / "dumped")
+    simulated_from_dump = imod.mf6.Modflow6Simulation.from_file(
+        tmp_path / "dumped" / "circle.toml"
+    )
+
+    paths_head = (tmp_path / "full").glob("**/*.hds")
+    paths_conc = (tmp_path / "full").glob("**/*.ucn")
+    paths_grb = (tmp_path / "full").glob("**/disv.disv.grb")
+
+    heads = []
+    concs = []
+    tstart = simulation["time_discretization"]["time"].isel(time=0).values
+    for path_head, path_conc, path_grb in zip(paths_head, paths_conc, paths_grb):
+        head_part = imod.mf6.open_hds(
+            path_head,
+            path_grb,
+            simulation_start_time=tstart,
+        )
+        heads.append(head_part)
+        conc_part = imod.mf6.open_conc(
+            path_conc,
+            path_grb,
+            simulation_start_time=tstart,
+        )
+        concs.append(conc_part)
+
+    head = xu.merge_partitions(heads)
+    conc = xu.merge_partitions(concs)
+    head = head.reindex_like(idomain)
+    conc = conc.reindex_like(idomain)
+
+    states_for_boundary = {
+        "GWF_1": head["head"].isel(time=-1, drop=True),
+        "transport": concentration["concentration"].isel(time=-1, drop=True),
+    }
+    # Act
+    half_simulation = simulated_from_dump.clip_box(
+        x_max=0.1, states_for_boundary=states_for_boundary
+    )
+    # Assert
+    # Test if model dims halved
+    idomain_half = half_simulation["GWF_1"]["disv"]["idomain"]
+    dim = idomain_half.grid.face_dimension
+    np.testing.assert_array_equal(idomain_half.sizes[dim] / idomain.sizes[dim], 0.5)
+    n_conc = (
+        half_simulation["transport"]["cnc_clipped"]["concentration"]
+        .notnull()
+        .sum()
+        .compute()
+    )
+    n_head = half_simulation["GWF_1"]["chd_clipped"]["head"].notnull().sum().compute()
+    assert n_conc == 24
+    assert n_head == 20
     # Test if model runs
     half_simulation.write(tmp_path / "half")
     half_simulation.run()
