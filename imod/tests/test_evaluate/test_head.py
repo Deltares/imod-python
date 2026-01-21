7a8e74a75a13f7006e108c6dc88120ee0b0aa5ca
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-01-21 17:34:17 +0100

    Make sure convert pointwaterhead to freshwaterhead also works for UgridDataArrays.

--- imod/tests/test_evaluate/test_head.py
+++ imod/tests/test_evaluate/test_head.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import pytest
 import xarray as xr
+import xugrid as xu
+from pytest_cases import parametrize_with_cases
 
 import imod
+from imod.typing.grid import full_like
 
 
 def test_convert_pointwaterhead_freshwaterhead_scalar():
@@ -20,18 +23,51 @@ def test_convert_pointwaterhead_freshwaterhead_scalar():
     )
 
 
-def test_convert_pointwaterhead_freshwaterhead_da():
-    data = np.ones((3, 2, 1))
-    coords = {"layer": [1, 2, 3], "y": [0.5, 1.5], "x": [0.5]}
+def structured_grid():
+    data = np.ones((3, 2, 2))
+    coords = {"layer": [1, 2, 3], "y": [0.5, 1.5], "x": [0.5, 1.5]}
     dims = ("layer", "y", "x")
 
-    da = xr.DataArray(data, coords, dims)
-    pwh = xr.full_like(da, 4.0)
-    dens = xr.full_like(da, 1025.0)
-    z = xr.full_like(da, 1.0)
-    fwh = xr.full_like(da, 4.075)
-    fwh2 = imod.evaluate.convert_pointwaterhead_freshwaterhead(pwh, dens, z)
-    assert fwh.equals(fwh2.round(5))
+    return xr.DataArray(data, coords, dims)
+
+
+class GridCases:
+    def case_structured(self):
+        return structured_grid()
+
+    def case_unstructured(self):
+        structured = structured_grid()
+        return xu.UgridDataArray.from_structured(structured)
+
+
+@parametrize_with_cases("da", cases=GridCases)
+def test_convert_pointwaterhead_freshwaterhead_da(da):
+    pwh = full_like(da, 4.0)
+    dens = full_like(da, 1025.0)
+    z = full_like(da, 1.0)
+    fwh_expected = full_like(da, 4.075)
+    fwh_actual = imod.evaluate.convert_pointwaterhead_freshwaterhead(pwh, dens, z)
+    assert type(fwh_actual) is type(da)
+    assert fwh_expected.equals(fwh_actual.round(5))
+
+
+@parametrize_with_cases("da", cases=GridCases)
+def test_convert_pointwaterhead_freshwaterhead_backfill(da):
+    # edge case: point water head below z
+    # return freshwater head of top underlying cell where elevation < pointwaterhead
+    # only for grids with layers
+    #
+    # Test this by setting point water head in top layer to 0.0. This should
+    # trigger a backfill with the value from layer 2, which is 4.075. So in the
+    # end, test results should be 4.075 everywhere.
+    pwh = full_like(da, 4.0)
+    pwh[0, ...] = 0.0
+    dens = full_like(da, 1025.0)
+    z = full_like(da, 1.0)
+    fwh_expected = full_like(da, 4.075)
+    fwh_actual = imod.evaluate.convert_pointwaterhead_freshwaterhead(pwh, dens, z)
+    assert type(fwh_actual) is type(da)
+    assert fwh_expected.equals(fwh_actual.round(5))
 
 
 @pytest.mark.parametrize("chunk", [False, True])
