6cca737558602e8ae2d4f197523c8c79a1d4e63e
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-05-07 09:10:47 +0200

    Fix messy import

--- imod/tests/test_formats/test_common.py
+++ imod/tests/test_formats/test_common.py
@@ -1,6 +1,7 @@
-def test_infer_delimwhitespace():
-    from imod.formats.gen.gen import infer_delimwhitespace
+from imod.formats.common import infer_delimwhitespace
 
+
+def test_infer_delimwhitespace():
     assert infer_delimwhitespace("1 2 3", 3) == (True, True)
     assert infer_delimwhitespace("1,2,3", 3) == (False, True)
     assert infer_delimwhitespace("1,2,3", 4) == (False, False)
