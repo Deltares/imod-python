b56a69f039cb808c9371ad395f276b2c67a15c09
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-07-08 11:48:04 +0200

    Avoid reassignging to variable with different type.

--- imod/common/utilities/mask.py
+++ imod/common/utilities/mask.py
@@ -218,7 +218,7 @@ def broadcast_and_mask_arrays(
     # will result in no spatial grid.
     if not is_spatial_grid(broadcasted_arrays[0]):
         raise ValueError("One or more arrays need to be a spatial grid.")
-    broadcasted_arrays = dict(zip(arrays.keys(), broadcasted_arrays))
+    broadcasted_arrays_dict = dict(zip(arrays.keys(), broadcasted_arrays))
 
     # Mask arrays with np.nan values
-    return mask_arrays(broadcasted_arrays)
+    return mask_arrays(broadcasted_arrays_dict)
