6405be447b34fcd41e1184809ada799f8383b0ce
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-07-16 15:15:02 +0200

    Change order of merging to first merge tables, then to grid and deal with edge case

--- imod/tests/_scratch.py
+++ imod/tests/_scratch.py
@@ -2,7 +2,7 @@
 import imod
 import xarray as xr
 from imod.util.dims import drop_layer_dim_cap_data
-
+import pandas as pd
 
 # %%
 SUBUNIT = 0
@@ -16,16 +16,15 @@ well = imod.mf6.LayeredWell.from_imod5_cap_data(
     imod5_data, target_dis=None, regridder_types=None, regrid_cache=None
 )
 # %%
+# Setup to get example args for _render() of SprinklingPoints
 prj_data, period_data = imod.formats.prj.open_projectfile_data(r"c:\Users\engelen\projects_wdir\imod-python\imod5_converter\NHI_sprint\Peelvenen\prjfiles\Peelvenen_relative_paths_dis_npf.PRJ")
 
-# %%
 dis_pkg = imod.mf6.StructuredDiscretization.from_imod5_data(prj_data, validate=False)
 dis_pkg["idomain"] = dis_pkg["idomain"].clip(min=0)
 npf_pkg = imod.mf6.NodePropertyFlow.from_imod5_data(prj_data, dis_pkg.dataset["idomain"])
 
 prj_data = drop_layer_dim_cap_data(prj_data)
 griddata, msw_active = imod.msw.GridData.from_imod5_data(prj_data, dis_pkg)
-# %%
 # Convert to args of Sprinkling._render()
 mf6_well = well.to_mf6_pkg(dis_pkg["idomain"], dis_pkg["top"], dis_pkg["bottom"], npf_pkg["k"])
 isactive_1d, svat = griddata.generate_isactive_svat_arrays()
@@ -44,6 +43,23 @@ art_grid = art_grid.rename("id_msw")
 dataset = xr.merge([art_grid, points_ds])
 
 # %%
+# Promote id to dim to join datasets
+mf6_well_ds = mf6_well.dataset.set_coords("id").swap_dims({"ncellid": "id"})
+# Convert the cellid DataArray to a broad table for easier manipulation.
+mf6_cellid_df = mf6_well_ds["cellid"].to_dataset("dim_cellid").to_dataframe()
+# Select only the cellid columns we need and reset index to promote id to column
+# for merging
+dim_cellid = ["layer", "row", "column"]
+mf6_cellid_df = mf6_cellid_df.loc[:, dim_cellid].reset_index()
+# Merge again to confine to wells actually used in the modflow6 model.
+# This drops points that are not in art_grid.
+points_mf6_merged_df = arl_points.reset_index().merge(
+    mf6_cellid_df,
+    on="id",
+    how="right",
+    validate="many_to_one"
+)
+# %%
 # Flatten id_msw grid → (y, x, id_msw) table, drop cells with no well
 # TODO: Verify with Peter that only the first subunit (landuse: agriculture) is
 #   relevant for the SVAT mapping.
@@ -60,28 +76,10 @@ art_df = (
 art_df = art_df.drop(["dx", "dy"], axis=1)
 
 # Join: each SVAT cell gets the matching well row(s) from arl_points
-msw_merged_df = art_df.merge(
-    arl_points.reset_index(),   # brings id back as a column
+msw_mf6_merged_df = art_df.merge(
+    points_mf6_merged_df,   # brings id back as a column
     left_on="id_msw",
     right_on="id2grid_p",
-    how="inner",
-    validate="many_to_one"
-)
-
-# %%
-# Promote id to dim to join datasets
-mf6_well_ds = mf6_well.dataset.set_coords("id").swap_dims({"ncellid": "id"})
-# Convert the cellid DataArray to a broad table for easier manipulation.
-mf6_cellid_df = mf6_well_ds["cellid"].to_dataset("dim_cellid").to_dataframe()
-# Select only the cellid columns we need and reset index to promote id to column
-# for merging
-dim_cellid = ["layer", "row", "column"]
-mf6_cellid_df = mf6_cellid_df.loc[:, dim_cellid].reset_index()
-# Merge again to confine to wells actually used in the modflow6 model.
-# This drops points that are not in art_grid.
-msw_mf6_merged_df = msw_merged_df.merge(
-    mf6_cellid_df,
-    on="id",
     how="inner",
     validate="many_to_one"
 )
@@ -99,22 +97,37 @@ svat_groundwater = svat_aligned.data[SUBUNIT, indexer[:, 1], indexer[:, 2]]
 msw_mf6_merged_df["svat_groundwater"] = svat_groundwater.astype(int)
 
 # %%
-# Deal with edge case: wells that are outside art_grid, but in model domain
-outside_art = ~mf6_cellid_df["id"].isin(msw_mf6_merged_df["id"].unique())
-indexer_outside = mf6_cellid_df.loc[outside_art, dim_cellid].to_numpy() - 1
-svat_outside = svat_aligned.data[SUBUNIT, indexer_outside[:, 1], indexer_outside[:, 2]]
-
-# %%
 # Select columns that need to be written to scap_svat.inp
 dataframe = msw_mf6_merged_df[["svat", "layer", "svat_groundwater"]]
 dataframe["svat"] = dataframe["svat"].astype(int)
 capacity = msw_mf6_merged_df["capacity"]
+# Set wells with layer > 0 to groundwater abstraction, and wells with layer = 0
+# to surfacewater abstraction.
 is_gw_extraction = msw_mf6_merged_df["layer"] > 0 
 dataframe["max_abstraction_groundwater"] = capacity.where(is_gw_extraction, 0.0)
 dataframe["max_abstraction_surfacewater"] = capacity.where(~is_gw_extraction, 0.0)
 
-# Cases to catch:
-# 1. Well is outside the model domain → surfacewater
-# 2. Well is in layer 0 → surfacewater
+# %% 
+# 
+# Deal with edge case: wells that are outside art_grid, but in model domain.
+# These will be assigned to surfacewater abstraction. We can identify these by
+# checking which wells in mf6_cellid_df are not in msw_mf6_merged_df.
+is_outside_art = ~mf6_cellid_df["id"].isin(msw_mf6_merged_df["id"].unique())
+outside_df = points_mf6_merged_df.loc[is_outside_art, ["layer", "capacity"]]
+# Select the SVAT subunit for these wells based on their row/col location.
+indexer_outside = mf6_cellid_df.loc[is_outside_art, dim_cellid].to_numpy() - 1
+svat_outside = svat_aligned.data[SUBUNIT, indexer_outside[:, 1], indexer_outside[:, 2]]
+outside_df["svat_groundwater"] = svat_outside.astype(int)
+outside_df["svat"] = svat_outside.astype(int)
+# Set capacity to surfacewater abstraction, and set groundwater abstraction to 0.
+outside_df = outside_df.rename(columns={"capacity": "max_abstraction_surfacewater"})
+outside_df["max_abstraction_groundwater"] = 0.0
 
+outside_df = outside_df.query("svat > 0").reset_index(drop=True)
 
+# %%
+#
+# Combine
+dataframe_out = pd.concat([dataframe, outside_df], axis=0, ignore_index=True)
+dataframe_out = dataframe_out.sort_values(by=["svat"]).reset_index(drop=True)
+# %%
