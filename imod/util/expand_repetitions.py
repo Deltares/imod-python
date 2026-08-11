8e6802ee83119fc0815a6c2c24fc258294aaf951
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-08-11 16:37:14 +0200

    Format

--- imod/util/expand_repetitions.py
+++ imod/util/expand_repetitions.py
@@ -117,7 +117,9 @@ def resample_timeseries(
     )
     if time_before_start_input[0]:
         intermediate_df.loc[time_before_start_input, "rate"] = 0.0
-        intermediate_df.loc[time_before_start_input, location_columns] = well_rate.iloc[0][location_columns].values
+        intermediate_df.loc[time_before_start_input, location_columns] = well_rate.iloc[
+            0
+        ][location_columns].values
 
     # compute time difference from perious to current row
     time_diff_col = intermediate_df["time"].diff()
